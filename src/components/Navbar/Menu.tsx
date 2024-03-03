cbae4eab32bdf329fecdc8a5fc010f36e7aabfe7
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-03 22:30:41 +1100

    Adding hamburger menu for mobile

---
+++ src/components/Navbar/Menu.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+
+const Menu = () => {
+  return (
+    <button type="button">
+      <RxHamburgerMenu className="text-2xl font-[700]" />
+    </button>
+  );
+};
+
+export default Menu;
