b7181c69b51392646530b9b2e47499df018a739a
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-04 19:11:29 +1100

    Adding sliding animation

--- src/components/Navbar/Menu.tsx
+++ src/components/Navbar/Menu.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoCloseOutline } from "react-icons/io5";
-import { useEffect, useState } from "react";
-
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 const Menu = () => {
   const [openNavigationMenu, setOpenNavigationMenu] = useState<boolean>(false);
 
@@ -16,24 +16,33 @@ const Menu = () => {
         <RxHamburgerMenu className="text-2xl font-[700]" />
       </button>
 
-      {openNavigationMenu && (
-        <ul
-          className={`fixed top-0 right-0  h-[100vh] w-[250px]  z-100 bg-white shadow-md transition duration-500`}
-        >
-          <button
-            type="button"
-            className="flex w-[95%] mr-14 justify-end  mt-3"
-            onClick={() => setOpenNavigationMenu(false)}
+      <AnimatePresence>
+        {openNavigationMenu && (
+          <motion.div
+            key="sidemenu"
+            initial={{ width: 0 }}
+            animate={{ width: 300 }}
+            transition={{ duration: 0.3, ease: "easeOut" }}
+            exit={{
+              width: 0,
+            }}
+            className={`fixed top-0 right-0  h-[100vh] w-[250px]  z-100 bg-white shadow-md transition duration-500`}
           >
-            <IoCloseOutline className="text-2xl font-[700] " />
-          </button>
-          <li>Solutions</li>
-          <li>Platforms</li>
-          <li>Industry</li>
-          <li>Resources</li>
-          <li>Customer</li>
-        </ul>
-      )}
+            <button
+              type="button"
+              className="flex w-[95%] mr-14 justify-end  mt-3"
+              onClick={() => setOpenNavigationMenu(false)}
+            >
+              <IoCloseOutline className="text-2xl font-[700] " />
+            </button>
+            <div>Solutions</div>
+            <div>Platforms</div>
+            <div>Industry</div>
+            <div>Resources</div>
+            <div>Customer</div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
