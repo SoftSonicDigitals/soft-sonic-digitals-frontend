2076ab01ae19d2d4a91f1e3da2942eefdcc91299
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-05 00:08:12 +1100

    Getting screen size for transition

--- src/components/Navbar/Menu.tsx
+++ src/components/Navbar/Menu.tsx
@@ -1,10 +1,28 @@
 "use client";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoCloseOutline } from "react-icons/io5";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
+import { FaPlus } from "react-icons/fa6";
 const Menu = () => {
   const [openNavigationMenu, setOpenNavigationMenu] = useState<boolean>(false);
+  const [screenWidth, setScreenWidth] = useState<number | null>(null);
+
+  useEffect(() => {
+    // Function to update screenWidth state
+    function handleResize() {
+      setScreenWidth(window.innerWidth);
+    }
+
+    // Add event listener to window resize
+    window.addEventListener("resize", handleResize);
+
+    // Initial call to set initial screenWidth state
+    handleResize();
+
+    // Remove event listener on component unmount
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <>
@@ -21,12 +39,14 @@ const Menu = () => {
           <motion.div
             key="sidemenu"
             initial={{ width: 0 }}
-            animate={{ width: 300 }}
+            animate={{
+              width: screenWidth && screenWidth / 2 ? screenWidth / 2 : 250,
+            }}
             transition={{ duration: 0.3, ease: "easeOut" }}
             exit={{
               width: 0,
             }}
-            className={`fixed top-0 right-0  h-[100vh] w-[250px]  z-100 bg-white shadow-md transition duration-500`}
+            className={`fixed top-0 right-0  h-[100vh]  z-100 bg-white shadow-md transition duration-500 `}
           >
             <button
               type="button"
@@ -35,11 +55,39 @@ const Menu = () => {
             >
               <IoCloseOutline className="text-2xl font-[700] " />
             </button>
-            <div>Solutions</div>
-            <div>Platforms</div>
-            <div>Industry</div>
-            <div>Resources</div>
-            <div>Customer</div>
+            <div className="ml-4 mr-5 ">
+              <div>
+                <div className="text-gray-800 font-bold text-md flex items-center justify-between border-b-[1px] border-gray-200 pb-3 mt-5">
+                  <p className="tracking-wider">Solutions</p>
+                  <FaPlus />
+                </div>
+              </div>
+              <div>
+                <div className="text-gray-800 font-bold text-md flex items-center justify-between border-b-[1px] border-gray-200 pb-3 mt-5">
+                  <p className="tracking-wider">Platforms</p>
+                  <FaPlus />
+                </div>
+              </div>
+
+              <div>
+                <div className="text-gray-800 font-bold text-md flex items-center justify-between border-b-[1px] border-gray-200 pb-3 mt-5">
+                  <p className="tracking-wider">Industry</p>
+                  <FaPlus />
+                </div>
+              </div>
+              <div>
+                <div className="text-gray-800 font-bold text-md flex items-center justify-between border-b-[1px] border-gray-200 pb-3 mt-5">
+                  <p className="tracking-wider">Resources</p>
+                  <FaPlus />
+                </div>
+              </div>
+              <div>
+                <div className="text-gray-800 font-bold text-md flex items-center justify-between border-b-[1px] border-gray-200 pb-3 mt-5">
+                  <p className="tracking-wider">Customers</p>
+                  <FaPlus />
+                </div>
+              </div>
+            </div>
           </motion.div>
         )}
       </AnimatePresence>
