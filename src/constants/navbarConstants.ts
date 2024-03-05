84025b7d5d6139dd4dced49c11d2165f0ece10d9
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-05 11:56:29 +1100

    [WEB-3] PR Review Adjustment

---
+++ src/constants/navbarConstants.ts
@@ -0,0 +1,13 @@
+export const SOLUTIONS = "Solutions";
+export const PLATFORM = "Platform";
+export const RESOURCES = "Resources";
+export const CUSTOMER = "Customer";
+
+export const solutionsMenu = {
+  title: "Solutions",
+  subMenu: [
+    {
+      subMenuHeading: "By Business",
+    },
+  ],
+};
