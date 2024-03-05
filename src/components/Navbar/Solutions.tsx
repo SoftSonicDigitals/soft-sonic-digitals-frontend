b03ac6d0d1266a3c089eb9e956ac540e09b06e5b
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-05 12:59:22 +1100

    [WEB-04] Adding Solutions Submenu

--- src/components/Navbar/Solutions.tsx
+++ src/components/Navbar/Solutions.tsx
@@ -4,12 +4,27 @@ import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 
 const Solutions = () => {
   return (
-    <div className="hidden lg:flex group h-full text-gray-800 hover:text-gray-900  ">
-      <div className=" flex-center gap-0.5 ">
-        <p>{SOLUTIONS_MENU.title}</p>
-        <MdOutlineKeyboardArrowDown className="text-3xl  group-hover:rotate-180 transition duration-300" />
+    <>
+      <div className="hidden lg:flex group h-full text-gray-800 hover:text-gray-900  ">
+        <div className=" flex-center gap-0.5 ">
+          <p>{SOLUTIONS_MENU.title}</p>
+          <MdOutlineKeyboardArrowDown className="text-3xl  group-hover:rotate-180 transition duration-300" />
+        </div>
       </div>
-    </div>
+      <div
+        className="absolute z-100 top-[5.5rem] right-0 bg-white w-[100vw] shadow-t-md text-gray-900 shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px]
+     transform origin-top
+      "
+      >
+        <div className="flex-center flex-col mt-8 ">
+          <h2 className="text-2xl mb-4">Solutions</h2>
+          <p className="text-gray-800 font-[400] tracking-normal mb-4">
+            We push ourselves. We push technology. And we push the boundaries of
+            conventional commerce.
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
 
