58d41b870f3fd94173e3de209c96ac04f797d38f
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-05 22:06:16 +1100

    [WEB-05] Downloading Platform icons

--- src/components/Navbar/Platform.tsx
+++ src/components/Navbar/Platform.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { PLATFORM } from "@/constants/navbar";
 import React, { useState } from "react";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 
@@ -12,7 +13,7 @@ const Platform = () => {
         className="hidden lg:flex group h-full text-gray-800 hover:text-gray-900  "
       >
         <div className=" flex-center gap-0.5 ">
-          <p>Platform</p>
+          <p>{PLATFORM}</p>
           <MdOutlineKeyboardArrowDown
             className={`text-3xl  hover:rotate-180
              transition duration-300`}
