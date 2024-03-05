05df2728d0a941bc99cc2f3ae6ebe40b2ec3d551
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-05 15:46:03 +1100

    [WEB-04] Adding Banner msg

--- src/components/Navbar/MenuBanner.tsx
+++ src/components/Navbar/MenuBanner.tsx
@@ -1,11 +1,15 @@
+import { BANNER_DETAILS } from "@/constants/navbar";
 import React from "react";
 
 const MenuBanner = () => {
   return (
-    <div className="w-full bg-gray-0 px-6">
+    <div className="w-full bg-gray-0 py-5 mx-2">
       <div className="wrapper_container flex justify-between ">
-        <div>Banner msg </div>
-        <div>BTn</div>
+        <div className="flex flex-col">
+          <h2 className="text-xl mb-1">{BANNER_DETAILS.title} </h2>
+          <p className="text-sm font-[400]">{BANNER_DETAILS.description} </p>
+        </div>
+        <div></div>
       </div>
     </div>
   );
--- src/constants/navbarConstants.ts
+++
@@ -1,4 +0,0 @@
-export const SOLUTIONS = "Solutions";
-export const PLATFORM = "Platform";
-export const RESOURCES = "Resources";
-export const Customer = "Customer";
