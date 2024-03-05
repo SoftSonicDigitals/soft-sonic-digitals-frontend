5141dc1cf487e4860827f9f0607be8e7096dc2bd
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-05 15:36:02 +1100

    [WEB-04] Adding banner templater for solution menu

--- src/components/Navbar/index.ts
+++ src/components/Navbar/index.ts
@@ -4,5 +4,14 @@ import Platform from "./Platform";
 import Resources from "./Resources";
 import Solutions from "./Solutions";
 import SolutionsSubmenu from "./SolutionsSubmenu";
+import MenuBanner from "./MenuBanner";
 
-export { Navbar, Solutions, Platform, Resources, SideMenu, SolutionsSubmenu };
+export {
+  Navbar,
+  Solutions,
+  Platform,
+  Resources,
+  SideMenu,
+  SolutionsSubmenu,
+  MenuBanner,
+};
