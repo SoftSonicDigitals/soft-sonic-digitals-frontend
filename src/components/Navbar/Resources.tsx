93d666f47080e4289bb0f538f990862967488bee
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-06 01:11:57 +1100

    [WEB-06] Designing the resources submenu

--- src/components/Navbar/Resources.tsx
+++ src/components/Navbar/Resources.tsx
@@ -8,21 +8,23 @@ const Resources = () => {
   const [isSubMenuOpen, setIsSubMenuOpen] = useState<boolean>(false);
   return (
     <>
-      <div
-        onMouseEnter={() => setIsSubMenuOpen(true)}
-        onMouseLeave={() => setIsSubMenuOpen(false)}
-        className="hidden lg:flex group h-full text-gray-800 hover:text-gray-900  "
-      >
-        <div className=" flex-center gap-0.5 ">
-          <p>{RESOURCES}</p>
-          <MdOutlineKeyboardArrowDown
-            className={`text-3xl  ${
-              isSubMenuOpen ? "rotate-180" : ""
-            } transition duration-300`}
-          />
+      <div className="relative">
+        <div
+          onMouseEnter={() => setIsSubMenuOpen(true)}
+          onMouseLeave={() => setIsSubMenuOpen(false)}
+          className="hidden lg:flex group h-full text-gray-800 hover:text-gray-900  "
+        >
+          <div className=" flex-center gap-0.5 ">
+            <p>{RESOURCES}</p>
+            <MdOutlineKeyboardArrowDown
+              className={`text-3xl  ${
+                isSubMenuOpen ? "rotate-180" : ""
+              } transition duration-300`}
+            />
+          </div>
         </div>
+        <ResourcesSubmenu setIsOpen={setIsSubMenuOpen} />
       </div>
-      <ResourcesSubmenu setIsOpen={setIsSubMenuOpen} />
     </>
   );
 };
--- src/components/Navbar/ResourcesSubmenu.tsx
+++ src/components/Navbar/ResourcesSubmenu.tsx
@@ -1,3 +1,5 @@
+import { RESOURCES_MENU } from "@/prototypes/navbar";
+import Link from "next/link";
 import React from "react";
 type ResourcesSubmenuProps = {
   setIsOpen: React.Dispatch<boolean>;
@@ -7,8 +9,20 @@ const ResourcesSubmenu = ({ setIsOpen }: ResourcesSubmenuProps) => {
     <div
       onMouseEnter={() => setIsOpen(true)}
       onMouseLeave={() => setIsOpen(false)}
-      className="hidden lg:block absolute z-100 top-[5.5rem] right-0 bg-white w-[100vw] shadow-t-md text-gray-900 shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px] transform origin-top  "
-    ></div>
+      className="hidden lg:block absolute z-100 top-[3.7rem] w-[155px]  bg-white  shadow-t-md text-gray-900 shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px] transform origin-top  "
+    >
+      <div className="flex flex-col  text-sm">
+        {RESOURCES_MENU.submenu.map((menu) => (
+          <Link
+            href={menu.link}
+            key={menu.title}
+            className="py-3 pl-3  hover:bg-gray-0"
+          >
+            {menu.title}
+          </Link>
+        ))}
+      </div>
+    </div>
   );
 };
 
