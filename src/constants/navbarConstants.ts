9f3a9425dd9e7c6069762366e50b14cfe5a518c1
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-05 12:04:33 +1100

    [WEB-3] Adding Solutions Constants

--- src/constants/navbarConstants.ts
+++ src/constants/navbarConstants.ts
@@ -1,13 +1,46 @@
-export const SOLUTIONS = "Solutions";
-export const PLATFORM = "Platform";
-export const RESOURCES = "Resources";
-export const CUSTOMER = "Customer";
-
 export const solutionsMenu = {
   title: "Solutions",
   subMenu: [
     {
       subMenuHeading: "By Business",
+      subMenuLinks: [
+        { title: "B2C ecommerce", link: "/" },
+        { title: "B2B ecommerce", link: "/" },
+        { title: "Multi-Vendor Marketplace", link: "/" },
+      ],
+    },
+    {
+      subMenuHeading: "By Architecture & Channel",
+      subMenuLinks: [
+        { title: "Composable Commerce", link: "/" },
+        { title: "Headless Commerce", link: "/" },
+        { title: "Hyva Development", link: "/" },
+        { title: "Mobile Commerce", link: "/" },
+      ],
+    },
+    {
+      subMenuHeading: "Industries",
+      subMenuLinks: [
+        { title: "Jewellery & Diamond", link: "/" },
+        { title: "Food & Grocery", link: "/" },
+        { title: "Furniture & Home Decor", link: "/" },
+        { title: "Manufacturing", link: "/" },
+        { title: "Distribution", link: "/" },
+        { title: "Automotive", link: "/" },
+        { title: "Health & Beauty", link: "/" },
+        { title: "Electronics", link: "/" },
+        { title: "Stones", link: "/" },
+        { title: "Floristry", link: "/" },
+      ],
+    },
+    {
+      subMenuHeading: "Data & Experience",
+      subMenuLinks: [
+        { title: "PIM- Product Information Management", link: "/" },
+        { title: "DAM- Digital Asset Management", link: "/" },
+        { title: "DXP- Digital Expeience Platform", link: "/" },
+        { title: "MDM- Master Data Management", link: "/" },
+      ],
     },
   ],
 };
