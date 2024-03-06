7cbe117d09a573fc434ed78bf6e486b6dee7b8ca
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-06 14:24:16 +1100

    [WEB-09] Adding dropdowns on  clicks

--- src/components/Navbar/SideMenuLinks.tsx
+++ src/components/Navbar/SideMenuLinks.tsx
@@ -3,27 +3,38 @@ import { SIDE_MENU } from "@/prototypes/navbar";
 import React, { useState } from "react";
 import { FaPlus } from "react-icons/fa6";
 import { SideMenuLink } from ".";
+import Link from "next/link";
 
 const SideMenuLinks = () => {
-  const [isDropDownMenuOpen, setIsDropDownMenuOpen] = useState<boolean>(false);
+  const [selectedDropDownMenu, setSelectedDropDownMenu] = useState<
+    number | null
+  >(null);
 
-  const dropDownMenuHandler = () => {
-    setIsDropDownMenuOpen((prev) => !prev);
+  const toggleDropDown = (index: number) => {
+    if (selectedDropDownMenu === index) {
+      return setSelectedDropDownMenu(null);
+    }
+    setSelectedDropDownMenu(index);
   };
 
   return (
     <div className="ml-4 mr-5 ">
-      {SIDE_MENU.map((menu) => (
+      {SIDE_MENU.map((menu, indexOfMenu) => (
         <div
-          onClick={dropDownMenuHandler}
+          onClick={() => toggleDropDown(indexOfMenu)}
           key={menu.title}
           className=" pb-2 border-b-[1px] border-gray-200"
         >
           <div className="text-gray-800 font-bold text-md flex items-center justify-between pb-3 mt-5">
-            <p className="tracking-wider">{menu.title}</p>
-            <FaPlus className="cursor-pointer" />
+            {menu.subMenu && <p className="tracking-wider">{menu.title}</p>}
+            {menu.subMenu && <FaPlus className="cursor-pointer" />}
+            {!menu.subMenu && menu.link && (
+              <Link href={menu.link}>{menu.title}</Link>
+            )}
           </div>
-          <SideMenuLink navLinks={menu.subMenu} />
+          {selectedDropDownMenu === indexOfMenu && (
+            <SideMenuLink navLinks={menu.subMenu} />
+          )}
         </div>
       ))}
     </div>
