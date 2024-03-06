4e5ffd99eb0897d426f2c51a64026c3cc36e1551
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-06 23:50:21 +1100

    [WEB-10] Making the Hero Sectio Responsive

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,13 +3,13 @@ import { PrimaryButton } from "../Reusable";
 
 const Hero = () => {
   return (
-    <div className="wrapper_container flex-center  flex-col h-[78vh]">
-      <div className="flex flex-col gap-6">
-        <div className="flex-center flex-col gap-4">
-          <h1 className="text-[2.4rem] font-[500] tracking-wide">
+    <div className="wrapper_container flex-center  flex-col h-[57vh] 2xl:h-[45vh]">
+      <div className="flex flex-col gap-6 px-4 sm:px-0 xl:mt-20  2x:mt-0">
+        <div className="flex-center flex-col gap-6 text-center leading-tight ">
+          <h1 className="text-[2.4rem] font-[500] mx-4 ">
             Empowering Digital Commerce Transformation
           </h1>
-          <h3 className="text-[1.5rem] font-[500] tracking-wide">
+          <h3 className="text-[.94rem] sm:text-lg md:text-[1.5rem] font-[500] tracking-wide">
             Retailers &nbsp; | &nbsp; Distributors &nbsp; | &nbsp; Manufacturers
           </h3>
         </div>
