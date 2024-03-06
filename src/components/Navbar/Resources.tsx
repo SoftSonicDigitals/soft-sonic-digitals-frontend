181a223c1ef6297088239f3e6c95daf00d834481
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-06 11:10:44 +1100

    [WEB-06] making the menu responsive

--- src/components/Navbar/Resources.tsx
+++ src/components/Navbar/Resources.tsx
@@ -6,25 +6,25 @@ import { ResourcesSubmenu } from ".";
 
 const Resources = () => {
   const [isSubMenuOpen, setIsSubMenuOpen] = useState<boolean>(false);
+
   return (
     <>
-      <div className="relative">
-        <div
-          onMouseEnter={() => setIsSubMenuOpen(true)}
-          onMouseLeave={() => setIsSubMenuOpen(false)}
-          className="hidden lg:flex group h-full text-gray-800 hover:text-gray-900  "
-        >
-          <div className=" flex-center gap-0.5 ">
-            <p>{RESOURCES}</p>
-            <MdOutlineKeyboardArrowDown
-              className={`text-3xl  ${
-                isSubMenuOpen ? "rotate-180" : ""
-              } transition duration-300`}
-            />
-          </div>
+      <div
+        id="resources_div"
+        onMouseEnter={() => setIsSubMenuOpen(true)}
+        onMouseLeave={() => setIsSubMenuOpen(false)}
+        className="hidden relative lg:flex group h-full text-gray-800 hover:text-gray-900  "
+      >
+        <div className=" flex-center gap-0.5 ">
+          <p>{RESOURCES}</p>
+          <MdOutlineKeyboardArrowDown
+            className={`text-3xl  ${
+              isSubMenuOpen ? "rotate-180" : ""
+            } transition duration-300`}
+          />
         </div>
-        <ResourcesSubmenu setIsOpen={setIsSubMenuOpen} />
       </div>
+      {true && <ResourcesSubmenu setIsOpen={setIsSubMenuOpen} />}
     </>
   );
 };
