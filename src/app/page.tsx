7d7cc15388e49b61dfe670e9a8cae094096ac45f
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-06 22:39:22 +1100

    [WEB-10] Adding Hero Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Hero } from "@/components/Hero";
 import React from "react";
 
 const page = () => {
-  return <div></div>;
+  return (
+    <main>
+      <Hero />
+    </main>
+  );
 };
 
 export default page;
