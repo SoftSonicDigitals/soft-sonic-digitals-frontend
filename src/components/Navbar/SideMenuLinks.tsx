69a4ab227597f7060b6a519f92d6fec53f82c0ca
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-06 14:06:35 +1100

    [WEB-09] Adding States for dropdowns link

--- src/components/Navbar/SideMenuLinks.tsx
+++ src/components/Navbar/SideMenuLinks.tsx
@@ -1,16 +1,27 @@
+"use client";
 import { SIDE_MENU } from "@/prototypes/navbar";
-import React from "react";
+import React, { useState } from "react";
 import { FaPlus } from "react-icons/fa6";
 import { SideMenuLink } from ".";
 
 const SideMenuLinks = () => {
+  const [isDropDownMenuOpen, setIsDropDownMenuOpen] = useState<boolean>(false);
+
+  const dropDownMenuHandler = () => {
+    setIsDropDownMenuOpen((prev) => !prev);
+  };
+
   return (
     <div className="ml-4 mr-5 ">
       {SIDE_MENU.map((menu) => (
-        <div key={menu.title} className=" border-b-[1px] border-gray-200">
+        <div
+          onClick={dropDownMenuHandler}
+          key={menu.title}
+          className=" pb-2 border-b-[1px] border-gray-200"
+        >
           <div className="text-gray-800 font-bold text-md flex items-center justify-between pb-3 mt-5">
             <p className="tracking-wider">{menu.title}</p>
-            <FaPlus />
+            <FaPlus className="cursor-pointer" />
           </div>
           <SideMenuLink navLinks={menu.subMenu} />
         </div>
