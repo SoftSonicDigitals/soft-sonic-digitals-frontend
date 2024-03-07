0045b34b6e22464943ddd867828393c7b64e9547
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-08 01:15:38 +1100

    [WEB-11] making a template to display categories

--- src/components/Commerce/Categories.tsx
+++ src/components/Commerce/Categories.tsx
@@ -3,9 +3,27 @@ import React from "react";
 
 const Categories = () => {
   return (
-    <div className="flex-center gap-6 px-8 2xl:px-0 2xl:mt-60 3xl:mt-0">
+    <div className="flex justify-center gap-8  ">
       {COMMERCE_CATEGORIES.map((category) => (
-        <h1 key={category.category_title}>{category.category_title}</h1>
+        <div key={category.category_title} className="">
+          <h1 className=" text-xl font-[700] text-gray-800 mb-6">
+            {category.category_title}
+          </h1>
+          <div
+            className={`${
+              category.category_items.length >= 5
+                ? "grid grid-cols-2 gap-4"
+                : "flex flex-col gap-4"
+            }`}
+          >
+            {category.category_items.map((item) => (
+              <div key={item.title} className="flex flex-col gap-2">
+                <h3 className="text-lg">{item.title}</h3>
+                <p className="text-sm">{item.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
       ))}
     </div>
   );
--- src/components/Commerce/Commerce.tsx
+++ src/components/Commerce/Commerce.tsx
@@ -8,7 +8,7 @@ import { Categories } from ".";
 const Commerce = () => {
   return (
     <section id="Commerce">
-      <div className="wrapper_container ">
+      <div className="wrapper_container flex flex-col gap-12 ">
         <div className="flex-center flex-col gap-6 px-8 2xl:px-0 2xl:mt-60 3xl:mt-0">
           <div className="flex flex-col gap-6 text-center leading-tight ">
             <h1 className="text-[2.2rem] font-[600] text-gray-800">
