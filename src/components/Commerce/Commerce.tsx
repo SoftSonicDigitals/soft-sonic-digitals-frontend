b676dee01f7c46fcc9ed2eab103ceba7f0416660
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-07 23:51:23 +1100

    [WEB-11] Adding constants

--- src/components/Commerce/Commerce.tsx
+++ src/components/Commerce/Commerce.tsx
@@ -1,3 +1,7 @@
+import {
+  COMMERCE_MAIN_HEADING,
+  COMMERCE_SUB_HEADING,
+} from "@/constants/Commerce";
 import React from "react";
 
 const Commerce = () => {
@@ -7,11 +11,10 @@ const Commerce = () => {
         <div className="flex flex-col gap-6 px-4 sm:px-0 2xl:mt-60 3xl:mt-0">
           <div className="flex flex-col gap-6 text-center leading-tight ">
             <h1 className="text-[2.2rem] font-[600] text-gray-800">
-              Leverage modern commerce to create incredible consumer experiences
+              {COMMERCE_MAIN_HEADING}
             </h1>
             <h3 className="text-[.94rem] sm:text-lg md:text-[1.28rem] font-[500] tracking-wide whitespace-pre-wrap">
-              We push ourselves. We push technology. And we push the boundaries
-              of conventional commerce.
+              {COMMERCE_SUB_HEADING}
             </h3>
           </div>
         </div>
---
+++ src/constants/Commerce/index.ts
@@ -0,0 +1,4 @@
+export const COMMERCE_MAIN_HEADING =
+  "Leverage modern commerce to create incredible consumer experiences";
+export const COMMERCE_SUB_HEADING =
+  "We push ourselves. We push technology. And we push the boundaries of conventional commerce.";
