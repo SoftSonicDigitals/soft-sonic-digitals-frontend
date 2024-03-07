8e0b6e38df18922395c698e71321bca3001e45d9
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-08 00:57:16 +1100

    [WEB-11] Fixing the category array

--- src/components/Commerce/Categories.tsx
+++ src/components/Commerce/Categories.tsx
@@ -3,7 +3,11 @@ import React from "react";
 
 const Categories = () => {
   return (
-    <div className="flex-center gap-6 px-8 2xl:px-0 2xl:mt-60 3xl:mt-0"></div>
+    <div className="flex-center gap-6 px-8 2xl:px-0 2xl:mt-60 3xl:mt-0">
+      {COMMERCE_CATEGORIES.map((category) => (
+        <h1 key={category.category_title}>{category.category_title}</h1>
+      ))}
+    </div>
   );
 };
 
