d72b0b801a10a21c84cc34ec1734cab682f045f6
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-08 00:40:42 +1100

    [WEB-11] Adding commerce categories types and prototypes  array

---
+++ src/components/Commerce/Categories.tsx
@@ -0,0 +1,10 @@
+import { COMMERCE_CATEGORIES } from "@/prototypes/commerce";
+import React from "react";
+
+const Categories = () => {
+  return (
+    <div className="flex-center gap-6 px-8 2xl:px-0 2xl:mt-60 3xl:mt-0"></div>
+  );
+};
+
+export default Categories;
--- src/components/Commerce/Commerce.tsx
+++ src/components/Commerce/Commerce.tsx
@@ -3,12 +3,13 @@ import {
   COMMERCE_SUB_HEADING,
 } from "@/constants/Commerce";
 import React from "react";
+import { Categories } from ".";
 
 const Commerce = () => {
   return (
     <section id="Commerce">
       <div className="wrapper_container ">
-        <div className="flex flex-col gap-6 px-4 sm:px-0 2xl:mt-60 3xl:mt-0">
+        <div className="flex-center flex-col gap-6 px-8 2xl:px-0 2xl:mt-60 3xl:mt-0">
           <div className="flex flex-col gap-6 text-center leading-tight ">
             <h1 className="text-[2.2rem] font-[600] text-gray-800">
               {COMMERCE_MAIN_HEADING}
@@ -18,6 +19,7 @@ const Commerce = () => {
             </h3>
           </div>
         </div>
+        <Categories />
       </div>
     </section>
   );
