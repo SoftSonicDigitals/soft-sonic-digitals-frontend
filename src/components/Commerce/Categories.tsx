d330194ffc322cd451802e4c34f3fda94d6338e1
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-08 12:35:49 +1100

    [WEB-11] Adding icons and border

--- src/components/Commerce/Categories.tsx
+++ src/components/Commerce/Categories.tsx
@@ -5,7 +5,10 @@ const Categories = () => {
   return (
     <div className="flex justify-center gap-8  ">
       {COMMERCE_CATEGORIES.map((category) => (
-        <div key={category.category_title} className="">
+        <div
+          key={category.category_title}
+          className=" border-r-[1px] border-gray-200 last:border-none"
+        >
           <h1 className=" text-xl font-[700] text-gray-800 mb-6">
             {category.category_title}
           </h1>
@@ -17,9 +20,18 @@ const Categories = () => {
             }`}
           >
             {category.category_items.map((item) => (
-              <div key={item.title} className="flex flex-col gap-2">
-                <h3 className="text-lg">{item.title}</h3>
-                <p className="text-sm">{item.description}</p>
+              <div key={item.title} className="flex items-center  gap-2">
+                <div>
+                  <img src={"/dropdown-icon.svg"} />
+                </div>
+                <div>
+                  <h3 className="text-lg">
+                    {item.title.includes("-")
+                      ? `(${item.title.split("-")[0]})`
+                      : item.title}
+                  </h3>
+                  <p className="text-sm">{item.description}</p>
+                </div>
               </div>
             ))}
           </div>
