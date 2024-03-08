8b95143c79a00532d199a37714066ad70a1eca72
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-08 22:55:26 +1100

    [WEB-12] adding states and conditional rendering to open up the categories item

--- src/components/Commerce/CategoriesMobileLayout.tsx
+++ src/components/Commerce/CategoriesMobileLayout.tsx
@@ -3,38 +3,63 @@ import { COMMERCE_CATEGORIES } from "@/prototypes/commerce";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
-import { FaPlus } from "react-icons/fa6";
+import { FaMinus, FaPlus } from "react-icons/fa6";
 
 const CategoriesMobileLayout = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  const [selectedDropDownMenu, setSelectedDropDownMenu] = useState<
+    number | null
+  >(null);
+
+  const toggleDropDown = (index: number) => {
+    if (selectedDropDownMenu === index) {
+      return setSelectedDropDownMenu(null);
+    }
+    setSelectedDropDownMenu(index);
+  };
+
+  const isMenuOpen = (selected: number | null, index: number) => {
+    return selected === index;
+  };
+
   return (
     <div className="mb-10">
-      {COMMERCE_CATEGORIES.map((category) => (
+      {COMMERCE_CATEGORIES.map((category, indexOfMenu) => (
         <div
+          onClick={() => toggleDropDown(indexOfMenu)}
           key={category.category_title}
           className="  first:border-t-[1px] border-b-[1px] border-gray-200 text-gray-800 mx-4 py-4   "
         >
           <div className="flex item-center justify-between ">
             <h4 className="font-[600] text-lg">{category.category_title}</h4>
             <div className="flex-center mx-2">
-              <FaPlus className="cursor-pointer text-lg" />
+              {!isMenuOpen(selectedDropDownMenu, indexOfMenu) ? (
+                <FaPlus className="cursor-pointer" />
+              ) : (
+                <FaMinus className="cursor-pointer" />
+              )}
             </div>
           </div>
-          <div className=" my-3 flex flex-col gap-3">
-            {category.category_items.map((item) => (
-              <div key={item.link} className="flex  gap-3 ml-4">
-                <div className="relative w-6 h-6">
-                  <Image src={"/dropdown-icon.svg"} alt="dropdown-logo" fill />
+          {isMenuOpen(selectedDropDownMenu, indexOfMenu) && (
+            <div className=" my-3 flex flex-col gap-3">
+              {category.category_items.map((item) => (
+                <div key={item.link} className="flex  gap-3 ml-4">
+                  <div className="relative w-6 h-6">
+                    <Image
+                      src={"/dropdown-icon.svg"}
+                      alt="dropdown-logo"
+                      fill
+                    />
+                  </div>
+                  <Link href={item.link} className="flex">
+                    {category.category_title ===
+                    "Individually Outstanding, Spectacular in Combination"
+                      ? `${item.title.split("-")[1]}`
+                      : item.title}
+                  </Link>
                 </div>
-                <Link href={item.link} className="flex">
-                  {category.category_title ===
-                  "Individually Outstanding, Spectacular in Combination"
-                    ? `${item.title.split("-")[1]}`
-                    : item.title}
-                </Link>
-              </div>
-            ))}
-          </div>
+              ))}
+            </div>
+          )}
         </div>
       ))}
     </div>
