37e72e693c12375a7ad10c3d2c6f3c7a36328706
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-08 21:21:05 +1100

    [WEB-11] Making the heading responsive

--- src/components/Commerce/Commerce.tsx
+++ src/components/Commerce/Commerce.tsx
@@ -11,7 +11,7 @@ const Commerce = () => {
       <div className="wrapper_container flex flex-col gap-12 ">
         <div className="flex-center flex-col gap-6 px-8 2xl:px-0 2xl:mt-60 3xl:mt-0">
           <div className="flex flex-col gap-6 text-center leading-tight ">
-            <h1 className="text-[2.2rem] font-[600] text-gray-800">
+            <h1 className="text-[1.75rem] lg:text-[2.2rem] font-[600] text-gray-800">
               {COMMERCE_MAIN_HEADING}
             </h1>
             <h3 className="text-[.94rem] sm:text-lg md:text-[1.28rem] font-[500] tracking-wide whitespace-pre-wrap">
