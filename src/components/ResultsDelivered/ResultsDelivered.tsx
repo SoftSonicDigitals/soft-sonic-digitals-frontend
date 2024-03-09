c775e0a4cfee2c71295c9420bd00db1e857c77dd
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-09 19:54:27 +1100

    [WEB-13] Adding headings

--- src/components/ResultsDelivered/ResultsDelivered.tsx
+++ src/components/ResultsDelivered/ResultsDelivered.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const ResultsDelivered = () => {
-  return <div className="wrapper_container">ResultsDelivered</div>;
+  return (
+    <section>
+      <div className="wrapper_container mt-24">
+        <div className="flex-center">
+          <h1 className="text-[2.1rem] font-[500] mx-4 ">
+            The results we deliver
+          </h1>
+        </div>
+        <div></div>
+      </div>
+    </section>
+  );
 };
 
 export default ResultsDelivered;
