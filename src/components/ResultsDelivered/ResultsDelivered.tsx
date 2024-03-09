fe6559dc2f70c9e81ec6d9d7d528e33e799d96d1
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-09 21:30:18 +1100

    [WEB-13] Adding borders,gaps  and adjusting protypes

--- src/components/ResultsDelivered/ResultsDelivered.tsx
+++ src/components/ResultsDelivered/ResultsDelivered.tsx
@@ -1,24 +1,30 @@
+import { RESULTS_DELIVERED } from "@/prototypes/Results";
 import React from "react";
 
 const ResultsDelivered = () => {
   return (
     <section>
-      <div className="wrapper_container mt-24 ">
+      <div className="wrapper_container mt-24 mb-24 ">
         <div className="flex-center mb-12">
           <h1 className="text-[2.1rem] font-[500] mx-4 text-gray-800 ">
             The results we deliver
           </h1>
         </div>
-        <div className="flex-center flex-col">
-          <div>
-            <div className="flex flex-center">
-              <h2 className="text-7xl font-[500] text-gray-800">120</h2>
-              <div className="text-8xl font-[400] text-red">+</div>
+        <div className="flex-center ">
+          {RESULTS_DELIVERED.map(({ title, value }) => (
+            <div
+              key={title}
+              className=" first:pl-0 last:pr-0 px-8 border-r-[1px] border-gray-200 last:border-none"
+            >
+              <div className="flex flex-center">
+                <h2 className="text-6xl font-[500] text-gray-800">{value}</h2>
+                <div className="text-7xl font-[400] text-red ">+</div>
+              </div>
+              <div className="text-[1.5rem] font-[600] text-gray-800">
+                {title}
+              </div>
             </div>
-            <div className="text-[1.7rem] font-[600] text-gray-800">
-              Global Team
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
--- src/prototypes/Results/index.ts
+++ src/prototypes/Results/index.ts
@@ -1,9 +1,10 @@
 import { ResultsDeliveredType } from "@/models/Results";
 
-const RESULTS_DELIVERED: ResultsDeliveredType[] = [
+export const RESULTS_DELIVERED: ResultsDeliveredType[] = [
   { title: "Global Team", value: "120" },
   { title: "Store Launched", value: "250" },
-  { title: "Revenue Empowered", value: "$8" },
-  { title: "Years of Experience", value: "1" },
+
+  { title: "Revenue Empowered", value: "$8 B" },
+  { title: "Years of Experience", value: "13" },
   { title: "Awards Won", value: "6" },
 ];
