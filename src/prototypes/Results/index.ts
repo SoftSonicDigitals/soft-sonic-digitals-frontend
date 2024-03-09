ee9f31ea4b0bce338431d161c3a64fbab0889f44
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-09 20:54:55 +1100

    [WEB-13] Adding types and prototypes for results

---
+++ src/prototypes/Results/index.ts
@@ -0,0 +1,9 @@
+import { ResultsDeliveredType } from "@/models/Results";
+
+const RESULTS_DELIVERED: ResultsDeliveredType[] = [
+  { title: "Global Team", value: "120" },
+  { title: "Store Launched", value: "250" },
+  { title: "Revenue Empowered", value: "$8" },
+  { title: "Years of Experience", value: "1" },
+  { title: "Awards Won", value: "6" },
+];
