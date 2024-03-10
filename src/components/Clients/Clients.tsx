dd2999079059544464b2a67e4cd953517c490f1b
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-10 17:30:20 +1100

    [WEB-14] Added Clients Component

---
+++ src/components/Clients/Clients.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Clients = () => {
+  return (
+    <section id="clients">
+      <div className="wrapper_container">Clients</div>
+    </section>
+  );
+};
+
+export default Clients;
