24e3f06dd6ad2a1ede0557d10b9e3865d9282e9f
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-11 00:52:42 +1100

    [WEB-14] added ruled grid using regular css

--- src/components/Clients/Clients.tsx
+++ src/components/Clients/Clients.tsx
@@ -5,11 +5,13 @@ import React from "react";
 const Clients = () => {
   return (
     <section id="clients">
-      <div className="wrapper_container">
-        <div className="grid grid-cols-4">
+      <div className="wrapper_container mb-24">
+        <div className="rule_grid">
           {CLIENTS.map(({ id, imgPath }) => (
-            <div key={id} className="relative w-[288px] h-[88px] m-6">
-              <Image fill src={`/clients/${imgPath}`} alt="client logo" />
+            <div key={id} className="p-6 card">
+              <div className="relative w-[288px] h-[88px] ">
+                <Image fill src={`/clients/${imgPath}`} alt="client logo" />
+              </div>
             </div>
           ))}
         </div>
