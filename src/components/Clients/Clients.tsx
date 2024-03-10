5e57aa41e7497ecbe0c42f134b5a101240b599cc
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-10 17:50:43 +1100

    [WEB-14] added the logo template formapping

--- src/components/Clients/Clients.tsx
+++ src/components/Clients/Clients.tsx
@@ -1,9 +1,14 @@
+import Image from "next/image";
 import React from "react";
 
 const Clients = () => {
   return (
     <section id="clients">
-      <div className="wrapper_container">Clients</div>
+      <div className="wrapper_container">
+        <div className="relative w-[288px] h-[88px] m-6">
+          <Image fill src="/clients/neom.pngw3.webp" alt="client logo" />
+        </div>
+      </div>
     </section>
   );
 };
