d4cd3afc6d90474dcab5dccfe91d864ad5cf824c
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-12 01:18:18 +1100

    [WEB-14] made the rule grid responsive

--- src/components/Clients/Clients.tsx
+++ src/components/Clients/Clients.tsx
@@ -5,14 +5,23 @@ import React from "react";
 const Clients = () => {
   return (
     <section id="clients">
-      <div className="wrapper_container mb-24">
-        <div className="rule_grid">
+      <div className="wrapper_container mb-24 md:px-16 lg:px-0 2xl:px-16 3xl:px-[2.8rem]">
+        <div className="rule_grid grid-cols-4">
           {CLIENTS.map(({ id, imgPath }) => (
-            <div key={id} className="p-6 card">
-              <div className="relative w-[288px] h-[88px] ">
-                <Image fill src={`/clients/${imgPath}`} alt="client logo" />
-              </div>
+            // <div key={id} className="card w-[88px] h-[44px]">
+            //   <div className="relative w-full h-full ">
+            <div key={id} className="card py-3 md:py-4">
+              <Image
+                width={288}
+                height={88}
+                src={`/clients/${imgPath}`}
+                alt="client logo"
+                layout="responsive"
+                style={{ objectFit: "contain" }}
+              />
             </div>
+            //   </div>
+            // </div>
           ))}
         </div>
       </div>
