094d90bf8974785addcbc381f24f94bdc118163f
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-12 01:35:26 +1100

    [WEB-14] fixed the props issue with client after the adjustment with prototypes

--- src/components/Clients/Clients.tsx
+++ src/components/Clients/Clients.tsx
@@ -7,12 +7,12 @@ const Clients = () => {
     <section id="clients">
       <div className="wrapper_container mb-24 md:px-16 lg:px-0 2xl:px-16 3xl:px-[2.8rem]">
         <div className="rule_grid grid grid-cols-4 overflow-hidden">
-          {CLIENTS.map(({ id, imgPath }) => (
+          {CLIENTS.map(({ id, imgName }) => (
             <div key={id} className="card py-3 md:py-4">
               <Image
                 width={288}
                 height={88}
-                src={`/clients/${imgPath}`}
+                src={`/clients/${imgName}`}
                 alt="client logo"
                 style={{ objectFit: "contain" }}
               />
