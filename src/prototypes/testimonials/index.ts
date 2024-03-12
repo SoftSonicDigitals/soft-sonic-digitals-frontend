35808e7d37bf9876a225896c1a93feb67cb63e88
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-12 15:22:29 +1100

    [WEb-15] added prototypes for testimonials

---
+++ src/prototypes/testimonials/index.ts
@@ -0,0 +1,52 @@
+const TESTIMONIALS = [
+  {
+    name: "Max Herber",
+    position: "Badkamermeudel Outlet, Owner",
+    thumbnail: "testimonial-1.webp",
+    videoLink:
+      "https://www.youtube.com/watch?v=C_UTN096Z20&ab_channel=MagnetoITSolutions",
+  },
+  {
+    name: "Hannah Roberts",
+    position: "Marketing Manager at Diamond Boutique",
+    thumbnail: "testimonial-2.webp",
+    videoLink:
+      "https://www.youtube.com/watch?v=C_UTN096Z20&ab_channel=MagnetoITSolutions",
+  },
+
+  {
+    name: "Monica Sawyer",
+    position: "Owner at V P Wedding Planner",
+    thumbnail: "testimonial-3.webp",
+    videoLink:
+      "https://www.youtube.com/watch?v=2PGMgbbMjw4&ab_channel=GoodFirms",
+  },
+  {
+    name: "Paul Daun",
+    position: "President of Diyi Technology",
+    thumbnail: "testimonial-4.webp",
+    videoLink:
+      "https://www.youtube.com/watch?v=OIdOp0-vBf4&ab_channel=MagnetoITSolutions",
+  },
+  {
+    name: "Olu Obasanjo",
+    position: "Shoptroniks, LLC",
+    thumbnail: "testimonial-5.webp",
+    videoLink:
+      "https://www.youtube.com/watch?v=tUWVSawNcJk&ab_channel=MagnetoITSolutions",
+  },
+  {
+    name: "Evelyne DjamatDubois",
+    position: "Marketing Analyst at Glazers Wholesale Distributors",
+    thumbnail: "testimonial-6.webp",
+    videoLink:
+      "https://www.youtube.com/watch?v=6kkb2S8Z9tw&ab_channel=MagnetoITSolutions",
+  },
+  {
+    name: "Jessica taylor",
+    position: "Job Recruitment Portal",
+    thumbnail: "testimonial-7.webp",
+    videoLink:
+      "https://www.youtube.com/watch?v=V_X97F8-mlY&ab_channel=MagnetoITSolutions",
+  },
+];
