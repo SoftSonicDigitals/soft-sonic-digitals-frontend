f6502585b9494fd67a7690c995aaa2e98b98cc0f
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-14 19:24:24 +1100

    [WEB-16] added styling to headings

--- src/components/CaseStudies/CaseStudies.tsx
+++ src/components/CaseStudies/CaseStudies.tsx
@@ -4,9 +4,11 @@ const CaseStudies = () => {
   return (
     <section id="case_studies">
       <div className="wrapper_container">
-        <div>
-          <h1>Case studies & stories</h1>
-          <p>
+        <div className="flex-center flex-col mb-12 gap-4">
+          <h1 className="text-2xl md:text-[2.1rem] font-[500] mx-4 ">
+            Case studies & stories
+          </h1>
+          <p className="text-xl font-[500] text-gray-800">
             As a digital commerce development agency, we work together across
             the globe to make a difference
           </p>
