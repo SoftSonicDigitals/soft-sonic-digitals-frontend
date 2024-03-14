463725a6648cdf28c12e05f2e5fd9382f4dcf203
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-14 22:30:07 +1100

    [WEB-16] added constants for case studies

---
+++ src/constants/case_studies/index.ts
@@ -0,0 +1,3 @@
+export const CASE_STUDIES_HEADING = "Case studies & stories";
+export const CASE_STUDIES_SUBHEADING =
+  "As a digital commerce development agency, we work together across the globe to make a difference";
