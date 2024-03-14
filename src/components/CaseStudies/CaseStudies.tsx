745570925400bfff4eea0f93a3397ada21589c21
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-14 17:19:21 +1100

    [WEB-16] added a template for case studies

--- src/components/CaseStudies/CaseStudies.tsx
+++ src/components/CaseStudies/CaseStudies.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const CaseStudies = () => {
-  return <div>CaseStudies</div>;
+  return (
+    <section id="case_studies">
+      <div className="wrapper_container">
+        <div>
+          <h1>Case studies & stories</h1>
+          <p>
+            As a digital commerce development agency, we work together across
+            the globe to make a difference
+          </p>
+        </div>
+        <div>Cases</div>
+      </div>
+    </section>
+  );
 };
 
 export default CaseStudies;
