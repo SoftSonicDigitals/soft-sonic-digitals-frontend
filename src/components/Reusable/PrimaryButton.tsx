fc93583803b3a00f305e85aa5241752096c34ac3
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-15 18:14:36 +1100

    [WEB-08] added event handlers to button

--- src/components/Reusable/PrimaryButton.tsx
+++ src/components/Reusable/PrimaryButton.tsx
@@ -1,22 +1,23 @@
+"use client";
 import React from "react";
-
 type ButtonProps = {
   title: string;
   className?: string;
   type?: "button" | "submit" | "reset" | undefined;
-  handleClick?: () => void;
+  clickHandler?: () => void;
 };
 
 const PrimaryButton = ({
   type,
   title,
   className,
-  handleClick,
+  clickHandler,
 }: ButtonProps) => {
   return (
     <button
       type={type}
       className={`text-white tracking-wider btn__backgroundcolor   font-[700] rounded  ${className} `}
+      onClick={clickHandler}
     >
       {title}
     </button>
