9111c80f15766570ea1d356f1fda2e972fd81099
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-15 18:08:13 +1100

    [WEB-08] added route for contacts

---
+++ src/app/(sites)/contact/page.tsx
@@ -0,0 +1,9 @@
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const Contact = () => {
+  const router = useRouter();
+  return <div> Contact page</div>;
+};
+
+export default Contact;
