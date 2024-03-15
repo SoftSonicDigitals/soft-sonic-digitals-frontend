0cdbc74a6aa10fcb3c47a75e4f94adfc7def708d
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-15 14:32:04 +1100

    [WEB-20] refactored the constants for the digital platforms

--- src/components/DigitalPlatforms/DigitalPlatformInfo.tsx
+++ src/components/DigitalPlatforms/DigitalPlatformInfo.tsx
@@ -1,19 +1,17 @@
+import {
+  DIGITAL_PLATFORM_DESCRIPTION,
+  DIGITAL_PLATFORM_HEADING,
+} from "@/constants/digital_platforms";
 import React from "react";
 
 const DigitalPlatformInfo = () => {
   return (
     <div className="flex flex-col gap-6 mb-4 px-5 md:px-6 2xl:px-8 pt-8 lg:pt-16">
       <h1 className="text-2xl lg:text-4xl font-[500]  ">
-        Digital Commerce Platform
+        {DIGITAL_PLATFORM_HEADING}
       </h1>
-      <p className="text-[1rem] lg:text-xl font-[500] text-gray-800 ">
-        We will recommend and propose a digital commerce platform that is most
-        aligned with your business vision and objectives.
-      </p>
-      <p className="text-[1rem] lg:text-xl font-[500] text-gray-800">
-        When you’ve reached a crossroads and are unsure what to do next, we will
-        use data-informed decisions that turn your transactional experience into
-        a journey driving absolute customer value
+      <p className="text-[1rem] lg:text-xl font-[500] text-gray-800 whitespace-pre-line ">
+        {DIGITAL_PLATFORM_DESCRIPTION}
       </p>
     </div>
   );
---
+++ src/constants/digital_platforms/index.ts
@@ -0,0 +1,3 @@
+export const DIGITAL_PLATFORM_HEADING = "Digital Commerce Platform";
+export const DIGITAL_PLATFORM_DESCRIPTION =
+  "We will recommend and propose a digital commerce platform that is most aligned with your business vision and objectives.\n\n When you’ve reached a crossroads and are unsure what to do next, we will use data-informed decisions that turn your transactional experience into a journey driving absolute customer value";
