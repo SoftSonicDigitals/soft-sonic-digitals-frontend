8b02370b12b54db544a14bfd19d99992633fca51
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-16 21:47:58 +1100

    [WEB-21] added type for awards

--- src/prototypes/awards/index.ts
+++ src/prototypes/awards/index.ts
@@ -1,31 +1,33 @@
-const AWARDS = [
+import { AwardsType } from "@/models/awards";
+
+const AWARDS: AwardsType = [
   {
-    id: 1,
+    id: "1",
     title: "Top eCommerce Development Agency 2023",
     imageName: "award-1.webp",
   },
   {
-    id: 2,
+    id: "2",
     title: "Leaders in B2B eCommerce Solutions 2023",
     imageName: "award-1.webp",
   },
   {
-    id: 3,
+    id: "3",
     title: "Magneto Developer Excellence Award 2023",
     imageName: "award-1.webp",
   },
   {
-    id: 4,
+    id: "4",
     title: "Web Development Leaders in Saudi Arabia 2022",
     imageName: "award-1.webp",
   },
   {
-    id: 5,
+    id: "5",
     title: "Leaders in Web Development Award 2021",
     imageName: "award-1.webp",
   },
   {
-    id: 6,
+    id: "6",
     title: "Technology Leaders in Digital Commerce 2021",
     imageName: "award-1.webp",
   },
