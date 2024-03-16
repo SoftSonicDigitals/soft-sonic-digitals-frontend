7d69f03ad1cc5ae93c30d5c4613e5062c6783329
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-16 21:45:25 +1100

    [WEB-21] added the awards array

---
+++ src/prototypes/awards/index.ts
@@ -0,0 +1,32 @@
+const AWARDS = [
+  {
+    id: 1,
+    title: "Top eCommerce Development Agency 2023",
+    imageName: "award-1.webp",
+  },
+  {
+    id: 2,
+    title: "Leaders in B2B eCommerce Solutions 2023",
+    imageName: "award-1.webp",
+  },
+  {
+    id: 3,
+    title: "Magneto Developer Excellence Award 2023",
+    imageName: "award-1.webp",
+  },
+  {
+    id: 4,
+    title: "Web Development Leaders in Saudi Arabia 2022",
+    imageName: "award-1.webp",
+  },
+  {
+    id: 5,
+    title: "Leaders in Web Development Award 2021",
+    imageName: "award-1.webp",
+  },
+  {
+    id: 6,
+    title: "Technology Leaders in Digital Commerce 2021",
+    imageName: "award-1.webp",
+  },
+];
