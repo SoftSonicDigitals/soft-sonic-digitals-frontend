c6b95c40f0cae72248341d0332b20216fae71ffc
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-16 21:36:03 +1100

    [WEB-21] added awards component

---
+++ src/components/Awards/Awards.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Awards = () => {
+  return (
+    <section id="awards">
+      <div className="wrapper_container">
+        <h1>Awards & Recognitions</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Awards;
