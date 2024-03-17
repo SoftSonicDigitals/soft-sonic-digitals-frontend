2e4201dbe7413a18cc47e8f64409b33d356c7074
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-17 21:12:18 +1100

    [WEB-22] roughly refactored the headings

--- src/components/Consultation/Consultation.tsx
+++ src/components/Consultation/Consultation.tsx
@@ -3,8 +3,15 @@ import React from "react";
 const Consultation = () => {
   return (
     <section id="consultation">
-      <div className="wrapper_container">
-        <div>Headign</div>
+      <div className="wrapper_container flex-center flex-col">
+        <div>
+          <h1 className="text-2xl md:text-[2.1rem] font-[500] mx-4 text-center">
+            How can we help you?
+          </h1>
+          <p className="text-[1rem] md:text-xl font-[500] text-gray-800 text-center">
+            Get all digital commerce services under one roof.
+          </p>
+        </div>
         <div>Cards</div>
       </div>
     </section>
