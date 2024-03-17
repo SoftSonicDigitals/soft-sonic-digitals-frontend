e3858ef532aa6bc2efebf1193a5ae813dbaac33f
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-17 22:55:44 +1100

    [WEB-22] styled and rendered the info for the cards

--- src/components/Consultation/Consultation.tsx
+++ src/components/Consultation/Consultation.tsx
@@ -1,16 +1,48 @@
 import React from "react";
 import { PrimaryStyledHeadings } from "../Reusable";
+import { MAIN_HEADING, SUB_HEADING } from "@/constants/consultation";
+import { CONSULTATION } from "@/prototypes/consultation";
+import Image from "next/image";
+import Link from "next/link";
 
 const Consultation = () => {
   return (
     <section id="consultation">
-      <div className="wrapper_container flex-center flex-col">
+      <div className="wrapper_container flex-center flex-col mb-24">
         <PrimaryStyledHeadings
-          mainHeading="How can we help you?"
-          subHeading="Get all digital commerce services under one roof."
+          mainHeading={MAIN_HEADING}
+          subHeading={SUB_HEADING}
           mainHeadingStyles="mx-4 "
         />
-        <div>Cards</div>
+        <div className="flex-center flex-col gap-16 ">
+          {CONSULTATION.map(({ id, title, description, imageName }) => (
+            <div
+              key={id}
+              className="flex flex-col gap-4 max-w-[680px]  hover:shadow-lg "
+            >
+              <div className="relative ">
+                <Image
+                  src={`/consultation/${imageName}`}
+                  alt={"team member"}
+                  fill
+                  style={{ objectFit: "contain" }}
+                  className="mb-8 max-h-[377px]"
+                />
+              </div>
+              <div className="px-4 pb-6 flex flex-col gap-6 shadow-md">
+                <h3 className=" text-2xl font-[600] text-gray-800">{title}</h3>
+                <p className="">{description}</p>
+
+                <button
+                  type="button"
+                  className="rounded border-2 px-8 py-2 border-gray-800 tracking-wider"
+                >
+                  <Link href={"/"}>{"LET'S TALK"}</Link>
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
--- src/prototypes/consultation/index.ts
+++ src/prototypes/consultation/index.ts
@@ -6,20 +6,20 @@ export const CONSULTATION: ConsultationType = [
     title: "Need consultation?",
     description:
       "If you want to have a jargon-free tech conversation with someone who speaks your language then drop me a message. I can help you find the right technical requirements for your product.",
-    imageName: "team-member-1",
+    imageName: "team-member-1.webp",
   },
   {
     id: "2",
     title: "Need a team?",
     description:
       "Need a dynamic team of digital commerce experts? Then you should talk to me. I’d be glad to discuss needs with you & help you build a team of top talent to help business reach its potential.",
-    imageName: "team-member-2",
+    imageName: "team-member-2.webp",
   },
   {
     id: "3",
     title: "Building an MVP?",
     description:
       "Looking to build fast and reduce your time to market? Drop me a message. Together we can find the core set of features to gather user information and iteratively create the best possible product.",
-    imageName: "team-member-3",
+    imageName: "team-member-3.webp",
   },
 ];
