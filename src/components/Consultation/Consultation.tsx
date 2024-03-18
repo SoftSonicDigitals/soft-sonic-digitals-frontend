a69dff889c074026f55c44bc547dab60af3066be
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-18 21:35:08 +1100

    [WEB-22] added shadows to the card

--- src/components/Consultation/Consultation.tsx
+++ src/components/Consultation/Consultation.tsx
@@ -21,7 +21,7 @@ const Consultation = () => {
           {CONSULTATION.map(({ id, title, description, imageName }) => (
             <div
               key={id}
-              className=" flex-col gap-4   hover:shadow-lg basis-full md:max-w-[325px] lg:max-w-[435px] 2xl:max-w-[375px] "
+              className=" flex-col gap-4    basis-full md:max-w-[325px] lg:max-w-[435px] 2xl:max-w-[375px] card_shadow  "
             >
               <div className="relative w-full max-w-3xl">
                 <Image
@@ -32,7 +32,7 @@ const Consultation = () => {
                   style={{ objectFit: "contain" }}
                 />
               </div>
-              <div className="px-5 md:px-8 pb-6 flex flex-col gap-6 shadow-md mt-8 text-gray-800">
+              <div className="px-5 md:px-8 pb-6 flex flex-col gap-6 card_bottom_shadow pt-8 text-gray-800">
                 <h3 className="text-lg sm:text-xl md:text-2xl font-[700] text-gray-800 tracking-wide">
                   {title}
                 </h3>
