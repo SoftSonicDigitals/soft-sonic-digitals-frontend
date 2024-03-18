0050757d581c5e54659fd02a34aef2d85f2d0a4b
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-18 22:09:02 +1100

    [WEB-22] adjusted the primary styled heading component

--- src/components/ResultsDelivered/ResultsDelivered.tsx
+++ src/components/ResultsDelivered/ResultsDelivered.tsx
@@ -3,6 +3,7 @@ import { RESULTS_DELIVERED } from "@/prototypes/results";
 import React from "react";
 import { ResultsDeliveredItem } from ".";
 import { RESULTS_DELIVERED_HEADING } from "@/constants/results";
+import { PrimaryStyledHeadings } from "../Reusable";
 
 const ResultsDelivered = () => {
   return (
@@ -13,6 +14,7 @@ const ResultsDelivered = () => {
             {RESULTS_DELIVERED_HEADING}
           </h1>
         </div>
+        {/* <PrimaryStyledHeadings mainHeading={RESULTS_DELIVERED_HEADING} /> */}
         <div className="  grid grid-cols-2  gap-4 lg:gap-0 md:flex item-center justify-center md:flex-wrap md:px-10 lg:px-0">
           {RESULTS_DELIVERED.map(({ title, value }) => (
             <ResultsDeliveredItem key={title} title={title} value={value} />
