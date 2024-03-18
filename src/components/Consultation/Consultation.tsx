682771c9f749664ac8a5bd701f2562e33efed88a
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-18 21:13:49 +1100

    [WEB-22] made the cards responsive

--- src/components/Consultation/Consultation.tsx
+++ src/components/Consultation/Consultation.tsx
@@ -14,30 +14,35 @@ const Consultation = () => {
           subHeading={SUB_HEADING}
           mainHeadingStyles="mx-4 "
         />
-        <div className="flex-center flex-col gap-16 ">
+        <div
+          className="grid  grid-cols-1
+          md:grid-cols-2 xl:grid-cols-3 gap-12  xl:mx-4 2xl:mx-12 px-4 "
+        >
           {CONSULTATION.map(({ id, title, description, imageName }) => (
             <div
               key={id}
-              className="flex flex-col gap-4 max-w-[680px]  hover:shadow-lg "
+              className=" flex-col gap-4   hover:shadow-lg basis-full md:max-w-[325px] lg:max-w-[435px] 2xl:max-w-[375px] "
             >
-              <div className="relative ">
+              <div className="relative w-full max-w-3xl">
                 <Image
                   src={`/consultation/${imageName}`}
                   alt={"team member"}
-                  fill
+                  width={700}
+                  height={705}
                   style={{ objectFit: "contain" }}
-                  className="mb-8 max-h-[377px]"
                 />
               </div>
-              <div className="px-4 pb-6 flex flex-col gap-6 shadow-md">
-                <h3 className=" text-2xl font-[600] text-gray-800">{title}</h3>
-                <p className="">{description}</p>
+              <div className="px-5 md:px-8 pb-6 flex flex-col gap-6 shadow-md mt-8 text-gray-800">
+                <h3 className="text-lg sm:text-xl md:text-2xl font-[700] text-gray-800 tracking-wide">
+                  {title}
+                </h3>
+                <p className="tracking-wider mb-2 font-[500]">{description}</p>
 
                 <button
                   type="button"
-                  className="rounded border-2 px-8 py-2 border-gray-800 tracking-wider"
+                  className="rounded border-2 px-8 py-2 border-gray-800 tracking-wider  font-[700]"
                 >
-                  <Link href={"/"}>{"LET'S TALK"}</Link>
+                  <Link href={"/contact"}>{"LET'S TALK"}</Link>
                 </button>
               </div>
             </div>
