39660b20ef1d3873f5f3d14ba13bbd9cca18b257
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-18 22:04:59 +1100

    [WEB-22] refactored case studies

--- src/components/CaseStudies/CaseStudies.tsx
+++ src/components/CaseStudies/CaseStudies.tsx
@@ -1,7 +1,7 @@
 import { CASE_STUDIES } from "@/prototypes/case_studies";
 import React from "react";
 import { CaseStudy } from ".";
-import { PrimaryButton } from "../Reusable";
+import { PrimaryButton, PrimaryStyledHeadings } from "../Reusable";
 import {
   CASE_STUDIES_HEADING,
   CASE_STUDIES_SUBHEADING,
@@ -11,14 +11,11 @@ const CaseStudies = () => {
   return (
     <section id="case_studies">
       <div className="wrapper_container mb-24">
-        <div className="flex-center flex-col mb-16 gap-4 px-5 md:px-8 ">
-          <h1 className="text-2xl md:text-[2.1rem] font-[500] mx-4 ">
-            {CASE_STUDIES_HEADING}
-          </h1>
-          <p className="text-[1rem] md:text-xl font-[500] text-gray-800 text-center">
-            {CASE_STUDIES_SUBHEADING}
-          </p>
-        </div>
+        <PrimaryStyledHeadings
+          mainHeading={CASE_STUDIES_HEADING}
+          subHeading={CASE_STUDIES_SUBHEADING}
+          mainHeadingStyles="mx-4"
+        />
         <div className="flex-center mb-16 ">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-4 items-center justify-center max-w-[1300px] md:px-[2%] lg:px-10 xl:px-6 2xl:p-0 2xl:gap-x-8 2xl:gap-y-0">
             {CASE_STUDIES.map(({ title, link, imageName, id }, index) => (
--- src/components/Consultation/Consultation.tsx
+++ src/components/Consultation/Consultation.tsx
@@ -24,7 +24,7 @@ const Consultation = () => {
               title={title}
               description={description}
               imageName={imageName}
-            ></ConsultationCard>
+            />
           ))}
         </div>
       </div>
