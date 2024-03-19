e94304f03b75cbbdefee34483694d91628325f87
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-19 15:50:36 +1100

    [WEB-23] rendered the headings for the reviews

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { PrimaryStyledHeadings } from "../Reusable";
+import { REVIEWS_MAIN_HEADING, REVIEWS_SUB_HEADING } from "@/constants/reviews";
 
 const Reviews = () => {
   return (
     <section id="reviews">
-      <div className="wrapper_container">Reviews</div>
+      <div className="wrapper_container">
+        <PrimaryStyledHeadings
+          mainHeading={REVIEWS_MAIN_HEADING}
+          subHeading={REVIEWS_SUB_HEADING}
+        />
+      </div>
     </section>
   );
 };
--- src/constants/reviews/index.ts
+++ src/constants/reviews/index.ts
@@ -1,3 +1,4 @@
-export const mainHeading = "We are trusted by people all across the globe";
-export const subHeading =
+export const REVIEWS_MAIN_HEADING =
+  "We are trusted by people all across the globe";
+export const REVIEWS_SUB_HEADING =
   "Check out what our customers have to say about our digital commerce management services";
