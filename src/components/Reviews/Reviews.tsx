15e0ca97d875f1c892649913e33f116165b0f659
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-19 15:58:51 +1100

    [WEB-23] stylde headings for this section

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -4,11 +4,14 @@ import { REVIEWS_MAIN_HEADING, REVIEWS_SUB_HEADING } from "@/constants/reviews";
 
 const Reviews = () => {
   return (
-    <section id="reviews">
-      <div className="wrapper_container">
+    <section id="reviews" className="bg-gray-0">
+      <div className="wrapper_container py-24 mb-24">
         <PrimaryStyledHeadings
           mainHeading={REVIEWS_MAIN_HEADING}
           subHeading={REVIEWS_SUB_HEADING}
+          mainHeadingStyles="mb-10"
+          subHeadingStyles="!font-[400]"
+          containerStyles="mb-10"
         />
       </div>
     </section>
