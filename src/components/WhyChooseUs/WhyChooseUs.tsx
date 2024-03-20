0c9bf45a3bdd6776637ba4f3664581e1c54d28b1
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-20 22:50:11 +1100

    [WEB-24] refactored the component

--- src/components/WhyChooseUs/WhyChooseUs.tsx
+++ src/components/WhyChooseUs/WhyChooseUs.tsx
@@ -1,23 +1,13 @@
 import Image from "next/image";
 import React from "react";
+import { WhyChooseUsHeadingInfo } from ".";
 
 const WhyChooseUs = () => {
   return (
     <section id="why_choose_us">
       <div className="wrapper_container mb-24 flex-center">
         <div className="grid items-center justify-center grid-cols-1 lg:grid-cols-2 2xl:gap-x-6  md:px-8 lg:px-0  2xl:pl-[6%] 2xl:pr-[4%] max-w-[750px] lg:max-w-max">
-          <div className="flex flex-col gap-6 mb-8 lg:mb-0 px-5 md:px-0 lg:px-6 2xl:px-8 pt-8 lg:pt-16 text-center lg:text-left  ">
-            <h1 className="text-2xl md:text-4xl font-[500]  ">
-              Why choose us?
-            </h1>
-            <p className="text-[1rem] md:text-xl font-[500] text-gray-800 whitespace-pre-line  ">
-              A team of digital commerce experts working with manufacturers,
-              wholesalers, and retailers globally, turning ideas into unique
-              customer & content-centric experiences using a platform-agnostic
-              approach. <br /> <br /> We Push Ourselves. We Push Technology & We
-              Push the Boundaries of Conventional Commerce.
-            </p>
-          </div>
+          <WhyChooseUsHeadingInfo />
           <div className="md:row-span-2">
             <Image
               src="/why_choose_us/Why-choose-us.webp"
---
+++ src/components/WhyChooseUs/WhyChooseUsHeadingInfo.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const WhyChooseUsHeadingInfo = () => {
+  return (
+    <div className="flex flex-col gap-6 mb-8 lg:mb-0 px-5 md:px-0 lg:px-6 2xl:px-8 pt-8 lg:pt-16 text-center lg:text-left  ">
+      <h1 className="text-2xl md:text-4xl font-[500]  ">Why choose us?</h1>
+      <p className="text-[1rem] md:text-xl font-[500] text-gray-800 whitespace-pre-line  ">
+        A team of digital commerce experts working with manufacturers,
+        wholesalers, and retailers globally, turning ideas into unique customer
+        & content-centric experiences using a platform-agnostic approach. <br />{" "}
+        <br /> We Push Ourselves. We Push Technology & We Push the Boundaries of
+        Conventional Commerce.
+      </p>
+    </div>
+  );
+};
+
+export default WhyChooseUsHeadingInfo;
