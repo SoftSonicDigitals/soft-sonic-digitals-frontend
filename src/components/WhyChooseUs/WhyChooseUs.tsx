ed10480eaff5909e689b8053d036dd2d3fdecafd
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-20 20:48:23 +1100

    [WEB-24] roughly rendered the elements

--- src/components/WhyChooseUs/WhyChooseUs.tsx
+++ src/components/WhyChooseUs/WhyChooseUs.tsx
@@ -1,7 +1,36 @@
+import Image from "next/image";
 import React from "react";
 
 const WhyChooseUs = () => {
-  return <div>WhyChooseUs</div>;
+  return (
+    <section id="why_choose_us">
+      <div className="wrapper_container mb-24">
+        <div className="grid grid-cols-1 lg:grid-cols-[1fr,1fr]  py-16 md:px-8  lg:px-0 max-w-[750px] lg:max-w-max">
+          <div className="flex flex-col gap-6 mb-4 px-5 md:px-6 2xl:px-8 pt-8 lg:pt-16">
+            <h1 className="text-2xl lg:text-4xl font-[500]  ">
+              Why choose us?
+            </h1>
+            <p className="text-[1rem] lg:text-xl font-[500] text-gray-800 whitespace-pre-line ">
+              A team of digital commerce experts working with manufacturers,
+              wholesalers, and retailers globally, turning ideas into unique
+              customer & content-centric experiences using a platform-agnostic
+              approach. <br /> <br /> We Push Ourselves. We Push Technology & We
+              Push the Boundaries of Conventional Commerce.
+            </p>
+          </div>
+          <div>
+            <Image
+              src="/why_choose_us/Why-choose-us.webp"
+              alt="image"
+              width={602}
+              height={516}
+              style={{ objectFit: "contain" }}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default WhyChooseUs;
