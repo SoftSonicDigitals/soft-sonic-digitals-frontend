df4e868a75887be66364cd6590d78f5e07d2fc1d
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-20 23:02:03 +1100

    [WEB-24] added array of reasons, types and mapped over it

---
+++ src/prototypes/why_choose_us/index.ts
@@ -0,0 +1,8 @@
+export const CHOOSING_REASON: string[] = [
+  "13+ Yrs Experience",
+  "Competitive Price",
+  "250+ Stores Developed",
+  "Strong Deployment Team",
+  "Certified Developers",
+  "Agile Method Approach",
+];
