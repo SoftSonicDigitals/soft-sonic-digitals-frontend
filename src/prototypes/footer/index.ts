7fc34b59c61fcc330ad70e67cc691aaf1a68e40b
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-22 19:51:34 +1100

    [WEB-27] added aray of fotter links

---
+++ src/prototypes/footer/index.ts
@@ -0,0 +1,58 @@
+export const SOCIALS = [{}];
+
+export const FOOTER_LINKS = [
+  {
+    title: "QUICK LINKS",
+    links: [
+      { title: "Company", link: "/" },
+      { title: "Referrals", link: "/" },
+      { title: "Write For Us", link: "/" },
+      { title: "Career", link: "/" },
+      { title: "Partners", link: "/" },
+      { title: "Contact", link: "/" },
+      { title: "Privacy Policy", link: "/" },
+    ],
+  },
+  {
+    title: "EXPERTISE",
+    links: [
+      { title: "B2C eCommerce", link: "/" },
+      { title: "B2B eCommerce", link: "/" },
+      { title: "Marketplace", link: "/" },
+      { title: "Headless Commerce", link: "/" },
+      { title: "Mobile Commerce", link: "/" },
+    ],
+  },
+  {
+    title: "SERVICES",
+    links: [
+      { title: "Consulting", link: "/" },
+      { title: "Design (UI/UX)", link: "/" },
+      { title: "Development", link: "/" },
+      { title: "Integration", link: "/" },
+      { title: "Support", link: "/" },
+      { title: "CRO", link: "/" },
+    ],
+  },
+  {
+    title: "PLATFORM",
+    links: [
+      { title: "Magento", link: "/" },
+      { title: "OroCommerce", link: "/" },
+      { title: "Pimcore", link: "/" },
+      { title: "Commercetools", link: "/" },
+      { title: "Fabric", link: "/" },
+    ],
+  },
+  {
+    title: "WEB",
+    links: [
+      { title: "Digital Transformation", link: "/" },
+      { title: "Software Development", link: "/" },
+      { title: "Symfony Development", link: "/" },
+      { title: "Node Js Development", link: "/" },
+      { title: "React Js Development", link: "/" },
+      { title: "Laravel Development", link: "/" },
+    ],
+  },
+];
