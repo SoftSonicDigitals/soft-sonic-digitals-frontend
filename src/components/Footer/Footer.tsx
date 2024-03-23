d5c17a09afa4f702c134a618cd5eef38661887fc
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-23 19:47:36 +1100

    [WEB-27] rendered the mobile view

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,8 @@
 import { FOOTER_LINKS } from "@/prototypes/footer";
 import React from "react";
 import { FooterLinks, Socials } from ".";
+import { FaPlus } from "react-icons/fa6";
+import Link from "next/link";
 
 const Footer = () => {
   return (
@@ -11,14 +13,26 @@ const Footer = () => {
             <FooterLinks key={index} title={title} links={links} />
           ))}
         </div>
+
         <div className="flex flex-col md:hidden w-full">
           {FOOTER_LINKS.map(({ title, links }, index) => (
-            <p
-              key={index}
-              className=" px-4 py-2.5 border-t-[1px] last:border-b-[1px] border-gray-200 text-sm capitalize"
-            >
-              {title}
-            </p>
+            <div key={index}>
+              <div className="flex items-center justify-between border-y-[1px] last:border-b-[1px] border-gray-200 px-4 py-2.5">
+                <p className="text-sm capitalize">{title}</p>
+                <FaPlus className="text-xs text-gray-700" />
+              </div>
+              <div className="flex flex-col gap-2.5 px-4 py-2.5">
+                {links.map(({ title, link }, index) => (
+                  <Link
+                    href={link}
+                    key={index}
+                    className="text-gray-700 font-[500] text-sm tracking-wide "
+                  >
+                    {title}
+                  </Link>
+                ))}
+              </div>
+            </div>
           ))}
         </div>
         <Socials />
