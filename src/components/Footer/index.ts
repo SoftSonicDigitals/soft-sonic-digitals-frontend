825d32fb96ce2a7099b4591c7dc46b1c3fff4a16
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-23 20:06:35 +1100

    [WEB-27] added states for drop downs

--- src/components/Footer/index.ts
+++ src/components/Footer/index.ts
@@ -1,5 +1,6 @@
 import Footer from "./Footer";
 import FooterLinks from "./FooterLinks";
+import MobileFooter from "./MobileFooter";
 import Socials from "./Socials";
 
-export { Footer, FooterLinks, Socials };
+export { Footer, FooterLinks, Socials, MobileFooter };
