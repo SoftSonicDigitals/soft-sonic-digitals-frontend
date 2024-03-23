6d3f22993601801b60e68579691b9bf6083ebb42
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-23 23:41:42 +1100

    [WEB-28] styled the copyright section

--- src/components/Copyrights/Copyright.tsx
+++ src/components/Copyrights/Copyright.tsx
@@ -3,8 +3,10 @@ import React from "react";
 
 const Copyright = () => {
   return (
-    <div className="wrapper_container flex-center ">
-      <p className="tracking-wide">{COPYRIGHTS_INFO}</p>
+    <div className="wrapper_container flex-center border-t-[1px] border-gray-600 max-w-[1270px] mb-6">
+      <p className="tracking-wide text-gray-700 mt-4 text-sm md:text-base ">
+        {COPYRIGHTS_INFO}
+      </p>
     </div>
   );
 };
