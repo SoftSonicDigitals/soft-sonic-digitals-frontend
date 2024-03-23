72f3d2f792cb89dee8eb574c8308e17a9ac6d55e
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-23 23:43:39 +1100

    [WEB-28] adjusted the stylings

--- src/components/Copyrights/Copyright.tsx
+++ src/components/Copyrights/Copyright.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Copyright = () => {
   return (
     <div className="wrapper_container flex-center border-t-[1px] border-gray-600 max-w-[1270px] mb-6">
-      <p className="tracking-wide text-gray-700 mt-4 text-sm md:text-base ">
+      <p className="tracking-wider text-gray-700 mt-4 text-sm  ">
         {COPYRIGHTS_INFO}
       </p>
     </div>
