a8a3e389efee1808ff4d4c24b7b9c8983d5d230a
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-23 23:43:39 +1100

    [WEB-28] adjusted the stylings

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ import { Copyright } from "../Copyrights";
 const Footer = () => {
   return (
     <footer>
-      <div className="wrapper_container mb-20 flex  flex-col md:flex-row md:justify-around lg:gap-10  max-w-[1270px]">
+      <div className="wrapper_container mb-12 md:mb-20 flex  flex-col md:flex-row md:justify-around lg:gap-10  max-w-[1270px]">
         <div className="hidden md:grid md:grid-cols-3 lg:grid-cols-5 gap-8 lg:gap-5">
           {FOOTER_LINKS.map(({ title, links }, index) => (
             <FooterLinks key={index} title={title} links={links} />
