4cd6d788670af73650b194197d35b56e5e303a1d
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-24 15:17:31 +1100

    [WEB-31] added constants for lets talk section

--- src/components/ContactPage/LetsTalkHeadings.tsx
+++ src/components/ContactPage/LetsTalkHeadings.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { PrimaryStyledHeadings } from "../Reusable";
+import {
+  LETS_TALK_MAIN_HEADING,
+  LETS_TALK_SUB_HEADING,
+} from "@/constants/contact_page";
 
+LETS_TALK_SUB_HEADING;
 const LetsTalkHeadings = () => {
   return (
-    <div className="wrapper_container">
+    <div className="wrapper_container mb-24">
       <PrimaryStyledHeadings
-        mainHeading="Let’s talk"
-        subHeading="Just fill out this simple form and we will contact you promptly to discuss your requirement."
+        mainHeading={LETS_TALK_MAIN_HEADING}
+        subHeading={LETS_TALK_SUB_HEADING}
         containerStyles="mt-20"
         mainHeadingStyles="!text-3xl sm:!text-4xl lg:!text-[40px] xl:!text-5xl  mb-2 xl:mb-5 text-gray-800"
         subHeadingStyles="hidden md:block !text-xl lg:!text-2xl xl:!text-[29px] xl:!leading-9 tracking-wide px-20"
