b68adab5247f2219470100542b4ae723c049bbcd
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-24 14:05:38 +1100

    [WEB-30] made the footer for contact section

--- src/app/(non_layout_pages)/contact/page.tsx
+++ src/app/(non_layout_pages)/contact/page.tsx
@@ -1,17 +1,30 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Contact = () => {
   return (
-    <div>
-      <h1 className="text-5xl ">Contact </h1>
-      <Link
-        href="
-    /"
+    <nav className="shadow-md relative">
+      <div
+        className="wrapper_container flex justify-center 
+       "
       >
-        HOME
-      </Link>
-    </div>
+        <Link href={"/"}>
+          <div
+            className="cursor-pointer relative w-[5.5rem] sm:w-24
+          h-16 my-3"
+          >
+            <Image src="/logo.svg" fill alt="logo" />
+          </div>
+        </Link>
+
+        <Link href={"/"}>
+          <div className="absolute top-[55%] -translate-y-[55%] right-[20px] flex-center cursor-pointer font-[600] uppercase tracking-wider text-[17px] border-b-2 border-black px-1 lg:mx-8">
+            Home
+          </div>
+        </Link>
+      </div>
+    </nav>
   );
 };
 
