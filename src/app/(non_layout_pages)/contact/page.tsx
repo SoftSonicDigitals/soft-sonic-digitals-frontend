979e2ed1f176aefb9df6044bbbbbf9de6bffce01
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-24 15:04:47 +1100

    [WEB-31] added responsive headings to the section

--- src/app/(non_layout_pages)/contact/page.tsx
+++ src/app/(non_layout_pages)/contact/page.tsx
@@ -1,9 +1,18 @@
+import { PrimaryStyledHeadings } from "@/components/Reusable";
 import React from "react";
 
 const Contact = () => {
   return (
     <main>
-      <div className="wrapper_container">heading</div>
+      <div className="wrapper_container">
+        <PrimaryStyledHeadings
+          mainHeading="Let’s talk"
+          subHeading="Just fill out this simple form and we will contact you promptly to discuss your requirement."
+          containerStyles="mt-20"
+          mainHeadingStyles="!text-3xl sm:!text-4xl lg:!text-[40px] xl:!text-5xl  mb-2 xl:mb-5 text-gray-800"
+          subHeadingStyles="hidden md:block !text-xl lg:!text-2xl xl:!text-[29px] xl:!leading-9 tracking-wide px-20"
+        />
+      </div>
     </main>
   );
 };
