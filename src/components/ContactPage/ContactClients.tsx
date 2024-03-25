215ad55815a2f52bc0b7429a9d95303b9e14b15d
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-25 23:51:12 +1100

    [WEB-32] made the section responsive, adjusted the stylings

--- src/components/ContactPage/ContactClients.tsx
+++ src/components/ContactPage/ContactClients.tsx
@@ -4,20 +4,20 @@ import React from "react";
 
 const ContactClients = () => {
   return (
-    <div className="basis-full">
-      <h3 className="text-xl text-center px-20 font-[500] tracking-wider mb-10">
+    <div className=" max-w-[555px]  px-4 md:px-[10%]  lg:pl-8 lg:pr-0 basis-full">
+      <h3 className="text-xl text-center font-[500] tracking-wider mb-10">
         We love helping start-up become brand names and corporations launch new
         products
       </h3>
-      <div className="grid items-center justify-center grid-cols-4 gap-y-8 px-8 2xl:px-16">
+      <div className="grid items-center justify-center grid-cols-4 gap-y-8  ">
         {CLIENTS.map(({ id, imgName }) => (
           <Image
             key={id}
             src={`/clients/${imgName}`}
-            alt={"logo"}
+            alt={"client-logo"}
             width={188}
             height={50}
-            className="py-2"
+            className="py-2 hidden md:block"
           />
         ))}
       </div>
--- src/components/ContactPage/LetsTalkHeadings.tsx
+++ src/components/ContactPage/LetsTalkHeadings.tsx
@@ -9,13 +9,13 @@ LETS_TALK_SUB_HEADING;
 const LetsTalkHeadings = () => {
   return (
     <section id="lets_talk">
-      <div className="wrapper_container mb-24">
+      <div className="wrapper_container  mb-8 lg:mb-24">
         <PrimaryStyledHeadings
           mainHeading={LETS_TALK_MAIN_HEADING}
           subHeading={LETS_TALK_SUB_HEADING}
-          containerStyles="mt-20"
+          containerStyles="mt-20 px-[2rem]"
           mainHeadingStyles="!text-3xl sm:!text-4xl lg:!text-[40px] xl:!text-5xl  mb-2 xl:mb-5 text-gray-800"
-          subHeadingStyles="hidden md:block !text-xl lg:!text-2xl xl:!text-[29px] xl:!leading-9 tracking-wide px-20"
+          subHeadingStyles="hidden md:block !text-xl lg:!text-2xl xl:!text-[29px] xl:!leading-9 tracking-wide 2xl:px-[7%] "
         />
       </div>
     </section>
