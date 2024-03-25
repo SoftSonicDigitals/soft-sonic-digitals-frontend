576887e4667d6fdfe139e8ec6e0a6337b4e5a9e6
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-25 21:08:37 +1100

    [WEB-32] added styled heading

--- src/components/ContactPage/ContactClients.tsx
+++ src/components/ContactPage/ContactClients.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const ContactClients = () => {
-  return <div>ContactClients</div>;
+  return (
+    <div className="basis-full">
+      <h3 className="text-xl text-center px-20 font-[500] tracking-wider">
+        We love helping start-up become brand names and corporations launch new
+        products
+      </h3>
+    </div>
+  );
 };
 
 export default ContactClients;
