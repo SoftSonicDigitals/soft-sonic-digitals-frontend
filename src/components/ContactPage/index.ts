2294b2d1f4de8da600ad341eea4690a0ff0cfdc0
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-26 22:46:28 +1100

    [WEB-33] added testimonial accordian component

--- src/components/ContactPage/index.ts
+++ src/components/ContactPage/index.ts
@@ -1,5 +1,11 @@
 import ContactClients from "./ContactClients";
 import ContactPageNavbar from "./ContactPageNavbar";
 import LetsTalkHeadings from "./LetsTalkHeadings";
+import TestimonialAccordian from "./TestimonialAccordian";
 
-export { LetsTalkHeadings, ContactPageNavbar, ContactClients };
+export {
+  LetsTalkHeadings,
+  ContactPageNavbar,
+  ContactClients,
+  TestimonialAccordian,
+};
