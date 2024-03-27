bb647dc6f8820a17591505a01bfc7bc9f4cb655b
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-27 20:35:29 +1100

    [WEB-33] adjusted the protoptype array

--- src/prototypes/contact_page/index.ts
+++ src/prototypes/contact_page/index.ts
@@ -4,27 +4,27 @@ export const CONTACT_TESTIMONIALS: ContactTestimonialType = [
   {
     testimonial:
       "“Magneto makes it really easy for customers to see the trading signals and act on them.”",
-    profile: "profile-1",
+    profile: "profile-1.png",
     name: "Markus Heitkoetter",
     title: "CEO, Rockwell Trading Services, LLC",
   },
   {
     testimonial: "“They are a joy to work with and they are always positive.”",
-    profile: "profile-2",
+    profile: "profile-2.png",
     name: "Monica McLeod Sawyer",
     title: "Owner of The One",
   },
   {
     testimonial:
       "“During our partnership with Magneto, we’ve continuously refined our goals, grown our business, and clarified our product.”",
-    profile: "profile-3",
+    profile: "profile-3.png",
     name: "Ryan Hyde",
     title: "CEO, RPH Engineering",
   },
   {
     testimonial:
       "“They’re very helpful, they met the deadlines, and they take care of all of my requirements. We are pleased with the deliverables that they receive from Magneto IT Solutions.”",
-    profile: "profile-4",
+    profile: "profile-4.png",
     name: "Ranjith K.K",
     title: "IT Manager, Ashrafs W.L.L",
   },
