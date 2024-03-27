65d2c94055f8f367d0377bed67a4ba4a525f6c84
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-27 20:55:39 +1100

    [WEB-33] installed react slick and made an accordian for testimonials

--- src/components/ContactPage/TestimonialAccordian.tsx
+++ src/components/ContactPage/TestimonialAccordian.tsx
@@ -1,17 +1,36 @@
+"use client";
 import { CONTACT_TESTIMONIALS } from "@/prototypes/contact_page";
 import { TestimonialCard } from ".";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const TestimonialAccordian = () => {
-  return CONTACT_TESTIMONIALS.map(
-    ({ title, profile, name, testimonial }, index) => (
-      <TestimonialCard
-        key={index}
-        title={title}
-        profile={profile}
-        name={name}
-        testimonial={testimonial}
-      />
-    )
+  const settings = {
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 360,
+    autoplaySpeed: 6000,
+    cssEase: "ease-in-out",
+  };
+  return (
+    <div className="slider-container">
+      <Slider {...settings}>
+        {CONTACT_TESTIMONIALS.map(
+          ({ title, profile, name, testimonial }, index) => (
+            <TestimonialCard
+              key={index}
+              title={title}
+              profile={profile}
+              name={name}
+              testimonial={testimonial}
+            />
+          )
+        )}
+      </Slider>
+    </div>
   );
 };
 
