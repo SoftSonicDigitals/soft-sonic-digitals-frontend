043a3eca7853be4a55bfa4fdf3f98e4aae5d3caa
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-27 20:13:40 +1100

    [WEB-33] added styling to the testimonial

--- src/components/ContactPage/TestimonialAccordian.tsx
+++ src/components/ContactPage/TestimonialAccordian.tsx
@@ -5,22 +5,17 @@ import React from "react";
 const testimonial = CONTACT_TESTIMONIALS[0];
 const TestimonialAccordian = () => {
   return (
-    <div>
-      <div className="flex flex-col gap-4 px-16 ">
-        <div className="text-center">{testimonial.testimonial}</div>
-        <div className="flex">
-          <div>
-            <Image
-              src={`/contact_page/profile-1.png`}
-              alt="profile-img"
-              height={64}
-              width={64}
-            />
-          </div>
-          <div>
-            <p>{testimonial.title}</p>
-            <p>{testimonial.name}</p>
-          </div>
+    <div className="flex flex-col gap-10  font-[500] tracking-wider text-gray-700 sm:px-16 md:px-12 ">
+      <div className="text-center px-16 md:px-10">
+        {testimonial.testimonial}
+      </div>
+      <div className="flex-center gap-6 px-10 lg:px-16">
+        <div className="relative h-[64px] min-w-[64px]">
+          <Image src={`/contact_page/profile-2.png`} alt="profile-img" fill />
+        </div>
+        <div className="">
+          <p>{testimonial.title}</p>
+          <p>{testimonial.name}</p>
         </div>
       </div>
     </div>
