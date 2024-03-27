5d9ae7fbd4572ef4fdeb25f645080c778dd88cc9
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-27 19:40:34 +1100

    [WEB-33] added type for the testimonial prototype

--- src/prototypes/contact_page/index.ts
+++ src/prototypes/contact_page/index.ts
@@ -1,32 +1,30 @@
-export const CONTACT_TESTIMONIALS = [
+import { ContactTestimonialType } from "@/models/contact_page";
+
+export const CONTACT_TESTIMONIALS: ContactTestimonialType = [
   {
-    id: 1,
     testimonial:
       "“Magneto makes it really easy for customers to see the trading signals and act on them.”",
-    profileImage: "profile-1",
+    profile: "profile-1",
     name: "Markus Heitkoetter",
     title: "CEO, Rockwell Trading Services, LLC",
   },
   {
-    id: 2,
     testimonial: "“They are a joy to work with and they are always positive.”",
-    profileImage: "profile-2",
+    profile: "profile-2",
     name: "Monica McLeod Sawyer",
     title: "Owner of The One",
   },
   {
-    id: 3,
     testimonial:
       "“During our partnership with Magneto, we’ve continuously refined our goals, grown our business, and clarified our product.”",
-    profileImage: "profile-3",
+    profile: "profile-3",
     name: "Ryan Hyde",
     title: "CEO, RPH Engineering",
   },
   {
-    id: 4,
     testimonial:
       "“They’re very helpful, they met the deadlines, and they take care of all of my requirements. We are pleased with the deliverables that they receive from Magneto IT Solutions.”",
-    profileImage: "profile-4",
+    profile: "profile-4",
     name: "Ranjith K.K",
     title: "IT Manager, Ashrafs W.L.L",
   },
