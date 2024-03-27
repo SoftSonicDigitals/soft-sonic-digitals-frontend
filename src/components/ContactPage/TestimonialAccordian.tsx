36b6a139b90a316558565d2109971806a4437c11
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-27 20:33:44 +1100

    [WEB-33] refactored the testimonial to its component

--- src/components/ContactPage/TestimonialAccordian.tsx
+++ src/components/ContactPage/TestimonialAccordian.tsx
@@ -1,24 +1,17 @@
 import { CONTACT_TESTIMONIALS } from "@/prototypes/contact_page";
-import Image from "next/image";
-import React from "react";
+import { TestimonialCard } from ".";
 
-const testimonial = CONTACT_TESTIMONIALS[3];
 const TestimonialAccordian = () => {
-  return (
-    <div className="flex flex-col gap-10  font-[500] tracking-wider text-gray-700 sm:px-16 md:px-12 ">
-      <div className="text-center px-[10%] md:px-10">
-        {testimonial.testimonial}
-      </div>
-      <div className="flex-center gap-6 px-14 md:px-8 lg:px-16">
-        <div className="relative h-[64px] min-w-[64px]">
-          <Image src={`/contact_page/profile-2.png`} alt="profile-img" fill />
-        </div>
-        <div className="">
-          <p>{testimonial.title}</p>
-          <p>{testimonial.name}</p>
-        </div>
-      </div>
-    </div>
+  return CONTACT_TESTIMONIALS.map(
+    ({ title, profile, name, testimonial }, index) => (
+      <TestimonialCard
+        key={index}
+        title={title}
+        profile={profile}
+        name={name}
+        testimonial={testimonial}
+      />
+    )
   );
 };
 
--- src/components/ContactPage/index.ts
+++ src/components/ContactPage/index.ts
@@ -2,10 +2,12 @@ import ContactClients from "./ContactClients";
 import ContactPageNavbar from "./ContactPageNavbar";
 import LetsTalkHeadings from "./LetsTalkHeadings";
 import TestimonialAccordian from "./TestimonialAccordian";
+import TestimonialCard from "./TestimonialCard";
 
 export {
   LetsTalkHeadings,
   ContactPageNavbar,
   ContactClients,
   TestimonialAccordian,
+  TestimonialCard,
 };
