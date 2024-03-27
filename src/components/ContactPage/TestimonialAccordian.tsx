3cce8f8def4267654dd9bbdcb5b8b60bd63eeb28
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-27 20:27:40 +1100

    [WEB-33] made the testimonial responsive

--- src/components/ContactPage/TestimonialAccordian.tsx
+++ src/components/ContactPage/TestimonialAccordian.tsx
@@ -2,14 +2,14 @@ import { CONTACT_TESTIMONIALS } from "@/prototypes/contact_page";
 import Image from "next/image";
 import React from "react";
 
-const testimonial = CONTACT_TESTIMONIALS[0];
+const testimonial = CONTACT_TESTIMONIALS[3];
 const TestimonialAccordian = () => {
   return (
     <div className="flex flex-col gap-10  font-[500] tracking-wider text-gray-700 sm:px-16 md:px-12 ">
-      <div className="text-center px-16 md:px-10">
+      <div className="text-center px-[10%] md:px-10">
         {testimonial.testimonial}
       </div>
-      <div className="flex-center gap-6 px-10 lg:px-16">
+      <div className="flex-center gap-6 px-14 md:px-8 lg:px-16">
         <div className="relative h-[64px] min-w-[64px]">
           <Image src={`/contact_page/profile-2.png`} alt="profile-img" fill />
         </div>
