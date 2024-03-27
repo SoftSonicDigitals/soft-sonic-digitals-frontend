3fea7f225cc08664b9d3f60a9e1a805fdf6a6dfa
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-27 19:51:05 +1100

    [WEB-33] roughly rendered the design for the testimonial

--- src/components/ContactPage/TestimonialAccordian.tsx
+++ src/components/ContactPage/TestimonialAccordian.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { CONTACT_TESTIMONIALS } from "@/prototypes/contact_page";
+import Image from "next/image";
+import React from "react";
 
+const testimonial = CONTACT_TESTIMONIALS[0];
 const TestimonialAccordian = () => {
   return (
-    <div>TestimonialAccordian</div>
-  )
-}
+    <div>
+      <div className="flex flex-col gap-4 px-16 ">
+        <div className="text-center">{testimonial.testimonial}</div>
+        <div className="flex">
+          <div>
+            <Image
+              src={`/contact_page/profile-1.png`}
+              alt="profile-img"
+              height={64}
+              width={64}
+            />
+          </div>
+          <div>
+            <p>{testimonial.title}</p>
+            <p>{testimonial.name}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default TestimonialAccordian
+export default TestimonialAccordian;
