53bb89467b15bd70e0800ab599d3a554b4dbd5fb
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-30 00:20:17 +1100

    [WEB-35] added footer

--- src/components/ContactPage/ContactPageFooter.tsx
+++ src/components/ContactPage/ContactPageFooter.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const ContactPageFooter = () => {
-  return <div>ContactPageFooter</div>;
+  return (
+    <section id="contact_footer" className="bg-black">
+      <div className="wrapper_container text-white">
+        <div className="grid items-center justify-center grid-cols-1 md:grid-cols-2 gap-4">
+          <div>
+            <p>Sales</p>
+            <p>info@magnetoitsolutions.com</p>
+          </div>
+          <div>
+            <p>Career</p>
+            <p>hiring@magnetoitsolutions.com</p>
+          </div>
+          <div>
+            <p>Media & Press</p>
+            <p>outreach@magnetoitsolutions.com</p>
+          </div>
+          <div>
+            <p>Skype</p>
+            <p>marketing_19207</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ContactPageFooter;
