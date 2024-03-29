b54301e2e70321652e8ddb86e23fa1cadf927bc3
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-29 20:43:19 +1100

    [WEB-34] adjusted the classname and styles and made it responsive

--- src/components/ContactPage/WhereToFind.tsx
+++ src/components/ContactPage/WhereToFind.tsx
@@ -12,7 +12,7 @@ const WhereToFind = () => {
           mainHeading="Where you'll find us"
           mainHeadingStyles="!font-[600] tracking-wide mb-24  "
         />
-        <div className="flex items-center justify-center lg:items-start flex-col lg:flex-row gap-8 text-gray-900">
+        <div className="flex items-center justify-center lg:items-start flex-col lg:flex-row gap-8 text-gray-900 px-10 sm:px-16 ">
           <Image
             src={`/contact_page/${locations[0].image}`}
             alt={`${locations[0].name}`}
@@ -23,28 +23,22 @@ const WhereToFind = () => {
 
           <div className="flex flex-col gap-8 max-w-[540px] mt-4">
             <div className="relative">
-              <h3 className="text-3xl capitalize font-[600] tracking-wide pb-3 ">
+              <h3 className="text-xl sm:text-2xl lg:text-3xl capitalize font-[600] tracking-wide pb-3 ">
                 {locations[0].name}
               </h3>
               <div className="absolute h-[3px] w-[40px] bg-gradient-to-r from-orange to-pink bottom-0" />
             </div>
             <div>
-              <p className="text-[25px] mb-2 font-[500]"> Contact us</p>
-              <p className="text-lg tracking-wider font-[500]">
-                {locations[0].email}
-              </p>
+              <p className="contact_headings">Contact us</p>
+              <p className="contact_info">{locations[0].email}</p>
             </div>
             <div>
-              <p className="text-[25px] mb-2 font-[500]"> Call us</p>
-              <p className="text-lg  tracking-wider font-[500]">
-                {locations[0].contactNo}
-              </p>
+              <p className="contact_headings">Call us</p>
+              <p className="contact_info">{locations[0].contactNo}</p>
             </div>
             <div>
-              <p className="text-[25px] mb-2 font-[500]">Address </p>
-              <p className="text-lg  tracking-wider font-[500]">
-                {locations[0].address}
-              </p>
+              <p className="contact_headings">Address</p>
+              <p className="contact_info">{locations[0].address}</p>
             </div>
           </div>
         </div>
