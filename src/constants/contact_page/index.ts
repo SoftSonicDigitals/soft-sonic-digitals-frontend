9c07bf92a68621835bd273f33d9acd9e4f10cb2e
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-29 21:09:26 +1100

    [WEB-34] added constants

--- src/constants/contact_page/index.ts
+++ src/constants/contact_page/index.ts
@@ -3,3 +3,6 @@ export const LETS_TALK_SUB_HEADING =
   "Just fill out this simple form and we will contact you promptly to discuss your requirement.";
 export const CLIENTS_PROMO_HEADING =
   "We love helping start-up become brand names and corporations launch new products";
+export const CONTACT_US = "Contact us";
+export const CALL_US = "Call us";
+export const ADDRESS = "Address";
