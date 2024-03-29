75565e7cf67af6620c1adbe89afc3f8ee4ff3989
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-29 17:51:09 +1100

    [WEB-34] added a rought format for display

--- src/components/ContactPage/WhereToFind.tsx
+++ src/components/ContactPage/WhereToFind.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { PrimaryStyledHeadings } from "../Reusable";
 
 const WhereToFind = () => {
   return (
     <section id="where_to_find_us">
-      <div className="wrapper_container">Where to find us</div>
+      <div className="wrapper_container mb-24">
+        <PrimaryStyledHeadings
+          mainHeading="Where you'll find us"
+          mainHeadingStyles="!font-[600] tracking-wide  "
+        />
+      </div>
+      <div className="flex flex-col lg:flex-row">
+        <div>Image</div>
+        <div>info</div>
+      </div>
     </section>
   );
 };
