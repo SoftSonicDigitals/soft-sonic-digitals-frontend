24b466b940eccdbf64a45914075ca34a6a4eb45c
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-29 21:19:28 +1100

    [WEB-34 adjusted the stylings

--- src/components/ContactPage/ContactInfo.tsx
+++ src/components/ContactPage/ContactInfo.tsx
@@ -16,7 +16,7 @@ const ContactInfo = ({
   address,
 }: ContactInfoType) => {
   return (
-    <div className="flex items-center justify-center lg:items-start flex-col md:flex-row gap-8 text-gray-900 px-6 sm:px-16  mb-24">
+    <div className="flex items-center justify-center lg:items-start flex-col md:flex-row gap-8 text-gray-900 px-6 sm:pl-8 lg:px-16 mb-24">
       <Image
         src={`/contact_page/${image}`}
         alt={`${name}`}
@@ -25,7 +25,7 @@ const ContactInfo = ({
         style={{ objectFit: "contain" }}
       />
 
-      <div className="flex flex-col gap-8 max-w-[540px] mt-4">
+      <div className="flex flex-col gap-8 md:gap-4 max-w-[540px] mt-4">
         <div className="relative">
           <h3 className="text-xl sm:text-2xl lg:text-3xl capitalize font-[600] tracking-wide pb-3 ">
             {name}
