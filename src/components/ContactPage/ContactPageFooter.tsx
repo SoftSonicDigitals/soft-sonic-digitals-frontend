09f4aa759695996a01bf38599635f50e03b1b13d
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-30 00:36:04 +1100

    [WEB-35] added a responsive footer

--- src/components/ContactPage/ContactPageFooter.tsx
+++ src/components/ContactPage/ContactPageFooter.tsx
@@ -1,25 +1,28 @@
+import Link from "next/link";
 import React from "react";
 
 const ContactPageFooter = () => {
   return (
     <section id="contact_footer" className="bg-black">
-      <div className="wrapper_container text-white">
-        <div className="grid items-center justify-center grid-cols-1 md:grid-cols-2 gap-4">
-          <div>
-            <p>Sales</p>
-            <p>info@magnetoitsolutions.com</p>
+      <div className="wrapper_container text-white flex-center py-16 px-5">
+        <div className="grid items-center justify-center grid-cols-1 md:grid-cols-2  gap-y-5 sm:gap-y-8">
+          <div className="pr-24">
+            <p className="footer_headings">Sales</p>
+            <Link href={"/"} className="footer_info">
+              info@magnetoitsolutions.com
+            </Link>
           </div>
-          <div>
-            <p>Career</p>
-            <p>hiring@magnetoitsolutions.com</p>
+          <div className="pr-24">
+            <p className="footer_headings">Career</p>
+            <p className="footer_info">hiring@magnetoitsolutions.com</p>
           </div>
-          <div>
-            <p>Media & Press</p>
-            <p>outreach@magnetoitsolutions.com</p>
+          <div className="pr-24">
+            <p className="footer_headings">Media & Press</p>
+            <p className="footer_info">outreach@magnetoitsolutions.com</p>
           </div>
-          <div>
-            <p>Skype</p>
-            <p>marketing_19207</p>
+          <div className="pr-24">
+            <p className="footer_headings">Skype</p>
+            <p className="footer_info">marketing_19207</p>
           </div>
         </div>
       </div>
