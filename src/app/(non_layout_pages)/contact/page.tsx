1c56889f7eea23b7d6972e8d3bd1348bc52d6d1a
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-29 16:51:59 +1100

    [WEB-33] made the carousel responsive

--- src/app/(non_layout_pages)/contact/page.tsx
+++ src/app/(non_layout_pages)/contact/page.tsx
@@ -8,9 +8,9 @@ const Contact = () => {
       <LetsTalkHeadings />
       <section>
         <div className="wrapper_container flex-center mb-24">
-          <div className="flex flex-col-reverse lg:flex-row  gap-8">
+          <div className="flex justify-center items-center flex-col-reverse lg:flex-row  gap-8 w-full">
             <ContactClients />
-            <div className="md:w-[555px] bg-gray-0 basis-full">form </div>
+            <div className="md:max-w-[555px] bg-gray-0 basis-full">form </div>
           </div>
         </div>
       </section>
--- src/components/ContactPage/TestimonialCard.tsx
+++ src/components/ContactPage/TestimonialCard.tsx
@@ -13,13 +13,15 @@ const TestimonialCard = ({
   profile,
 }: TestimonialCardType) => {
   return (
-    <div className="flex flex-col gap-10  font-[500] tracking-wider text-gray-700 sm:px-16 md:px-12 ">
-      <div className="text-center px-[10%] md:px-10">{testimonial}</div>
-      <div className="flex-center gap-6 px-14 md:px-8 lg:px-16">
+    <div className="flex flex-col gap-10  font-[500] tracking-wider text-gray-700 sm:px-8 md:px-4   ">
+      <div className="text-center px-[10%] sm:px-0 md:px-10 lg:px-6 2xl:px-16">
+        {testimonial}
+      </div>
+      <div className="flex-center gap-6 px-12 sm:px-4 md:px-10 lg:px-10 2xl:px-[20%] 3xl:px-28">
         <div className="relative h-[64px] min-w-[64px]">
           <Image src={`/contact_page/${profile}`} alt="profile-img" fill />
         </div>
-        <div className="">
+        <div>
           <p>{title}</p>
           <p>{name}</p>
         </div>
