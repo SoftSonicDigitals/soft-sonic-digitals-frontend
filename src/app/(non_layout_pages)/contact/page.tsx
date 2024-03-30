b8348fbdbf17648d830c775224ec430a449fcab1
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-03-30 20:31:22 +1100

    [WEB-36] added form component

--- src/app/(non_layout_pages)/contact/page.tsx
+++ src/app/(non_layout_pages)/contact/page.tsx
@@ -1,5 +1,6 @@
 import {
   ContactClients,
+  Form,
   LetsTalkHeadings,
   WhereToFind,
 } from "@/components/ContactPage";
@@ -15,12 +16,7 @@ const Contact = () => {
           <div className="flex justify-center items-center flex-col-reverse lg:flex-row  gap-8 w-full">
             <ContactClients />
 
-            <section
-              id="contact_form"
-              className="md:max-w-[555px] bg-gray-0 basis-full"
-            >
-              form
-            </section>
+            <Form />
           </div>
         </div>
       </section>
