715ec1c14c36019890013bbf2a656e32781014a6
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-01 22:08:45 +1100

    [WEB-38] rendered the banner image

--- src/components/CaseStudiesPage/CaseHeadings.tsx
+++ src/components/CaseStudiesPage/CaseHeadings.tsx
@@ -5,7 +5,7 @@ type CaseHeadingsPropsType = {
 };
 const CaseHeadings = ({ caseName, caseOutcomes }: CaseHeadingsPropsType) => {
   return (
-    <div className="wrapper_container mb-24 px-4 md:px-2">
+    <div className="wrapper_container mb-12 md:mb-24 px-4 md:px-2">
       <div className="pt-[150px] lg:pt-[170px] mb-9 md:mb-12 text-base lg:text-lg xl:text-base font-[700] text-gray-700 tracking-wide uppercase">
         {caseName}
       </div>
