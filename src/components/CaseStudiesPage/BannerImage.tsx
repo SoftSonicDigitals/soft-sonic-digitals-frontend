08140cfe7228c09161985ea47a2371ac9c9672a5
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-01 22:08:45 +1100

    [WEB-38] rendered the banner image

--- src/components/CaseStudiesPage/BannerImage.tsx
+++ src/components/CaseStudiesPage/BannerImage.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
 import React from "react";
 
 const BannerImage = () => {
-  return <section></section>;
+  return (
+    <section>
+      <div className="relative w-[100vw] mb-24 ">
+        <Image
+          src={"/case_study_pages/feather_down/feather_down_banner.webp"}
+          alt="banner image"
+          height={688}
+          width={1920}
+          layout="responsive"
+          style={{ objectFit: "contain" }}
+          sizes={"(max-width: 768px) 100vw,(max-width: 1200px) 50vw,33vw"}
+        />
+      </div>
+    </section>
+  );
 };
 
 export default BannerImage;
