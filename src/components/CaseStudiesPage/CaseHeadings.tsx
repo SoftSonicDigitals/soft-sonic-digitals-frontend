08633b8596892982c0d0c31644473bc407f55b3d
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-01 20:18:13 +1100

    [WEB-37] added props to the component

--- src/components/CaseStudiesPage/CaseHeadings.tsx
+++ src/components/CaseStudiesPage/CaseHeadings.tsx
@@ -1,14 +1,16 @@
 import React from "react";
-
-const CaseHeadings = () => {
+type CaseHeadingsPropsType = {
+  caseName: string;
+  caseOutcomes: string;
+};
+const CaseHeadings = ({ caseName, caseOutcomes }: CaseHeadingsPropsType) => {
   return (
     <div className="wrapper_container mb-24 px-4 md:px-2">
       <div className="pt-[150px] lg:pt-[170px] mb-9 md:mb-12 text-base lg:text-lg xl:text-base font-[700] text-gray-700 tracking-wide uppercase">
-        Feather down
+        {caseName}
       </div>
       <div className="text-2xl md:text-[35px] font-[500] text-gray-800 md:leading-snug ">
-        By delivering a unique client experience, the website assisted in
-        promoting the glamping industry and increasing revenue for the company.
+        {caseOutcomes}
       </div>
     </div>
   );
