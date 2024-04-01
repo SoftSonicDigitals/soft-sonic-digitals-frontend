fdf289a0a63f6246bac7a1e575c2217a9a7e2302
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-01 21:26:38 +1100

    [WEB-37] added props to the componnent in the case studies page

--- src/app/(layout_pages)/case-study/page.tsx
+++ src/app/(layout_pages)/case-study/page.tsx
@@ -1,10 +1,14 @@
 import { CaseHeadings } from "@/components/CaseStudiesPage";
+import { FEATHER_DOWN } from "@/prototypes/case_study_pages/featherDown";
 import React from "react";
 
 const page = () => {
   return (
     <>
-      <CaseHeadings />
+      <CaseHeadings
+        caseName={FEATHER_DOWN.caseName}
+        caseOutcomes={FEATHER_DOWN.caseOutcomes}
+      />
     </>
   );
 };
