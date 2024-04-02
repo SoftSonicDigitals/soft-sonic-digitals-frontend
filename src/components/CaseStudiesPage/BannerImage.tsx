8fe7f5f32edb450dfb5b7250b9f7ce8b34421ed3
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-03 10:15:10 +1100

    [WEB-38] rendered the responsive image

--- src/components/CaseStudiesPage/BannerImage.tsx
+++ src/components/CaseStudiesPage/BannerImage.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 import React from "react";
 
 const BannerImage = () => {
@@ -8,11 +8,11 @@ const BannerImage = () => {
         <Image
           src={"/case_study_pages/feather_down/feather_down_banner.webp"}
           alt="banner image"
+          layout="responsive"
           height={688}
           width={1920}
-          layout="responsive"
-          style={{ objectFit: "contain" }}
-          sizes={"(max-width: 768px) 100vw,(max-width: 1200px) 50vw,33vw"}
+          objectFit="contain"
+          priority
         />
       </div>
     </section>
