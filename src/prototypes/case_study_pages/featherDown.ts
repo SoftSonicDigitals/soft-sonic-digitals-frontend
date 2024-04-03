22324b222ace5826cc6eaea0b640a8d76eb04248
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-03 21:17:02 +1100

    [WEB-45] added constants and description in the array

--- src/prototypes/case_study_pages/featherDown.ts
+++ src/prototypes/case_study_pages/featherDown.ts
@@ -26,4 +26,6 @@ export const FEATHER_DOWN = {
     "We have integrated inventory software with the system to ensure changes, transactions, and order updates are automatically updated.",
     "We have developed a separate portal for countries like the Netherlands and UK.",
   ],
+  factsFigures:
+    "The proof for success is an expansion of farm sites in the USA in addition to leading European countries. There are already 64 farm sites running successfully, and the average customer review is 9.5/10, with more than 35% repeat customer rate. In a few months, clients have experienced a significant improvement in revenue. As we have integrated the website with backend inventory software, handling and managing sales accounts were automated. By getting heavy traffic from the Netherlands, we have developed another website for customers in the Netherlands. Sales and revenue were improved up to 6 times.",
 };
