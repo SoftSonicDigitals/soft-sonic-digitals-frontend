83c7090f913ef0117861e617a012761c6ca83710
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-03 12:13:36 +1100

    [WEB-39] adjusted the paddings and margins for the adjacent section

--- src/components/CaseStudiesPage/TheAsk.tsx
+++ src/components/CaseStudiesPage/TheAsk.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const TheAsk = () => {
   return (
     <section>
-      <div className="wrapper_container mb-24">
+      <div className="wrapper_container mb-12 md:mb-24 px-4 md:px-2">
         <div className="text-2xl md:text-[35px] font-[500] text-gray-800 md:leading-snug ">
           {" "}
           The ask{" "}
