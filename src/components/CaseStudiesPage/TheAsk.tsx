f8379f663dd7a294249b6d8ea544cc9ab62ee03f
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-03 12:48:37 +1100

    [WEB-39] refactored to receive props

--- src/components/CaseStudiesPage/TheAsk.tsx
+++ src/components/CaseStudiesPage/TheAsk.tsx
@@ -1,18 +1,15 @@
 import { THE_ASK } from "@/constants/case_studies_page";
 import React from "react";
-
-const TheAsk = ({}) => {
+type TheAskPropsType = {
+  theAskDescription: string;
+};
+const TheAsk = ({ theAskDescription }: TheAskPropsType) => {
   return (
     <section>
       <div className="wrapper_container mb-12 md:mb-24 px-4 md:px-2">
         <div className="case_study_headings mb-8">{THE_ASK} </div>
         <div className="text-lg font-[500] text-gray-400">
-          Camping addresses the escapism to unwind in real nature, but
-          serenity-obsessed people couldn’t compromise on comfort and luxury.
-          The client had the concept of a Glamping site, a blend of camping and
-          glamorous living at the farm site. For this idea, the client wanted to
-          develop a portal from where customers can book camps and at the same
-          time be able to manage the inventory with enhanced accountability.
+          {theAskDescription}
         </div>
       </div>
     </section>
