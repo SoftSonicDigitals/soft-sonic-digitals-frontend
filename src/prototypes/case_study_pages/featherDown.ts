5ee3dea93997fb56b317dd3eacfef652376fef95
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-03 19:37:22 +1100

    [WEB-43] added the image to the array

--- src/prototypes/case_study_pages/featherDown.ts
+++ src/prototypes/case_study_pages/featherDown.ts
@@ -17,4 +17,5 @@ export const FEATHER_DOWN = {
   showCaseImg1Path: "/case_study_pages/feather_down/show_case.webp",
   solution:
     "We conducted research, studied, and understood core areas of the glamping industry. We prepared a list of features that could be ideal for the client glamping website based on our research. This case study portrays the contribution of Magneto development services in delivering reliable solutions to the client",
+  showCaseImg2Path: "/case_study_pages/feather_down/show_case_2.webp",
 };
