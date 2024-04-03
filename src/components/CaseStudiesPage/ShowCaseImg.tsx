7e2490a3e28a1d56a9f14c7e624c02d261ca4728
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-03 18:45:36 +1100

    [WEB-41] refactored the component

--- src/components/CaseStudiesPage/ShowCaseImg.tsx
+++ src/components/CaseStudiesPage/ShowCaseImg.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import React from "react";
-
-const ShowCaseImg = () => {
+type ShowCaseImgPropsType = {
+  showCaseImagePath: string;
+};
+const ShowCaseImg = ({ showCaseImagePath }: ShowCaseImgPropsType) => {
   return (
     <section>
       <div className="wrapper_container mb-12 md:mb-24 px-3  ">
         <div className="flex-center">
           <Image
-            src={"/case_study_pages/feather_down/show_case.webp"}
+            src={showCaseImagePath}
             alt="show-case Image"
             width={1224}
             height={768}
