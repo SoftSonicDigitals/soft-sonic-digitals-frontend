35cc897a714f509ce03df294a93ef5f1a3b8e26c
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-03 12:43:07 +1100

    [WEB-39] added the ask description

--- src/components/CaseStudiesPage/TheAsk.tsx
+++ src/components/CaseStudiesPage/TheAsk.tsx
@@ -4,7 +4,15 @@ const TheAsk = () => {
   return (
     <section>
       <div className="wrapper_container mb-12 md:mb-24 px-4 md:px-2">
-        <div className="case_study_headings"> The ask </div>
+        <div className="case_study_headings mb-8"> The ask </div>
+        <div className="text-lg font-[500] text-gray-400">
+          Camping addresses the escapism to unwind in real nature, but
+          serenity-obsessed people couldn’t compromise on comfort and luxury.
+          The client had the concept of a Glamping site, a blend of camping and
+          glamorous living at the farm site. For this idea, the client wanted to
+          develop a portal from where customers can book camps and at the same
+          time be able to manage the inventory with enhanced accountability.
+        </div>
       </div>
     </section>
   );
