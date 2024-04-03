d81f03f639dedf4b85d6ccd4093d868b6bbfeb14
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-03 16:24:02 +1100

    [WEB-40] addedd common issue component

---
+++ src/components/CaseStudiesPage/CommonIssue.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CommonIssue = () => {
+  return (
+    <section>
+      <div className="wrapper_container mb-12 md:mb-24">CommonIssue</div>
+    </section>
+  );
+};
+
+export default CommonIssue;
