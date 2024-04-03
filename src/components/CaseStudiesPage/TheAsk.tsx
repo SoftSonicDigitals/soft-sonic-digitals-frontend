8f1e34275351f487f7e8c52f027a5c84310a57bd
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-03 12:46:08 +1100

    [WEB-39] added the constants for the ask component

--- src/components/CaseStudiesPage/TheAsk.tsx
+++ src/components/CaseStudiesPage/TheAsk.tsx
@@ -1,10 +1,11 @@
+import { THE_ASK } from "@/constants/case_studies_page";
 import React from "react";
 
-const TheAsk = () => {
+const TheAsk = ({}) => {
   return (
     <section>
       <div className="wrapper_container mb-12 md:mb-24 px-4 md:px-2">
-        <div className="case_study_headings mb-8"> The ask </div>
+        <div className="case_study_headings mb-8">{THE_ASK} </div>
         <div className="text-lg font-[500] text-gray-400">
           Camping addresses the escapism to unwind in real nature, but
           serenity-obsessed people couldn’t compromise on comfort and luxury.
---
+++ src/constants/case_studies_page/index.ts
@@ -0,0 +1,1 @@
+export const THE_ASK = "The ask";
