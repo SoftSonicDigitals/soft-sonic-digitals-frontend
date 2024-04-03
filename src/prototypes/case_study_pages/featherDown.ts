dd211d4c3f65be809e5edd92e93187586e5153d6
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-03 17:08:38 +1100

    [WEB-40] added bullet points to the array

--- src/prototypes/case_study_pages/featherDown.ts
+++ src/prototypes/case_study_pages/featherDown.ts
@@ -5,4 +5,13 @@ export const FEATHER_DOWN = {
   bannerImgPath: "/case_study_pages/feather_down/feather_down_banner.webp",
   theAsk:
     "Camping addresses the escapism to unwind in real nature, but serenity-obsessed people couldn’t compromise on comfort and luxury. The client had the concept of a Glamping site, a blend of camping and glamorous living at the farm site. For this idea, the client wanted to develop a portal from where customers can book camps and at the same time be able to manage the inventory with enhanced accountability.",
+  commonIssues: [
+    "The client was facing problems managing the customer’s requests from the different regions.",
+    "It has become tough for his team to deal with customers on the phone about the inquiry and give them updates.",
+    "Dealing with clients from the different European countries was becoming tough, and managing their details simultaneously was inconsistent and needed more manpower.",
+    "Checking the camping site based on the request from the customers was taking a lot of time to give them an update about it.",
+    "Based on the Customers’ inquiries about the camping site for different locations, Giving them status about the availability of camps was a lot of time-consuming.",
+    "Clients were facing the issue of managing the payments from the customers.",
+    "Manual handling of inventory and records updation has become a surpassing issue.",
+  ],
 };
