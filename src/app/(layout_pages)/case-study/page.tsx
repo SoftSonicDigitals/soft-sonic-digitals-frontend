9d26d234c997cf494793f5b0045a207cfe276b78
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-03 18:27:09 +1100

    [WEB-41] added show img component

--- src/app/(layout_pages)/case-study/page.tsx
+++ src/app/(layout_pages)/case-study/page.tsx
@@ -2,6 +2,7 @@ import {
   BannerImage,
   CaseHeadings,
   CommonIssue,
+  ShowCaseImg,
   TheAsk,
 } from "@/components/CaseStudiesPage";
 import { FEATHER_DOWN } from "@/prototypes/case_study_pages/featherDown";
@@ -17,6 +18,7 @@ const page = () => {
       <BannerImage bannerImgPath={FEATHER_DOWN.bannerImgPath} />
       <TheAsk theAskDescription={FEATHER_DOWN.theAsk} />
       <CommonIssue commonIssues={FEATHER_DOWN.commonIssues} />
+      <ShowCaseImg />
     </>
   );
 };
--- src/components/CaseStudiesPage/index.ts
+++ src/components/CaseStudiesPage/index.ts
@@ -1,6 +1,7 @@
 import BannerImage from "./BannerImage";
 import CaseHeadings from "./CaseHeadings";
 import CommonIssue from "./CommonIssue";
+import ShowCaseImg from "./ShowCaseImg";
 import TheAsk from "./TheAsk";
 
-export { CaseHeadings, BannerImage, TheAsk, CommonIssue };
+export { CaseHeadings, BannerImage, TheAsk, CommonIssue, ShowCaseImg };
