d1ea0d64e717297a6908eb52b34ed514a6b1a53f
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-03 12:48:37 +1100

    [WEB-39] refactored to receive props

--- src/prototypes/case_study_pages/featherDown.ts
+++ src/prototypes/case_study_pages/featherDown.ts
@@ -3,4 +3,6 @@ export const FEATHER_DOWN = {
   caseOutcomes:
     "By delivering a unique client experience, the website assisted in promoting the glamping industry and increasing revenue for the company.",
   bannerImgPath: "/case_study_pages/feather_down/feather_down_banner.webp",
+  theAsk:
+    "Camping addresses the escapism to unwind in real nature, but serenity-obsessed people couldn’t compromise on comfort and luxury. The client had the concept of a Glamping site, a blend of camping and glamorous living at the farm site. For this idea, the client wanted to develop a portal from where customers can book camps and at the same time be able to manage the inventory with enhanced accountability.",
 };
