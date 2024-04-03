d73a50a5d2d766ca3b355672aa01d688b6af4355
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-03 17:25:28 +1100

    [WEB-40] chnaged the name of the component and mapped over the array

---
+++ src/components/Reusable/PrimaryLists.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+type PrimaryListsPropsType = {
+  listArray: string[];
+};
+const PrimaryLists = ({ listArray }: PrimaryListsPropsType) => {
+  return (
+    <ul className="text-lg text-gray-400 mb-2 font-[500] list-disc list-outside pl-6 marker:text-orange marker:text-3xl ">
+      {listArray.map((bullet, index) => (
+        <li key={index} className="mb-1 last:mb-0">
+          {bullet}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default PrimaryLists;
