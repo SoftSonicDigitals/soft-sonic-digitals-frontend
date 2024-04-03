82d6accdc28758612d6f8579824047a0b16a2aea
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-03 17:33:48 +1100

    [WEB-40] provided props to the componenet

--- src/components/Reusable/PrimaryLists.tsx
+++ src/components/Reusable/PrimaryLists.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 type PrimaryListsPropsType = {
   listArray: string[];
+  ulStyles?: string;
+  liStyles?: string;
 };
-const PrimaryLists = ({ listArray }: PrimaryListsPropsType) => {
+const PrimaryLists = ({
+  listArray,
+  ulStyles,
+  liStyles,
+}: PrimaryListsPropsType) => {
   return (
-    <ul className="text-lg text-gray-400 mb-2 font-[500] list-disc list-outside pl-6 marker:text-orange marker:text-3xl ">
+    <ul
+      className={`text-lg text-gray-400 mb-2 font-[500] list-disc list-outside pl-6 marker:text-orange marker:text-3xl  ${ulStyles}`}
+    >
       {listArray.map((bullet, index) => (
-        <li key={index} className="mb-1 last:mb-0">
+        <li key={index} className={`mb-1 last:mb-0  ${liStyles}`}>
           {bullet}
         </li>
       ))}
