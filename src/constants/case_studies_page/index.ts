695e6377698f02995af3c8bad31a2035b94ec37f
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-03 21:17:02 +1100

    [WEB-45] added constants and description in the array

--- src/constants/case_studies_page/index.ts
+++ src/constants/case_studies_page/index.ts
@@ -2,3 +2,4 @@ export const THE_ASK = "The ask";
 export const COMMON_ISSUES = "Common issues faced";
 export const SOLUTION = "Solution";
 export const BENEFITS = "Benefits";
+export const FACTS_FIGURES = "Facts & figures";
