be13d07f7391a9a127fbd0c8065517a5ed72ba6c
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-03 19:04:27 +1100

    [WEB-42] added the solution component

--- src/components/CaseStudiesPage/index.ts
+++ src/components/CaseStudiesPage/index.ts
@@ -3,5 +3,13 @@ import CaseHeadings from "./CaseHeadings";
 import CommonIssue from "./CommonIssue";
 import ShowCaseImg from "./ShowCaseImg";
 import TheAsk from "./TheAsk";
+import TheSolution from "./TheSolution";
 
-export { CaseHeadings, BannerImage, TheAsk, CommonIssue, ShowCaseImg };
+export {
+  CaseHeadings,
+  BannerImage,
+  TheAsk,
+  CommonIssue,
+  ShowCaseImg,
+  TheSolution,
+};
