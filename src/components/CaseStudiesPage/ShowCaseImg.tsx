b79bc5dbce4feeb90eaf80b9e5d26ba3b6229bba
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-03 18:47:38 +1100

    [WEB-41] added path to the array and provided props to the componenet

--- src/components/CaseStudiesPage/ShowCaseImg.tsx
+++ src/components/CaseStudiesPage/ShowCaseImg.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
 import React from "react";
 type ShowCaseImgPropsType = {
-  showCaseImagePath: string;
+  showCaseImgPath: string;
 };
-const ShowCaseImg = ({ showCaseImagePath }: ShowCaseImgPropsType) => {
+const ShowCaseImg = ({ showCaseImgPath }: ShowCaseImgPropsType) => {
   return (
     <section>
       <div className="wrapper_container mb-12 md:mb-24 px-3  ">
         <div className="flex-center">
           <Image
-            src={showCaseImagePath}
+            src={showCaseImgPath}
             alt="show-case Image"
             width={1224}
             height={768}
