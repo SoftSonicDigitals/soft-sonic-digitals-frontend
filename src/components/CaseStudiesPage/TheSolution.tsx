e4c43a80a6053a36b7a93aae35561bd9f655b3af
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-03 19:07:55 +1100

    [WEB-42] added constants

--- src/components/CaseStudiesPage/TheSolution.tsx
+++ src/components/CaseStudiesPage/TheSolution.tsx
@@ -1,10 +1,11 @@
+import { SOLUTION } from "@/constants/case_studies_page";
 import React from "react";
 
 const TheSolution = () => {
   return (
     <section>
       <div className="wrapper_container mb-12 md:mb-24 px-4 md:px-2">
-        <div className="case_study_headings ">Solution </div>
+        <div className="case_study_headings "> {SOLUTION}</div>
         <div className="text-lg font-[500] text-gray-400">
           We conducted research, studied, and understood core areas of the
           glamping industry. We prepared a list of features that could be ideal
