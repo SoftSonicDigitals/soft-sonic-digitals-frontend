53437356ccfc8e3fe34c2fa1c5010257ebb8ba7f
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-03 18:43:43 +1100

    [WEB-41] added image

--- src/components/CaseStudiesPage/ShowCaseImg.tsx
+++ src/components/CaseStudiesPage/ShowCaseImg.tsx
@@ -1,9 +1,20 @@
+import Image from "next/image";
 import React from "react";
 
 const ShowCaseImg = () => {
   return (
     <section>
-      <div className="wrapper_container"></div>
+      <div className="wrapper_container mb-12 md:mb-24 px-3  ">
+        <div className="flex-center">
+          <Image
+            src={"/case_study_pages/feather_down/show_case.webp"}
+            alt="show-case Image"
+            width={1224}
+            height={768}
+            style={{ objectFit: "contain" }}
+          />
+        </div>
+      </div>
     </section>
   );
 };
