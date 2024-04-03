8d362a6bbdf3e6b21696e95eeb89c0ffffc864e0
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-03 19:22:33 +1100

    [WEB-42] refactored the the ask and solution component

--- src/app/(layout_pages)/case-study/page.tsx
+++ src/app/(layout_pages)/case-study/page.tsx
@@ -1,11 +1,11 @@
 import {
   BannerImage,
   CaseHeadings,
+  CaseStudyInfo,
   CommonIssue,
   ShowCaseImg,
-  TheAsk,
-  TheSolution,
 } from "@/components/CaseStudiesPage";
+import { SOLUTION, THE_ASK } from "@/constants/case_studies_page";
 import { FEATHER_DOWN } from "@/prototypes/case_study_pages/featherDown";
 import React from "react";
 
@@ -17,10 +17,10 @@ const page = () => {
         caseOutcomes={FEATHER_DOWN.caseOutcomes}
       />
       <BannerImage bannerImgPath={FEATHER_DOWN.bannerImgPath} />
-
+      <CaseStudyInfo title={THE_ASK} description={FEATHER_DOWN.theAsk} />
       <CommonIssue commonIssues={FEATHER_DOWN.commonIssues} />
       <ShowCaseImg showCaseImgPath={FEATHER_DOWN.showCaseImg1Path} />
-      <TheSolution />
+      <CaseStudyInfo title={SOLUTION} description={FEATHER_DOWN.solution} />
     </>
   );
 };
