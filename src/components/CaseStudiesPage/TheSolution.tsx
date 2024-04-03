39df7098bdb35f5c1261803bb93f0bafd8bfe69c
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-03 19:06:46 +1100

    [WEB-42] rendered the solution

--- src/components/CaseStudiesPage/TheSolution.tsx
+++ src/components/CaseStudiesPage/TheSolution.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const TheSolution = () => {
-  return <div>TheSolution</div>;
+  return (
+    <section>
+      <div className="wrapper_container mb-12 md:mb-24 px-4 md:px-2">
+        <div className="case_study_headings ">Solution </div>
+        <div className="text-lg font-[500] text-gray-400">
+          We conducted research, studied, and understood core areas of the
+          glamping industry. We prepared a list of features that could be ideal
+          for the client glamping website based on our research. This case study
+          portrays the contribution of Magneto development services in
+          delivering reliable solutions to the client
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default TheSolution;
