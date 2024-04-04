747bdc9ba9e834a30847cf8a6e3f02a3f926ffc4
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-04 12:25:41 +1100

    [wEB-51] chnaged the case study on the home page

---
+++ src/prototypes/case_study_pages/petZone.ts
@@ -0,0 +1,37 @@
+export const PETZONE = {
+  caseName: "petzone",
+  caseOutcomes:
+    "Petzone has successfully created an online store and a mobile app to improve their customers' omnichannel experience.",
+  bannerImg1Path: "/case_study_pages/powerlook/bannerImg1.webp",
+  theAsk:
+    "Powerlook is one of the fastest-growing e-commerce brands in India which offers a unique range of men’s casual wear with a shade of western touch. Powerlook is a family-owned business that started its first retail shop in 2010. They have three retail shops in Andheri, Borivali and Thane in Mumbai.This case study shows how Magneto IT Solutions helped Powerlook to enhance their website performance.",
+  commonIssues: [
+    "There was no process to manage returned items from customers due to which manual work of the client increased.",
+    "Backend issues were impacting the customer journey as they were not able to access certain features of the website.",
+    "Customers were not able to navigate through the website and could not purchase hence the cart abandonment rate increased.",
+    "Since the server was not configured properly there were some issues with the website which depreciated the customer experience.",
+    "While auditing the website there were multiple extensions that were not getting used that hampered the website performance.",
+    "Admin was facing several challenges while processing the orders from the backend.",
+  ],
+  showCaseImg1Path: "/case_study_pages/powerlook/showCaseImg1.webp",
+  solution:
+    "In order to resolve the issues faced by client, Magneto IT Solutions conducted our research and analysis to understand the problem. Looking at the scope of work and tasks we deployed an experienced team. Magneto IT Solutions designed a website with headless architecture and used React for frontend development and Magento for backend development.",
+  showCaseImg2Path: "/case_study_pages/powerlook/showCaseImg2.webp",
+  benefits: [
+    "We consulted the Powerlook team to conduct code auditing of their website to understand the code quality.",
+    "Complete Code was revamped and functionalities were improved to increase the performance of the website.",
+    "We added a return management module which helped the management to track the returned goods and decrease the manual work for them.",
+    "Extra extensions and plug-ins that were not used which increased the loading time of the website were removed.",
+    "Backend structure was improved to ease out the admin efforts and improve the customer experience.",
+  ],
+  factsFigures:
+    "The new upgraded store made by Magneto IT Solutions had a great bug free user experience and customers spent more time on the website. Due to several feature integrations the manual work of the client was decreased to a certain extent.",
+  showCaseImg3Path: "/case_study_pages/powerlook/showCaseImg3.webp",
+  bannerImg2Path: "/case_study_pages/powerlook/bannerImg2.webp",
+  responsiveLayout:
+    "We created a mobile responsive website for Powerlook. We made sure that the web pages work smoothly on all mobile devices. In addition to this, the navigation and content categories were created in such a way that it provides a consistent experience to the customers.",
+  responsiveShowCaseImgPath:
+    "/case_study_pages/powerlook/responsive_layout.webp",
+  takeAway:
+    "Magneto IT Solution helped Powerlook to create a new improved platform for their customers. This case study also depicts how improving website features and functionality can improve the customer shopping experience and also reduce the manual efforts of management.",
+};
