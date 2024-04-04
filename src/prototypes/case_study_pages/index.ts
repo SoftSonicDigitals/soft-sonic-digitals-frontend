264b698b30da80aa9bde46702f35106c6c7b1f2d
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-04 21:49:44 +1100

    [WEB-51] adjusted the array

--- src/prototypes/case_study_pages/index.ts
+++ src/prototypes/case_study_pages/index.ts
@@ -1,5 +1,10 @@
 import { FEATHER_DOWN } from "./featherDown";
 import { LIVING_IT_UP } from "./livingItUp";
+import { PETZONE } from "./petZone";
 import { POWERLOOK } from "./powerLook";
-
-export const CASE_STUDIES_ARRAY = [LIVING_IT_UP, FEATHER_DOWN, POWERLOOK];
+export const CASE_STUDIES_ARRAY = [
+  LIVING_IT_UP,
+  FEATHER_DOWN,
+  POWERLOOK,
+  PETZONE,
+];
