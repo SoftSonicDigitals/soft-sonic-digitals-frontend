53260c6ae557b8784a84ea7097d7ea21231e5a28
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-04 12:04:33 +1100

    [WEB-51] added living_it_up array

--- src/prototypes/case_study_pages/index.ts
+++ src/prototypes/case_study_pages/index.ts
@@ -1,3 +1,4 @@
 import { FEATHER_DOWN } from "./featherDown";
+import { LIVING_IT_UP } from "./livingItUp";
 
-export const CASE_STUDIES_ARRAY = [FEATHER_DOWN];
+export const CASE_STUDIES_ARRAY = [LIVING_IT_UP];
---
+++ src/prototypes/case_study_pages/livingItUp.ts
@@ -0,0 +1,37 @@
+export const LIVING_IT_UP = {
+  caseName: "living it up",
+  caseOutcomes:
+    "The client enhanced customer engagement due to the website revamping, which resulted in more business.",
+  bannerImg1Path: "/case_study_pages/living_it_up/bannerImg1.webp",
+  theAsk:
+    "In 2005, Living It Up was founded. It has been making made-to-order beds, sofas, chairs, and footstools for over a decade. Their selection is constantly increasing as they bring the newest interior design trends with the timeless classics that have been a part of their range since the beginning. Living It Up works directly with suppliers, giving them complete control over the designs available. Only the best quality materials and beautiful textiles are used to create the made-to-order furniture.",
+  commonIssues: [
+    "The customer had the website built by another agency but was dissatisfied with its performance since it did not meet their expectations.",
+    "Because the previous developers did not follow standard coding techniques, several backend modules did not function properly.",
+    "To preserve the brand’s standing and establish relationships with customers, the website must have a beautiful UI and interactive homepage.",
+    "Customers were unable to place orders owing to several difficulties with the website.",
+    "Since the current website’s navigation and purchasing procedure were inadequate, there was a high cart abandonment rate, which resulted in business loss.",
+  ],
+  showCaseImg1Path: "/case_study_pages/living_it_up/showCaseImg1.webp",
+  solution:
+    "Even after establishing an e-commerce store, the client remained dissatisfied with the website’s performance. The number of sales and business conversions created did not meet expectations. Magneto IT Solutions redesigned their website by engaging the services of an experienced team of Magento developers. We revamped it by eliminating all of the problems and defects in their previous websites, resulting in a rich user experience and simple navigation around the site for consumers.",
+  showCaseImg2Path: "/case_study_pages/living_it_up/showCaseImg2.webp",
+  benefits: [
+    "We conducted a thorough audit of the website and checked the code standards of the website to understand if previous agencies followed the appropriate code architecture or not.",
+    "Following the audit, some technical flaws in the code were discovered, which were ultimately addressed to enhance the website’s performance.",
+    "We revamped the website’s most essential layouts, such as the homepage, product detail page, log in, and cart page, resulting in a considerable increase in consumer engagement.",
+    "Customers were able to navigate easily due to the seamless UI and dynamic homepage, which enhanced the client’s customer conversion rate.",
+    "We improved the website’s user experience in product images and descriptions, allowing users to spend more time on the site.",
+    "We also helped the client with digital marketing, which helped him raise brand recognition among customers.",
+  ],
+  factsFigures:
+    "Magneto IT Solutions provided technical help and direction to Living it Up, and results were noticeable in a short period, as the e-Commerce business generated income increased.",
+  showCaseImg3Path: "/case_study_pages/living_it_up/showCaseImg3.webp",
+  bannerImg2Path: "/case_study_pages/living_it_up/bannerImg2.webp",
+  responsiveLayout:
+    "For the Living it Up website, we built a mobile-friendly theme. We tested the web pages on various mobile devices to ensure that they function correctly. Furthermore, the navigation and content categories were designed so that clients get a seamless experience.",
+  responsiveShowCaseImgPath:
+    "/case_study_pages/living_it_up/responsive_layout.webp",
+  takeAway:
+    "This case study shows how Magneto IT Solutions, as a technology partner, helped Living it Up redesign their website in terms of appearance, functionality, and customer experience to increase customer traffic and income through their e-Commerce platform.",
+};
