f1fcf8eb441f51ba69f340d4ba894853285fe7c9
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-05 10:54:28 +1100

    [WEB-52] made the section responsive

--- src/components/CaseStudiesPage/LetsTalkBanner.tsx
+++ src/components/CaseStudiesPage/LetsTalkBanner.tsx
@@ -1,16 +1,15 @@
 import React from "react";
-import { PrimaryButton } from "../Reusable";
-import { LetsTalkContactButton } from ".";
+import { GetInTouchContactBtn } from "../GetInTouch";
 
 const LetsTalkBanner = () => {
   return (
     <section className="bg-black ">
-      <div className="wrapper_container py-16 mb-24 flex justify-between px-8">
-        <p className="text-white text-2xl font-[500] tracking-wider">
+      <div className="wrapper_container py-16 mb-24 flex flex-col lg:flex-row items-center  justify-between px-8  gap-6">
+        <p className="text-white text-lg md:text-2xl font-[500] tracking-wider text-center lg:text-left">
           Have a project in mind? We’d love to make your ideas into reality.
         </p>
         <div>
-          <LetsTalkContactButton />
+          <GetInTouchContactBtn />
         </div>
       </div>
     </section>
