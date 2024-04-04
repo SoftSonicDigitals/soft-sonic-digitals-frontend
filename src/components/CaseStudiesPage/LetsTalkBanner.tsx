b6eb2de144ce5c65aab25006403e774633ec578a
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-05 10:55:44 +1100

    [WEB-52] added constants

--- src/components/CaseStudiesPage/LetsTalkBanner.tsx
+++ src/components/CaseStudiesPage/LetsTalkBanner.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { GetInTouchContactBtn } from "../GetInTouch";
+import { BANNER_MESSAGE } from "@/constants/case_studies_page";
 
 const LetsTalkBanner = () => {
   return (
     <section className="bg-black ">
       <div className="wrapper_container py-16 mb-24 flex flex-col lg:flex-row items-center  justify-between px-8  gap-6">
         <p className="text-white text-lg md:text-2xl font-[500] tracking-wider text-center lg:text-left">
-          Have a project in mind? We’d love to make your ideas into reality.
+          {BANNER_MESSAGE}
         </p>
         <div>
           <GetInTouchContactBtn />
