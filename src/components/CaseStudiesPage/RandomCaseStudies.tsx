dfb483827404bc508f6e597f5a99a6e895b7f43c
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-05 12:42:41 +1100

    [WEB-54] rendered the downloaded images

--- src/components/CaseStudiesPage/RandomCaseStudies.tsx
+++ src/components/CaseStudiesPage/RandomCaseStudies.tsx
@@ -1,9 +1,22 @@
+import Image from "next/image";
 import React from "react";
 
 const RandomCaseStudies = () => {
   return (
     <section>
-      <div className="wrapper_container"></div>
+      <div className="wrapper_container mb-12 md:mb-24">
+        <div className="flex flex-col lg:flex-row">
+          <div>
+            <Image
+              src={`/case_study_pages/feather_down/poster.webp`}
+              height={400}
+              width={576}
+              alt="poster"
+            />
+          </div>
+          <div></div>
+        </div>
+      </div>
     </section>
   );
 };
