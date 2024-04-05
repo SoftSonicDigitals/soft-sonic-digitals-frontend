1381dbc454f6e01dc74460317e0716e5085d2f9f
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-06 10:39:31 +1100

    [WEB-54] added reusable view all btn component

--- src/components/CaseStudiesPage/RandomCaseStudies.tsx
+++ src/components/CaseStudiesPage/RandomCaseStudies.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CaseStudyTiles from "./CaseStudyTiles";
 import { CaseStudyPage } from "@/models/case_study_page";
+import { ViewAllBtn } from "../Reusable";
 type RandomCaseStudiesProps = {
   randomCases: CaseStudyPage[];
 };
@@ -8,7 +9,7 @@ const RandomCaseStudies = ({ randomCases }: RandomCaseStudiesProps) => {
   return (
     <section>
       <div className="wrapper_container mb-12 md:mb-24 px-4 pt-16">
-        <div className="grid  grid-cols-1  md:grid-cols-2 gap-8 max-w-[576px] md:max-w-fit mx-auto">
+        <div className="grid  grid-cols-1  md:grid-cols-2 gap-8 max-w-[576px] md:max-w-fit mx-auto mb-20">
           {randomCases.map((caseStudy, index) => (
             <CaseStudyTiles
               key={index}
@@ -18,6 +19,7 @@ const RandomCaseStudies = ({ randomCases }: RandomCaseStudiesProps) => {
             />
           ))}
         </div>
+        <ViewAllBtn />
       </div>
     </section>
   );
