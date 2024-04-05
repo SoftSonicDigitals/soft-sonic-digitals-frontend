c52bc5777979df61bfd6d9c0adc9239d28301048
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-05 20:32:53 +1100

    [WEB-54] centerd the grid

--- src/components/CaseStudiesPage/RandomCaseStudies.tsx
+++ src/components/CaseStudiesPage/RandomCaseStudies.tsx
@@ -5,8 +5,8 @@ import React from "react";
 const RandomCaseStudies = () => {
   return (
     <section>
-      <div className="wrapper_container mb-12 md:mb-24 px-4 md:px-2 pt-16">
-        <div className="grid  grid-cols-1  md:grid-cols-2 gap-8">
+      <div className="wrapper_container mb-12 md:mb-24 px-4 pt-16">
+        <div className="grid  grid-cols-1  md:grid-cols-2 gap-8 max-w-[576px] md:max-w-fit mx-auto">
           <Link href="">
             <div className="relative ">
               <Image
@@ -28,10 +28,10 @@ const RandomCaseStudies = () => {
                 }}
               />
             </div>
-            <p className="mt-2.5 text-lg font-[500]">{"Arabian Ceramics"}</p>
+            <p className="mt-2.5 text-lg font-[500] ">{"Arabian Ceramics"}</p>
           </Link>
 
-          <div className="relative ">
+          {/* <div className="relative ">
             <Image
               src={`/case_study_pages/feather_down/poster.webp`}
               height={400}
@@ -50,7 +50,7 @@ const RandomCaseStudies = () => {
                 transform: "translate(-50%,-50%)",
               }}
             />
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
