d0a95b085304e6063dd5379f29d11c8fe1e57abb
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-05 11:11:31 +1100

    [WEB-53] added button for request for proposal

---
+++ src/components/CaseStudiesPage/RequestProposalBtn.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import { PrimaryButton } from "../Reusable";
+
+const RequestProposalBtn = () => {
+  return (
+    <PrimaryButton
+      title={"REQUEST FOR PROPOSAL"}
+      className="absolute right-0 top-1/2 -translate-y-1/2"
+    />
+  );
+};
+
+export default RequestProposalBtn;
