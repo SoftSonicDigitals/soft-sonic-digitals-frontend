e76bba881a79ae914c57b9646157e44744af8291
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-05 11:42:12 +1100

    [WEB-53] added route for the button

--- src/components/CaseStudiesPage/RequestProposalBtn.tsx
+++ src/components/CaseStudiesPage/RequestProposalBtn.tsx
@@ -1,10 +1,14 @@
 "use client";
 import { REQUEST_BTN_MESSAGE } from "@/constants/case_studies_page";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const RequestProposalBtn = () => {
+  const router = useRouter();
+
   return (
     <button
+      onClick={() => router.push("/contact")}
       className="hidden md:block fixed right-0 top-1/2 translate-x-1/2 -rotate-90 mr-5 px-5 py-2.5 hover:border-orange hover:border-[2px]  text-white tracking-wider  font-[700] rounded   hover:text-orange hover:bg-none   hover:bg-white bg-gradient-to-r from-orange to-pink
     "
     >
