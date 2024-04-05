d6bdce30d800b5fc79357b306b84ae74bca42c6c
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-06 10:14:22 +1100

    [WEB-54] downloaded poster and logo for pages

--- src/prototypes/case_study_pages/powerLook.ts
+++ src/prototypes/case_study_pages/powerLook.ts
@@ -34,4 +34,6 @@ export const POWERLOOK = {
     "/case_study_pages/powerlook/responsive_layout.webp",
   takeAway:
     "Magneto IT Solution helped Powerlook to create a new improved platform for their customers. This case study also depicts how improving website features and functionality can improve the customer shopping experience and also reduce the manual efforts of management.",
+  posterPath: "/case_study_pages/powerlook/poster.webp",
+  logo: " /case_study_pages/powerlook/logo.webp",
 };
