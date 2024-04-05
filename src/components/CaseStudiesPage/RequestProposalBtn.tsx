a4aaaebd22713c9f6eb30f542be18633514d05e4
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-05 11:40:50 +1100

    [WEB-53] added contsytants

--- src/components/CaseStudiesPage/RequestProposalBtn.tsx
+++ src/components/CaseStudiesPage/RequestProposalBtn.tsx
@@ -1,6 +1,6 @@
 "use client";
+import { REQUEST_BTN_MESSAGE } from "@/constants/case_studies_page";
 import React from "react";
-import { PrimaryButton } from "../Reusable";
 
 const RequestProposalBtn = () => {
   return (
@@ -8,7 +8,7 @@ const RequestProposalBtn = () => {
       className="hidden md:block fixed right-0 top-1/2 translate-x-1/2 -rotate-90 mr-5 px-5 py-2.5 hover:border-orange hover:border-[2px]  text-white tracking-wider  font-[700] rounded   hover:text-orange hover:bg-none   hover:bg-white bg-gradient-to-r from-orange to-pink
     "
     >
-      REQUEST FOR PROPOSAL
+      {REQUEST_BTN_MESSAGE}
     </button>
   );
 };
