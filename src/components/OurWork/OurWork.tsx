9414c64ba43927ce15fa22954537704a16c98f7c
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-18 20:18:51 +1000

    [WEB-56] added tailwind color

--- src/components/OurWork/OurWork.tsx
+++ src/components/OurWork/OurWork.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const OurWork = () => {
   return (
     <section id="our_w">
-      <div className="h-[100vh] flex-center ">
-        <h1 className="text-6xl">Our Work</h1>
+      <div className="h-[100vh] flex-center bg-gray-950">
+        <h1 className="text-6xl text-white">Our Work</h1>
       </div>
     </section>
   );
