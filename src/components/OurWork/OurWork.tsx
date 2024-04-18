bb694e9d46f5e9cc3a06f271cf7dc307d34d449b
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-18 21:21:53 +1000

    [WEB-56] added constants to the our work

--- src/components/OurWork/OurWork.tsx
+++ src/components/OurWork/OurWork.tsx
@@ -1,3 +1,4 @@
+import { OUR_WORK_DESCRIPTION, OUR_WORK_HEADING } from "@/constants/our_work";
 import React from "react";
 
 const OurWork = () => {
@@ -5,11 +6,11 @@ const OurWork = () => {
     <section id="our_work">
       <div className=" h-[calc(80vh-48px)]  xl:h-[100vh] flex-center bg-gray-950">
         <div className="flex flex-col max-w-[600px] lg:max-w-[720px]  text-white items-center justify-center text-center mx-6 md:mx-10 pt-[48px] lg:pt-[88px]">
-          <h1 className="text-3xl md:text-6xl  mb-3 tracking-wide">Our Work</h1>
+          <h1 className="text-3xl md:text-6xl  mb-3 tracking-wide">
+            {OUR_WORK_HEADING}
+          </h1>
           <p className="font-[500] tracking-wider text-base">
-            Our years of expertise in the industry helped us to deliver bespoke
-            smartphone, tablet, and web-based development services within
-            allotted time and budget without compromising on quality.
+            {OUR_WORK_DESCRIPTION}
           </p>
         </div>
       </div>
