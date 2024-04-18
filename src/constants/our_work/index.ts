717c2cbce83f441a554d0ee91a18b3ee2a470a89
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-18 21:21:53 +1000

    [WEB-56] added constants to the our work

---
+++ src/constants/our_work/index.ts
@@ -0,0 +1,3 @@
+export const OUR_WORK_HEADING = "Our Work";
+export const OUR_WORK_DESCRIPTION =
+  "Our years of expertise in the industry helped us to deliver bespoke smartphone, tablet, and web-based development services within allotted time and budget without compromising on quality.";
