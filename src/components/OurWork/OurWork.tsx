8b52e72aa0f027e034ca8c5fab3351d9656954b1
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-18 20:33:43 +1000

    [WEB-56] added the template for the our work section

--- src/components/OurWork/OurWork.tsx
+++ src/components/OurWork/OurWork.tsx
@@ -2,9 +2,16 @@ import React from "react";
 
 const OurWork = () => {
   return (
-    <section id="our_w">
+    <section id="our_work">
       <div className="h-[100vh] flex-center bg-gray-950">
-        <h1 className="text-6xl text-white">Our Work</h1>
+        <div className="flex flex-col max-w-[720px] text-white items-center justify-center text-center">
+          <h1 className="text-6xl mb-3 ">Our Work</h1>
+          <p className="font-[500] tracking-wider ">
+            Our years of expertise in the industry helped us to deliver bespoke
+            smartphone, tablet, and web-based development services within
+            allotted time and budget without compromising on quality.
+          </p>
+        </div>
       </div>
     </section>
   );
