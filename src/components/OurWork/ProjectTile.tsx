44322fd65a1a6c7b37ffb97efeb911b159837b27
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-25 12:17:17 +1000

    [WEB-57] adjust the hover effect

--- src/components/OurWork/ProjectTile.tsx
+++ src/components/OurWork/ProjectTile.tsx
@@ -14,14 +14,14 @@ const ProjectTile = () => {
           height={720}
         />
         <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 pointer-events-none group-hover:opacity-40 group-hover:pointer-events-auto" />
-        <div className="absolute top-0  w-full h-full  ">
+        <div className="absolute top-0  w-full h-full opacity-0 pointer-events-none group-hover:opacity-100 group-hover:pointer-events-auto ">
           <div className="flex-center flex-col w-full h-full">
             <h2 className="text-4xl tracking-wide text-white font-[500] mb-12 text-center">
-              NEOM
+              Featherdown
             </h2>
             <p className="text-lg text-white tracking-wide px-4 text-center mb-8">
-              Developed NEOMs custom ticketing portal by integrating Shopify
-              with SAP
+              It a portal from where customers can book camps and at the same
+              time be able to manage the inventory with enhanced accountability.
             </p>
             <Link
               href="/"
