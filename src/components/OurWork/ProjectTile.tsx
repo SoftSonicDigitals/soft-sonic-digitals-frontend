ce7750c44e2dacf5541f76035fe80d42d1b3feaa
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-25 12:13:16 +1000

    [WEB-57] added view case study button

--- src/components/OurWork/ProjectTile.tsx
+++ src/components/OurWork/ProjectTile.tsx
@@ -6,28 +6,32 @@ import React from "react";
 const ProjectTile = () => {
   return (
     <>
-      <Link href={"#"}>
-        <div className="lg:max-w-[400px] lg:max-h-[400px] relative group">
-          <Image
-            src={FEATHER_DOWN.ourWorkPageDisplayPath}
-            alt="display"
-            width={720}
-            height={720}
-          />
-          <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 pointer-events-none group-hover:opacity-40 group-hover:pointer-events-auto" />
-          <div className="absolute top-0  w-full h-full  ">
-            <div className="flex-center flex-col w-full h-full">
-              <h2 className="text-4xl tracking-wide text-white font-[500] mb-12 text-center">
-                NEOM
-              </h2>
-              <p className="text-lg text-white tracking-wide px-4 text-center">
-                Developed NEOMs custom ticketing portal by integrating Shopify
-                with SAP
-              </p>
-            </div>
+      <div className="lg:max-w-[400px] lg:max-h-[400px] relative group">
+        <Image
+          src={FEATHER_DOWN.ourWorkPageDisplayPath}
+          alt="display"
+          width={720}
+          height={720}
+        />
+        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 pointer-events-none group-hover:opacity-40 group-hover:pointer-events-auto" />
+        <div className="absolute top-0  w-full h-full  ">
+          <div className="flex-center flex-col w-full h-full">
+            <h2 className="text-4xl tracking-wide text-white font-[500] mb-12 text-center">
+              NEOM
+            </h2>
+            <p className="text-lg text-white tracking-wide px-4 text-center mb-8">
+              Developed NEOMs custom ticketing portal by integrating Shopify
+              with SAP
+            </p>
+            <Link
+              href="/"
+              className="px-14 py-2.5 text-white font-[700] border-2 border-white text-sm tracking-wider hover:bg-white hover:text-gray-900 transition duration-200"
+            >
+              VIEW CASE STUDY
+            </Link>
           </div>
         </div>
-      </Link>
+      </div>
     </>
   );
 };
