34e6a7015d5b99135f2a29eb49cd1853a97b16e8
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-25 12:04:26 +1000

    [WEB-57] added overlay on hover

--- src/app/(layout_pages)/case-studies/page.tsx
+++ src/app/(layout_pages)/case-studies/page.tsx
@@ -6,7 +6,7 @@ const page = () => {
     <main>
       <OurWork />
       <section id="projects">
-        <div className="wrapper_container flex-center">
+        <div className="wrapper_container flex-center sm:mt-4 mb-12 lg:mb-24">
           <ProjectTile />
         </div>
       </section>
