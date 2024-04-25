51f953e2261922d776f1d888c1ccbc55adba1fa0
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-25 12:56:06 +1000

    [WEB-57] mapped over the case study pages to display tiles

--- src/app/(layout_pages)/case-studies/page.tsx
+++ src/app/(layout_pages)/case-studies/page.tsx
@@ -1,4 +1,5 @@
 import { OurWork, ProjectTile } from "@/components/OurWork";
+import { CASE_STUDIES_ARRAY } from "@/prototypes/case_study_pages";
 import React from "react";
 
 const page = () => {
@@ -7,9 +8,16 @@ const page = () => {
       <OurWork />
       <section id="projects">
         <div className="wrapper_container grid md:grid-cols-2 lg:grid-cols-3 sm:mt-4 mb-12 lg:mb-24 max-w-[1190px]">
-          <ProjectTile />
-          <ProjectTile />
-          <ProjectTile />
+          {CASE_STUDIES_ARRAY.map(
+            ({ caseName, displayImg, displayDescription }, index) => (
+              <ProjectTile
+                key={index}
+                title={caseName}
+                description={displayDescription}
+                displayImg={displayImg}
+              />
+            )
+          )}
         </div>
       </section>
     </main>
--- src/components/OurWork/ProjectTile.tsx
+++ src/components/OurWork/ProjectTile.tsx
@@ -6,7 +6,6 @@ type ProjectTileProps = {
   displayImg: string;
   title: string;
   description: string;
-  link: string;
 };
 
 const ProjectTile = ({ displayImg, title, description }: ProjectTileProps) => {
@@ -17,14 +16,14 @@ const ProjectTile = ({ displayImg, title, description }: ProjectTileProps) => {
         <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 pointer-events-none group-hover:opacity-40 group-hover:pointer-events-auto" />
         <div className="absolute top-0  w-full h-full opacity-0 pointer-events-none group-hover:opacity-100 group-hover:pointer-events-auto ">
           <div className="flex-center flex-col w-full h-full">
-            <h2 className="text-4xl tracking-wide text-white font-[500] mb-12 text-center">
+            <h2 className="text-4xl tracking-wide text-white font-[500] mb-12 text-center capitalize">
               {title}
             </h2>
             <p className="text-lg text-white tracking-wide px-4 text-center mb-8">
               {description}
             </p>
             <Link
-              href={""}
+              href={`/case-study/${title.replaceAll(" ", "_")}`}
               className="px-14 py-2.5 text-white font-[700] border-2 border-white text-sm tracking-wider hover:bg-white hover:text-gray-900 transition duration-200"
             >
               {VIEW_CASE_STUDY_BTN}
