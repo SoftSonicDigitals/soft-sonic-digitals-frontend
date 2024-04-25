065ca651aabb1913a78d44c4adf47322c053ed76
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-25 12:04:26 +1000

    [WEB-57] added overlay on hover

--- src/components/OurWork/ProjectTile.tsx
+++ src/components/OurWork/ProjectTile.tsx
@@ -7,13 +7,25 @@ const ProjectTile = () => {
   return (
     <>
       <Link href={"#"}>
-        <div className="lg:max-w-[400px] lg:max-h-[400px]">
+        <div className="lg:max-w-[400px] lg:max-h-[400px] relative group">
           <Image
             src={FEATHER_DOWN.ourWorkPageDisplayPath}
             alt="display"
             width={720}
             height={720}
           />
+          <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 pointer-events-none group-hover:opacity-40 group-hover:pointer-events-auto" />
+          <div className="absolute top-0  w-full h-full  ">
+            <div className="flex-center flex-col w-full h-full">
+              <h2 className="text-4xl tracking-wide text-white font-[500] mb-12 text-center">
+                NEOM
+              </h2>
+              <p className="text-lg text-white tracking-wide px-4 text-center">
+                Developed NEOMs custom ticketing portal by integrating Shopify
+                with SAP
+              </p>
+            </div>
+          </div>
         </div>
       </Link>
     </>
