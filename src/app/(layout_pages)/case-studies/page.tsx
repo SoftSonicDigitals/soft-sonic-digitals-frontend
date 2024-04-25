b2784ca49b9ca234ad78a8bfdc48a1bcc3e4b7ed
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-25 12:29:50 +1000

    [WEB-57] styled the grid

--- src/app/(layout_pages)/case-studies/page.tsx
+++ src/app/(layout_pages)/case-studies/page.tsx
@@ -6,7 +6,9 @@ const page = () => {
     <main>
       <OurWork />
       <section id="projects">
-        <div className="wrapper_container flex-center sm:mt-4 mb-12 lg:mb-24">
+        <div className="wrapper_container grid md:grid-cols-2 lg:grid-cols-3 sm:mt-4 mb-12 lg:mb-24 max-w-[1190px]">
+          <ProjectTile />
+          <ProjectTile />
           <ProjectTile />
         </div>
       </section>
