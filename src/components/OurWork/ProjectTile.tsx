4f1d5b413f86a91770dfd6ddb0d9bb24361a87a2
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-25 13:03:13 +1000

    [WEB-57] added commments and adjusted the stylings

--- src/components/OurWork/ProjectTile.tsx
+++ src/components/OurWork/ProjectTile.tsx
@@ -11,9 +11,12 @@ type ProjectTileProps = {
 const ProjectTile = ({ displayImg, title, description }: ProjectTileProps) => {
   return (
     <>
-      <div className="lg:max-w-[400px] lg:max-h-[400px] relative group overflow-hidden">
+      <div className="lg:max-w-[400px] lg:max-h-[400px] relative group ">
         <Image src={displayImg} alt="display" width={720} height={720} />
+
+        {/* overlay */}
         <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 pointer-events-none group-hover:opacity-40 group-hover:pointer-events-auto" />
+
         <div className="absolute top-0  w-full h-full opacity-0 pointer-events-none group-hover:opacity-100 group-hover:pointer-events-auto ">
           <div className="flex-center flex-col w-full h-full">
             <h2 className="text-4xl tracking-wide text-white font-[500] mb-12 text-center capitalize">
