3adeeef42cc193b6d0b2e005ba25abed31f8e43b
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-25 12:35:59 +1000

    [WEB-57] made the componennt require props

--- src/constants/our_work/index.ts
+++ src/constants/our_work/index.ts
@@ -1,3 +1,4 @@
 export const OUR_WORK_HEADING = "Our Work";
 export const OUR_WORK_DESCRIPTION =
   "Our years of expertise in the industry helped us to deliver bespoke smartphone, tablet, and web-based development services within allotted time and budget without compromising on quality.";
+export const VIEW_CASE_STUDY_BTN = "VIEW CASE STUDY";
