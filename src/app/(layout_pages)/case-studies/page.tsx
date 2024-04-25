eb3488c98af17971975dd6f0a13942b24d2728be
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-25 10:44:49 +1000

    [WEB-57] added the project tiles component

--- src/app/(layout_pages)/case-studies/page.tsx
+++ src/app/(layout_pages)/case-studies/page.tsx
@@ -1,10 +1,11 @@
-import { OurWork } from "@/components/OurWork";
+import { OurWork, ProjectTile } from "@/components/OurWork";
 import React from "react";
 
 const page = () => {
   return (
     <main>
       <OurWork />
+      <ProjectTile />
     </main>
   );
 };
