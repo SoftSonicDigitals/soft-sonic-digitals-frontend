d8d7fef750fe36cd655d2b96d774a4a435b1cd61
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-28 19:33:49 +1000

    [WEB-60] added the background

--- src/components/TemplateComponents/HeroTemplate.tsx
+++ src/components/TemplateComponents/HeroTemplate.tsx
@@ -3,15 +3,15 @@ import React from "react";
 
 const HeroTemplate = () => {
   return (
-    <section
-      style={{
-        background: "#EFEFEF url(/prototype_page/hero.webp)",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundPosition: "top",
-      }}
-    >
-      <div className="h-[100vh] w-[100vw]"></div>
+    <section className="mb-24">
+      <div className="relative w-[100vw] h-[70vh]">
+        <Image
+          src="/prototype_page/hero.webp"
+          alt="hero"
+          fill
+          className="object-cover object-[85%]"
+        />
+      </div>
     </section>
   );
 };
