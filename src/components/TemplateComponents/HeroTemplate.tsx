441f9a35a1510265f027c02e89565a7b5d27f313
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-28 19:56:09 +1000

    [WEB-60] added the headings

--- src/components/TemplateComponents/HeroTemplate.tsx
+++ src/components/TemplateComponents/HeroTemplate.tsx
@@ -1,16 +1,36 @@
 import Image from "next/image";
 import React from "react";
+import { PrimaryButton } from "../Reusable";
 
 const HeroTemplate = () => {
   return (
     <section className="mb-24">
-      <div className="relative w-[100vw] h-[70vh]">
-        <Image
-          src="/prototype_page/hero.webp"
-          alt="hero"
-          fill
-          className="object-cover object-[85%]"
-        />
+      <div className="pt-[48px] lg:pt-[88px]">
+        <div className="relative w-[100vw] h-[55vh]">
+          <Image
+            src="/prototype_page/hero.webp"
+            alt="hero"
+            fill
+            className="object-cover object-[85%] md:object-top"
+          />
+          <div className="absolute w-full h-full ">
+            <div className="wrapper_container h-full flex flex-col justify-center">
+              <h2 className="text-[2.5rem] font-[600] text-gray-800 mb-8 tracking-tight">
+                Transform your Jewelry Business Online!
+              </h2>
+              <p className="text-lg font-[500] text-gray-700 mb-2">
+                We are the One Stop Shop for all your Jewelry website design &
+                development needs.
+              </p>
+              <div>
+                <PrimaryButton
+                  title="REQUEST A PROPOSAL"
+                  className="px-2 py-2.5"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
