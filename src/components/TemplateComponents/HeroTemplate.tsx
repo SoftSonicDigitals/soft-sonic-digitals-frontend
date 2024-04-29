3fe688ca654518479075a04746e790de73866c8c
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-04-29 14:22:47 +1000

    [WEB-60] made the heading responsive

--- src/components/TemplateComponents/HeroTemplate.tsx
+++ src/components/TemplateComponents/HeroTemplate.tsx
@@ -6,7 +6,7 @@ const HeroTemplate = () => {
   return (
     <section className="mb-24">
       <div className="pt-[48px] lg:pt-[88px]">
-        <div className="relative w-[100vw] h-[55vh]">
+        <div className="relative w-[100vw] h-[60vh] xl:h-[100vh] 2xl:h-[55vh]">
           <Image
             src="/prototype_page/hero.webp"
             alt="hero"
@@ -14,11 +14,11 @@ const HeroTemplate = () => {
             className="object-cover object-[85%] md:object-top"
           />
           <div className="absolute w-full h-full ">
-            <div className="wrapper_container h-full flex flex-col justify-center">
-              <h2 className="text-[2.5rem] font-[600] text-gray-800 mb-8 tracking-tight">
+            <div className="wrapper_container h-full flex flex-col justify-center items-center lg:items-start text-center lg:text-left">
+              <h2 className="text-3xl lg:text-[2.5rem]  font-[600] text-gray-800 mb-5 tracking-tight ">
                 Transform your Jewelry Business Online!
               </h2>
-              <p className="text-lg font-[500] text-gray-700 mb-2">
+              <p className="text-lg font-[500] text-gray-700 mb-2 ">
                 We are the One Stop Shop for all your Jewelry website design &
                 development needs.
               </p>
