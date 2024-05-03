da3dfe7617b8b74b6bba11806b6e146e7ab4f030
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-03 19:20:09 +1000

    [WEB-61] changed the folder name of assests of industry page

--- src/prototypes/industries/jewellery.ts
+++ src/prototypes/industries/jewellery.ts
@@ -1,5 +1,5 @@
 export const JEWELLERY = {
-  imgPath: "/prototype_page/hero.webp",
+  imgPath: "/industry/hero.webp",
   heading: "Transform your Jewelry Business Online!",
   description:
     "We are the One Stop Shop for all your Jewelry website design & development needs.",
