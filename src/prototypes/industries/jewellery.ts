8b78226a0c05d847d327d702b2ae6fadc4a75330
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-03 18:14:06 +1000

    [WEB-60] added the prototype array for the industry hero section

---
+++ src/prototypes/industries/jewellery.ts
@@ -0,0 +1,9 @@
+export const JEWELLERY = {
+  imgPath: "/prototype_page/hero.webp",
+  heading: "Transform your Jewelry Business Online!",
+  description:
+    "We are the One Stop Shop for all your Jewelry website design & development needs.",
+  btnText: "REQUEST A PROPOSAL",
+  btnStyles:
+    "bg-gradient-to-l from-orange to-pink hover:bg-gradient-to-r from-pink to-orange",
+};
