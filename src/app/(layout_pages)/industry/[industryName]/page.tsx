cec5825c2e11569ec9dc058b1d8e9eeed3d0bb37
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-03 20:14:22 +1000

    [WEB-61] mapped the tiles over the array details

--- src/app/(layout_pages)/industry/[industryName]/page.tsx
+++ src/app/(layout_pages)/industry/[industryName]/page.tsx
@@ -2,7 +2,10 @@ import {
   IndustryHero,
   SpecializationTiles,
 } from "@/components/IndustryComponents";
-import { JEWELLERY } from "@/prototypes/industries/jewellery";
+import {
+  JEWELLERY,
+  SPECIALIZATION_TILES_DATA,
+} from "@/prototypes/industries/jewellery";
 import React from "react";
 
 const page = () => {
@@ -10,11 +13,11 @@ const page = () => {
     <>
       <IndustryHero
         imgPath={JEWELLERY.imgPath}
-        heading="Transform your Jewellry Business Online!"
-        description="We are the One Stop Shop for all your Jewellry website design & development needs."
-        btnText="REQUEST A PROPOSAL"
+        heading={JEWELLERY.heading}
+        description={JEWELLERY.description}
+        btnText={JEWELLERY.btnText}
       />
-      <SpecializationTiles />
+      <SpecializationTiles tilesDataArr={SPECIALIZATION_TILES_DATA} />
     </>
   );
 };
--- src/prototypes/industries/jewellery.ts
+++ src/prototypes/industries/jewellery.ts
@@ -4,6 +4,15 @@ export const JEWELLERY = {
   description:
     "We are the One Stop Shop for all your Jewelry website design & development needs.",
   btnText: "REQUEST A PROPOSAL",
-  btnStyles:
-    "bg-gradient-to-l from-orange to-pink hover:bg-gradient-to-r from-pink to-orange",
 };
+
+export const SPECIALIZATION_TILES_DATA = [
+  { imgPath: "/industry/b2c.png", title: "For B2C Business Model" },
+  { imgPath: "/industry/b2b.png", title: "For B2B Business Model" },
+  { imgPath: "/industry/d2c.png", title: "For D2C Business Model" },
+  { imgPath: "/industry/api.png", title: "For Flawless API Integration" },
+  {
+    imgPath: "/industry/marketplace.png",
+    title: "For Marketplace Development",
+  },
+];
