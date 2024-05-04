bc2e1a2e4ab7489f1625ddc493d65a3745e78161
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-04 19:09:50 +1000

    [WEB-62] added props

--- src/components/Reusable/InfoSection.tsx
+++ src/components/Reusable/InfoSection.tsx
@@ -1,7 +1,17 @@
 import Image from "next/image";
 import React from "react";
-
-const InfoSection = () => {
+type InfoSectionProps = {
+  heading: string;
+  subHeading: string;
+  description: string;
+  imgpath: string;
+};
+const InfoSection = ({
+  heading,
+  subHeading,
+  description,
+  imgpath,
+}: InfoSectionProps) => {
   return (
     <section>
       <div className="wrapper_container mb-24">
@@ -10,40 +20,23 @@ const InfoSection = () => {
             <h1
               className={` text-[1.7rem] md:text-[2.1rem] font-[500] mb-8 text-center md:text-left`}
             >
-              Digital Asset Management
+              {heading}
             </h1>
 
             <h3
               className={`text-xl md:text-2xl lg:text-xl  font-[500] text-gray-900 text-center md:text-left  tracking-wider`}
             >
-              DAM is a centralized platform for jewelry design asset sharing,
-              version control, and task assignment. It enables smooth
-              communication, enhances collaboration, and improves overall
-              workflow management. It can effortlessly conduct global jewelry
-              design collaboration support and co-marketing campaigns for
-              multiple stakeholders, such as designers, photographers,
-              marketers, and customers. It will help to reduce offline marketing
-              material wastage.
+              {subHeading}
             </h3>
             <br />
             <p
               className={`text-[1rem] md:text-lg lg:text-xl font-[500] text-gray-800  md:text-left  tracking-wider text-center `}
             >
-              Efficient design inventory management is essential to accurately
-              track and categorize jewelry and diamond assets. Media asset
-              management tools help businesses organize design inventory, track
-              stock levels, find specific items, and maintain a consistent brand
-              image. It saves time and reduces errors associated with manual
-              inventory management processes.
+              {description}
             </p>
           </div>
           <div className="basis-full">
-            <Image
-              src={"/industry/info1.png"}
-              alt="assets"
-              width={678}
-              height={538}
-            />
+            <Image src={`${imgpath}`} alt="assets" width={678} height={538} />
           </div>
         </div>
       </div>
