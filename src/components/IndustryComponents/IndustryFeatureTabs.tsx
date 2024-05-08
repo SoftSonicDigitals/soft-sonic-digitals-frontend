78217bfe57a3a6df4cf9a4817b080f1930e715a9
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-08 13:54:31 +1000

    [WEB-64] refactored the feature details by providing props

--- src/components/IndustryComponents/IndustryFeatureTabs.tsx
+++ src/components/IndustryComponents/IndustryFeatureTabs.tsx
@@ -1,9 +1,10 @@
 "use client";
 import { JEWELLERY } from "@/prototypes/industries/jewellery";
-import Image from "next/image";
 import React, { useState } from "react";
+import FeatureDetails from "./FeatureDetails";
+
 const IndustryFeatureTabs = () => {
-  const [toogleState, setToogleState] = useState<number>(0);
+  const [toogleIndex, setToogleIndex] = useState<number>(0);
 
   return (
     <section id="features">
@@ -20,12 +21,12 @@ const IndustryFeatureTabs = () => {
               <button
                 key={index}
                 className={`text-[17px] font-[600] relative text-gray-700 rounded-t px-2 py-3 mx-[2px] border-gray-600  border-[1px] ${
-                  toogleState === index
+                  toogleIndex === index
                     ? " border-[1px] text-orange bg-white after:w-full after:h-[2px]  after:absolute after:left-0 after:-bottom-0.5 after:bg-white "
                     : " bg-white-100  hover:bg-white-200 border-white-100"
                 }`}
                 onClick={() => {
-                  setToogleState(index);
+                  setToogleIndex(index);
                 }}
               >
                 {feature.heading}
@@ -33,37 +34,18 @@ const IndustryFeatureTabs = () => {
             ))}
           </div>
 
-          {
-            <div className="flex-center gap-8 px-12">
-              <div className="basis-full">
-                <Image
-                  src={JEWELLERY.featuresSection.features[toogleState].imgPath}
-                  alt="assets"
-                  width={587}
-                  height={362}
-                />
-              </div>
-              <div className={`basis-full  max-w-[700px]  `}>
-                <h1
-                  className={` text-[1.7rem] md:text-[2.1rem] font-[500] mb-8 text-center md:text-left capitalize`}
-                >
-                  {JEWELLERY.featuresSection.features[toogleState].heading}
-                </h1>
-
-                <h3
-                  className={`text-xl md:text-2xl lg:text-xl  font-[500] text-gray-900 text-center md:text-left  tracking-wider`}
-                >
-                  {JEWELLERY.featuresSection.features[toogleState].subHeading}
-                </h3>
-                <br />
-                <p
-                  className={`text-[1rem] md:text-lg lg:text-xl font-[500] text-gray-800  md:text-left  tracking-wider text-center whitespace-pre-line`}
-                >
-                  {JEWELLERY.featuresSection.features[toogleState].description}
-                </p>
-              </div>
-            </div>
-          }
+          <div className="flex-center gap-8 px-12">
+            <FeatureDetails
+              heading={JEWELLERY.featuresSection.features[toogleIndex].heading}
+              subHeading={
+                JEWELLERY.featuresSection.features[toogleIndex].subHeading
+              }
+              description={
+                JEWELLERY.featuresSection.features[toogleIndex].description
+              }
+              imgPath={JEWELLERY.featuresSection.features[toogleIndex].imgPath}
+            />
+          </div>
         </div>
       </div>
     </section>
