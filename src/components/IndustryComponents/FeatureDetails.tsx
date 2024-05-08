39154f619d4cbebafb9564e2d6e5232c921f7117
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-08 13:48:55 +1000

    [WEB-64] refactored the featured details

---
+++ src/components/IndustryComponents/FeatureDetails.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import React from "react";
+type FeatureDetailsProps = {
+  heading: string;
+  subHeading: string;
+  description: string;
+  imgPath: string;
+};
+
+const FeatureDetails = ({
+  heading,
+  subHeading,
+  description,
+  imgPath,
+}: FeatureDetailsProps) => {
+  return (
+    <>
+      <div className="basis-full">
+        <Image src={imgPath} alt="assets" width={587} height={362} />
+      </div>
+      <div className={`basis-full  max-w-[700px]  `}>
+        <h1
+          className={` text-[1.7rem] md:text-[2.1rem] font-[500] mb-8 text-center md:text-left capitalize`}
+        >
+          {heading}
+        </h1>
+
+        <h3
+          className={`text-xl md:text-2xl lg:text-xl  font-[500] text-gray-900 text-center md:text-left  tracking-wider`}
+        >
+          {subHeading}
+        </h3>
+        <br />
+        <p
+          className={`text-[1rem] md:text-lg lg:text-xl font-[500] text-gray-800  md:text-left  tracking-wider text-center whitespace-pre-line`}
+        >
+          {description}
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default FeatureDetails;
--- src/components/IndustryComponents/index.ts
+++ src/components/IndustryComponents/index.ts
@@ -1,5 +1,11 @@
+import FeatureDetails from "./FeatureDetails";
 import IndustryFeatureTabs from "./IndustryFeatureTabs";
 import IndustryHero from "./IndustryHero";
 import SpecializationTiles from "./SpecializationTiles";
 
-export { IndustryHero, SpecializationTiles, IndustryFeatureTabs };
+export {
+  IndustryHero,
+  SpecializationTiles,
+  IndustryFeatureTabs,
+  FeatureDetails,
+};
