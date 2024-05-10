eade63692e67a2f3e7a88ea48a678345f657b511
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-10 19:31:33 +1000

    [WEB-68] adjusted the stylings for the industry banner

--- src/components/IndustryComponents/IndustryBanner.tsx
+++ src/components/IndustryComponents/IndustryBanner.tsx
@@ -12,7 +12,7 @@ const IndustryBanner = () => {
             subHeading="Communication is the key for us to understand each other. Allow us to understand your requirements or queries. Present us with an opportunity to serve you."
             subHeadingStyles="text-white 2xl:px-20"
             mainHeadingStyles="mb-8 text-white"
-            containerStyles="mb-12 px-8  md:px-12 lg:px-8 xl:px-12 "
+            containerStyles="mb-12 px-4  md:px-12 lg:px-8 xl:px-12  text-center"
           />
           <div className="flex-center w-full">
             <GetInTouchContactBtn>HIRE NOW</GetInTouchContactBtn>
