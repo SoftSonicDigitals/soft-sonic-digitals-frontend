6c7b300bbd70a4b5e72992c31f734287b6c9962a
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-10 21:41:32 +1000

    [WEB-68] provided props to the banner component

--- src/components/IndustryComponents/IndustryBanner.tsx
+++ src/components/IndustryComponents/IndustryBanner.tsx
@@ -2,20 +2,26 @@ import React from "react";
 import { GetInTouchContactBtn } from "../GetInTouch";
 import { PrimaryStyledHeadings } from "../Reusable";
 
-const IndustryBanner = () => {
+type IndustryBanner = {
+  title: string;
+  description: string;
+  btnText: string;
+};
+
+const IndustryBanner = ({ title, description, btnText }: IndustryBanner) => {
   return (
     <section id="banner" className="bg-black">
       <div className="wrapper_container py-8 mb-12 md:mb-24">
         <div className="my-3 ">
           <PrimaryStyledHeadings
-            mainHeading="Talk to our gems & jewelry web designer consultant"
-            subHeading="Communication is the key for us to understand each other. Allow us to understand your requirements or queries. Present us with an opportunity to serve you."
+            mainHeading={title}
+            subHeading={description}
             subHeadingStyles="text-white 2xl:px-20"
             mainHeadingStyles="mb-8 text-white"
             containerStyles="mb-12 px-4  md:px-12 lg:px-8 xl:px-12  text-center"
           />
           <div className="flex-center w-full">
-            <GetInTouchContactBtn>HIRE NOW</GetInTouchContactBtn>
+            <GetInTouchContactBtn>{btnText}</GetInTouchContactBtn>
           </div>
         </div>
       </div>
