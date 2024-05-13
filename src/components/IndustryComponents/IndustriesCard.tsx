0de264e5e4e5da37ba17f2529c8802d0214a4599
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-13 15:56:55 +1000

    [WEB-69] mapped over the array and provided props to the industry card component

--- src/components/IndustryComponents/IndustriesCard.tsx
+++ src/components/IndustryComponents/IndustriesCard.tsx
@@ -1,20 +1,14 @@
 import Image from "next/image";
 import React from "react";
-
-const IndustriesCard = ({}) => {
+type IndustriesCardProps = {
+  name: string;
+  image: string;
+};
+const IndustriesCard = ({ name, image }: IndustriesCardProps) => {
   return (
-    <div
-      key={index}
-      className=" px-10 py-3.5 rounded shadow-lg max-w-[156px] max-h-[152px]"
-    >
+    <div className=" px-10 py-3.5 rounded shadow-lg max-w-[156px] max-h-[152px]">
       <div className="flex-center flex-col ">
-        <Image
-          src={img}
-          alt="industry"
-          width={72}
-          height={72}
-          className="mb-6"
-        />
+        <Image src={image} alt={name} width={72} height={72} className="mb-6" />
         <p className="text-center text-lg font-[500]">{name}</p>
       </div>
     </div>
