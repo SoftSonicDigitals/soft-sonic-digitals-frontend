6e18c752b8a7ae9f55b1f50d798222903a9cb8c6
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-16 00:05:33 +1000

    [WEB-70] mapped over the ecommerce frame work arrya and rendered teh logos

--- src/components/IndustryComponents/EcommerceFrameworks.tsx
+++ src/components/IndustryComponents/EcommerceFrameworks.tsx
@@ -1,9 +1,20 @@
+import { INDUSTRY_ECOMMERCE_FRAMEWORKS } from "@/constants/industry";
+import Image from "next/image";
 import React from "react";
 
 const EcommerceFrameworks = () => {
   return (
     <section id="ecommerce_frameworks">
-      <div className="wrapper_container mb-24">ecommerce Frmae works</div>
+      <div className="wrapper_container mb-24">
+        <div className=" flex flex-col gap-4">
+          {INDUSTRY_ECOMMERCE_FRAMEWORKS.map(({ name, logo }, index) => (
+            <div key={index} className="flex flex-col flex-center">
+              <Image src={logo} alt={`${name}-logo`} width={80} height={80} />
+              <p className="text-lg font-[500] mt-5">{name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
