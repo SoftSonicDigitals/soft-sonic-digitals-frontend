05f5f6c4d2b968b20776bc791af39d53b9b23ccb
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-15 23:43:10 +1000

    [WEB-70] set up the section

--- src/components/IndustryComponents/EcommerceFrameworks.tsx
+++ src/components/IndustryComponents/EcommerceFrameworks.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const EcommerceFrameworks = () => {
-  return <div>EcommerceFrameworks</div>;
+  return (
+    <section id="ecommerce_frameworks">
+      <div className="wrapper_container mb-24">ecommerce Frmae works</div>
+    </section>
+  );
 };
 
 export default EcommerceFrameworks;
