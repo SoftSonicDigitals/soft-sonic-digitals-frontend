aa5ade1509314f5bdee87568a50e1226104aebee
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-16 22:03:18 +1000

    [WEB-73] moved the constant to the coonstant folder from the array

--- src/constants/industry/index.ts
+++ src/constants/industry/index.ts
@@ -66,3 +66,14 @@ export const INDUSTRY_ECOMMERCE_FRAMEWORKS: EcommerceFrameworksArr = [
   { name: "Salesforce", logo: "/industry/ecommerce_frameworks/salesforce.png" },
   { name: "Node.js", logo: "/industry/ecommerce_frameworks/node-js-logo.png" },
 ];
+
+export const SPECIALIZATION_TILES_DATA = [
+  { imgPath: "/industry/b2c.png", title: "For B2C Business Model" },
+  { imgPath: "/industry/b2b.png", title: "For B2B Business Model" },
+  { imgPath: "/industry/d2c.png", title: "For D2C Business Model" },
+  { imgPath: "/industry/api.png", title: "For Flawless API Integration" },
+  {
+    imgPath: "/industry/marketplace.png",
+    title: "For Marketplace Development",
+  },
+];
