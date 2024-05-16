7eb990a0a2f6e1b244fe010971e63d4d35a0f067
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-16 19:39:56 +1000

    [WEB-72] rendered the ecommerce project banner

--- src/components/IndustryComponents/EcommerceProjectBanner.tsx
+++ src/components/IndustryComponents/EcommerceProjectBanner.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { PrimaryStyledHeadings } from "../Reusable";
+import { GetInTouchContactBtn } from "../GetInTouch";
 
 const EcommerceProjectBanner = () => {
   return (
     <section id="banner" className="bg-black">
       <div className="wrapper_container py-8 mb-12 md:mb-24">
-        <div className="my-3 "></div>
+        <div className="flex justify-around items-center">
+          <h1 className="text-xl md:text-2xl font-[500] text-white">
+            Have an eCommerce project in mind? We’d love to make your ideas into
+            reality.
+          </h1>
+          <GetInTouchContactBtn>LET&apos;S TALK </GetInTouchContactBtn>
+        </div>
       </div>
     </section>
   );
