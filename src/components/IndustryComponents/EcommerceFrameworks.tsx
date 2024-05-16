0872df41adbd013359ee59afb0b795efb966a1e0
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-16 18:32:49 +1000

    [WEB-70] added the the slider

--- src/components/IndustryComponents/EcommerceFrameworks.tsx
+++ src/components/IndustryComponents/EcommerceFrameworks.tsx
@@ -1,18 +1,50 @@
+"use client";
+
 import { INDUSTRY_ECOMMERCE_FRAMEWORKS } from "@/constants/industry";
 import Image from "next/image";
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { PrimaryStyledHeadings } from "../Reusable";
 
 const EcommerceFrameworks = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 300,
+    autoplaySpeed: 1000,
+    cssEase: "ease-in-out",
+  };
   return (
     <section id="ecommerce_frameworks">
-      <div className="wrapper_container mb-24">
-        <div className=" flex flex-col gap-4">
-          {INDUSTRY_ECOMMERCE_FRAMEWORKS.map(({ name, logo }, index) => (
-            <div key={index} className="flex flex-col flex-center">
-              <Image src={logo} alt={`${name}-logo`} width={80} height={80} />
-              <p className="text-lg font-[500] mt-5">{name}</p>
-            </div>
-          ))}
+      <div className="wrapper_container mb-24 ">
+        <PrimaryStyledHeadings
+          mainHeading="eCommerce Frameworks We Work With"
+          containerStyles="mb-24"
+        />
+        <div className="mx-20">
+          <Slider {...settings}>
+            {INDUSTRY_ECOMMERCE_FRAMEWORKS.map(({ name, logo }, index) => (
+              <div
+                key={index}
+                className="flex flex-col item-center justify-center"
+              >
+                <div className="flex-center">
+                  <Image
+                    src={logo}
+                    alt={`${name}-logo`}
+                    width={80}
+                    height={80}
+                  />
+                </div>
+                <p className="text-lg font-[500] mt-5 text-center">{name}</p>
+              </div>
+            ))}
+          </Slider>
         </div>
       </div>
     </section>
