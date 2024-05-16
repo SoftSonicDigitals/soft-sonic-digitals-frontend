5b2f44211f401c4741831b97c64a8ff7a9265463
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-16 19:47:43 +1000

    [WEB-72] added the constants and adjusted the getintouch contact button for class name

--- src/components/IndustryComponents/EcommerceProjectBanner.tsx
+++ src/components/IndustryComponents/EcommerceProjectBanner.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { PrimaryStyledHeadings } from "../Reusable";
 import { GetInTouchContactBtn } from "../GetInTouch";
+import { ECOMMERCE_PROJECT_BANNER_HEADING } from "@/constants/industry";
 
 const EcommerceProjectBanner = () => {
   return (
@@ -8,10 +8,11 @@ const EcommerceProjectBanner = () => {
       <div className="wrapper_container py-8 mb-12 md:mb-24">
         <div className="flex justify-around items-center">
           <h1 className="text-xl md:text-2xl font-[500] text-white">
-            Have an eCommerce project in mind? We’d love to make your ideas into
-            reality.
+            {ECOMMERCE_PROJECT_BANNER_HEADING}
           </h1>
-          <GetInTouchContactBtn>LET&apos;S TALK </GetInTouchContactBtn>
+          <GetInTouchContactBtn btnStyles="hover:text-black transition duration-200">
+            LET&apos;S TALK
+          </GetInTouchContactBtn>
         </div>
       </div>
     </section>
