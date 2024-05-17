799c14b647c17ffe1add5f171073c6fcb6101c16
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-17 20:34:34 +1000

    [WEB-75] downloaded hero img

--- src/components/B2CEcommerce/B2CHero.tsx
+++ src/components/B2CEcommerce/B2CHero.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const B2CHero = () => {
-  return <div>B2CHero</div>;
+  return (
+    <section id="b2c_hero">
+      <div className="wrapper_container mb-24"></div>
+    </section>
+  );
 };
 
 export default B2CHero;
