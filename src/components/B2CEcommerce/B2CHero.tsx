bd804694ca05e7ac0a8ab345107a84c6bf7409c7
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-17 20:40:21 +1000

    [WEB-75] rendered the image

--- src/components/B2CEcommerce/B2CHero.tsx
+++ src/components/B2CEcommerce/B2CHero.tsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
 import React from "react";
 
 const B2CHero = () => {
   return (
-    <section id="b2c_hero">
-      <div className="wrapper_container mb-24"></div>
+    <section id="b2c_hero" className="bg-white-100">
+      <div className="wrapper_container mb-24 mt-[48px] lg:mt-[88px]">
+        <Image src="/b2c/hero.png" alt="hero" width={496} height={502} />
+      </div>
     </section>
   );
 };
