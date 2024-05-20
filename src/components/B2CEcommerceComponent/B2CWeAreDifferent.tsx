8915a4ba90a7fa3f29712b81423ec4ca909ae917
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-20 20:36:09 +1000

    [WEB-76] added b2c we are different componet

---
+++ src/components/B2CEcommerceComponent/B2CWeAreDifferent.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const B2CWeAreDifferent = () => {
+  return <div>B2CWeAreDifferent</div>;
+};
+
+export default B2CWeAreDifferent;
---
+++ src/components/B2CEcommerceComponent/index.ts
@@ -0,0 +1,4 @@
+import B2CHero from "./B2CHero";
+import B2CWeAreDifferent from "./B2CWeAreDifferent";
+
+export { B2CHero, B2CWeAreDifferent };
