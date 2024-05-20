2b40a93b7f2451dfde1cbadefb78ab326cd9ad9b
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-20 20:42:38 +1000

    [WEB-76] added the rough template for rendering

--- src/components/B2CEcommerceComponent/B2CWeAreDifferent.tsx
+++ src/components/B2CEcommerceComponent/B2CWeAreDifferent.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const B2CWeAreDifferent = () => {
-  return <div>B2CWeAreDifferent</div>;
+  return (
+    <section id="we_are_different">
+      <div className="wrapper_container mb-24">
+        <div>Paragraph</div>
+        <div>
+          <div>Image</div>
+          <div>Key</div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default B2CWeAreDifferent;
