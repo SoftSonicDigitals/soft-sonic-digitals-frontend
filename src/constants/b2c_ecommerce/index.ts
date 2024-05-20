f9e955165c560fab7b5b58ef40eb1d1acaeff42f
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-20 19:31:25 +1000

    [WEB-75] added b2c ecommerce constants

---
+++ src/constants/b2c_ecommerce/index.ts
@@ -0,0 +1,8 @@
+export const B2C_ECOMMERCE_WORD = "B2C e-commerce";
+export const B2C_HERO_HEADING =
+  "Online retailers don't just love us, they trust us to solve their B2C e-commerce challenges";
+
+export const B2C_HERO_DESC =
+  "Because we believe in, It's not about trying different things,";
+export const B2C_HERO_QUOTE =
+  " it's about doing things differently in retail e-commerce";
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
       md: "768px",
       // => @media (min-width: 768px) { ... }
 
+      "min-[955]": "955px",
       lg: "1024px",
       // => @media (min-width: 1024px) { ... }
 
