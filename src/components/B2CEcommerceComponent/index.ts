86b9c52584d5ce9254dbfd09d69dd4b0a4d4b5db
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-21 13:59:12 +1000

    [WEB-77] refactored the retailer concerns to its component

--- src/components/B2CEcommerceComponent/index.ts
+++ src/components/B2CEcommerceComponent/index.ts
@@ -1,5 +1,6 @@
 import B2CHero from "./B2CHero";
 import B2CWeAreDifferent from "./B2CWeAreDifferent";
 import B2ConcernsExpertise from "./B2ConcernsExpertise";
+import RetailerListLayout from "./RetailerListLayout";
 
-export { B2CHero, B2CWeAreDifferent, B2ConcernsExpertise };
+export { B2CHero, B2CWeAreDifferent, B2ConcernsExpertise, RetailerListLayout };
