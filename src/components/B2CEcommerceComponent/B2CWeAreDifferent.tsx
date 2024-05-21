7046afb06d5414922736f935d779ea36c4e17a6d
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-21 12:56:27 +1000

    [WEB-76] mapped over the reasons array

--- src/components/B2CEcommerceComponent/B2CWeAreDifferent.tsx
+++ src/components/B2CEcommerceComponent/B2CWeAreDifferent.tsx
@@ -2,6 +2,7 @@ import {
   WE_ARE_DIFF_HEADING,
   WE_ARE_DIFF_HEADING_TIMEFRAME,
 } from "@/constants/b2c_ecommerce";
+import { WE_ARE_DIFF_REASONS } from "@/prototypes/b2c";
 import Image from "next/image";
 import React from "react";
 
@@ -18,6 +19,7 @@ const B2CWeAreDifferent = () => {
             {WE_ARE_DIFF_HEADING}
           </h1>
         </div>
+
         <div className="flex-center flex-col lg:flex-row gap-10  md:mx-6 lg:mx-10 xl:mx-32 mb-12 ">
           <div className="hidden sm:block basis-full">
             <Image
@@ -31,32 +33,16 @@ const B2CWeAreDifferent = () => {
 
           <div className=" basis-full pl-2 lg:pl-12 ">
             <h3 className="text-2xl font-[700] font-gray-800 text-left mb-10 ">
-              Here's how we're different
+              {WE_ARE_DIFF_REASONS.title}
             </h3>
-            <div className="mb-8">
-              <p className="text-xl font-[700] mb-2">We listen</p>
-              <p>
-                We've interviewed over 100 retail store owners and experts to
-                understand their unique challenges
-              </p>
-            </div>
-            <div className="mb-8">
-              <p className="text-xl font-[700] mb-2">
-                We focus on your needs, not ours
-              </p>
-              <p>
-                We've interviewed over 100 retail store owners and experts to
-                understand their unique challenges
-              </p>
-            </div>
-            <div className="mb-8">
-              <p className="text-xl font-[700] mb-2">
-                We don't follow trends,we set them
-              </p>
-              <p>
-                Our proven methods and creative approach can help you stand out
-              </p>
-            </div>
+            {WE_ARE_DIFF_REASONS.reasons.map(
+              ({ reason, description }, index) => (
+                <div key={index} className="mb-8">
+                  <p className="text-xl font-[700] mb-2">{reason}</p>
+                  <p>{description}</p>
+                </div>
+              )
+            )}
           </div>
         </div>
       </div>
