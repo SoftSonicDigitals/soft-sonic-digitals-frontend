38629bae0c3a88292bbcb2483e3eb30d4b44b3b3
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-21 11:46:01 +1000

    [WEB-76] added the headings for the section

--- src/app/(layout_pages)/services/b2c-retail-ecommerce/page.tsx
+++ src/app/(layout_pages)/services/b2c-retail-ecommerce/page.tsx
@@ -1,10 +1,11 @@
-import { B2CHero } from "@/components/B2CEcommerceComponent";
+import { B2CHero, B2CWeAreDifferent } from "@/components/B2CEcommerceComponent";
 import React from "react";
 
 const page = () => {
   return (
     <main className="overflow-hidden">
       <B2CHero />;
+      <B2CWeAreDifferent />
     </main>
   );
 };
