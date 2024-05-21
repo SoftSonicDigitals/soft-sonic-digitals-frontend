d1753756fce9ac279ea5c9ab621a1caa2bd28edf
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-21 15:14:57 +1000

    [WEB-77] added the drop down feature

--- src/components/B2CEcommerceComponent/ConcernExpertiseContainer.tsx
+++ src/components/B2CEcommerceComponent/ConcernExpertiseContainer.tsx
@@ -6,13 +6,18 @@ import useDropDown from "@/hooks/useDropDown";
 
 const ConcernExpertiseContainer = () => {
   const { selected, toggle, isOpen } = useDropDown();
+
   return (
     <>
       {CONCERNS_AND_GOALS.map((listItem, index) => (
         <RetailerListLayout
+          toogle={toggle}
+          selected={selected}
           key={index}
           title={listItem.title}
           list={listItem.list}
+          isOpen={isOpen}
+          index={index}
         />
       ))}
     </>
--- src/components/B2CEcommerceComponent/RetailerListLayout.tsx
+++ src/components/B2CEcommerceComponent/RetailerListLayout.tsx
@@ -2,19 +2,41 @@ import React from "react";
 type RetailerListLayoutProps = {
   title: string;
   list: { title: string; description: string }[];
+  selected: number | null;
+  toogle: (index: number) => void;
+  isOpen: (selected: number | null, index: number) => boolean;
+  index: number;
 };
-const RetailerListLayout = ({ title, list }: RetailerListLayoutProps) => {
+const RetailerListLayout = ({
+  title,
+  list,
+  toogle,
+  selected,
+  isOpen,
+  index,
+}: RetailerListLayoutProps) => {
   return (
-    <div className="border-l-2 border-gray-700 pl-8  py-3 tracking-wide mb-10">
-      <h3 className="text-4xl font-[700] text-gray-800">{title}</h3>
-      <div className="mt-8">
-        {list.map(({ title, description }, index) => (
-          <div key={index} className="mb-3">
-            <p className="text-2xl font-[700] text-gray-800 mb-1">{title}</p>
-            <p>{description}</p>
-          </div>
-        ))}
-      </div>
+    <div
+      className="border-l-2 border-gray-700 pl-8  py-3 tracking-wide mb-10"
+      onClick={() => toogle(index)}
+    >
+      <h3
+        className={`text-4xl font-[700] ${
+          isOpen(selected, index) ? "text-gray-800" : "text-gray-700"
+        }`}
+      >
+        {title}
+      </h3>
+      {isOpen(selected, index) && (
+        <div className="mt-8">
+          {list.map(({ title, description }, index) => (
+            <div key={index} className="mb-3">
+              <p className="text-2xl font-[700] text-gray-800 mb-1">{title}</p>
+              <p>{description}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
