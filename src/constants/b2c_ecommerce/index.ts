77b992142a2efced5e2d2477e68bd523e53858aa
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-21 12:52:59 +1000

    [WEB-76] added prototypes array for the reasons

--- src/constants/b2c_ecommerce/index.ts
+++ src/constants/b2c_ecommerce/index.ts
@@ -8,6 +8,6 @@ export const B2C_HERO_QUOTE =
   " it's about doing things differently in retail e-commerce";
 
 // WE are different constants
-export const WE_ARE_DIFF_HEADING_TIMEFRAME = "For over a decade";
+export const WE_ARE_DIFF_HEADING_TIMEFRAME = "For over a decade,";
 export const WE_ARE_DIFF_HEADING =
   "we've been working with online retailers, learning their business and challenges. We don't believe in cookie-cutter approaches or empty promises";
---
+++ src/prototypes/b2c/index.ts
@@ -0,0 +1,20 @@
+export const WE_ARE_DIFF_REASONS = {
+  title: "Here's how we're different",
+  reasons: [
+    {
+      reason: "We listen",
+      description:
+        "We've interviewed over 100 retail store owners and experts to understand their unique challenges",
+    },
+    {
+      reason: "We focus on your needs, not ours",
+      description:
+        "We play actively with your existing systems and find a way to integrate with new ones",
+    },
+    {
+      reason: "We don't follow trends,we set them",
+      description:
+        "Our proven methods and creative approach can help you stand out",
+    },
+  ],
+};
