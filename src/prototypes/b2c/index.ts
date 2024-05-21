76fa583385eec236ac2b462b9fd7642cc1987d76
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-21 13:48:21 +1000

    [WEB-77] added prototypes and type for the concerns and expertise

--- src/prototypes/b2c/index.ts
+++ src/prototypes/b2c/index.ts
@@ -1,4 +1,4 @@
-import { WeAreDifferentReasons } from "@/models/b2c";
+import { ConcernsAndExpertise, WeAreDifferentReasons } from "@/models/b2c";
 
 export const WE_ARE_DIFF_REASONS: WeAreDifferentReasons = {
   title: "Here's how we're different",
@@ -17,6 +17,45 @@ export const WE_ARE_DIFF_REASONS: WeAreDifferentReasons = {
       reason: "We don't follow trends,we set them",
       description:
         "Our proven methods and creative approach can help you stand out",
+    },
+  ],
+};
+
+export const RETAILERS_CONCERNS: ConcernsAndExpertise = {
+  title: "Retailer’s Immediate Concerns while going digital",
+  list: [
+    {
+      title: "Quick time-to-market",
+      description: "We've got your back.",
+    },
+    {
+      title: "No more complex backend systems",
+      description: "We simplify them for you.",
+    },
+    {
+      title: "Easily Modify without developers' availability",
+      description: "We designate you to take control.",
+    },
+    {
+      title: "Easily integrated with existing ERP, POS, and loyalty systems",
+      description: "We know your challenges.",
+    },
+    {
+      title: "Add new features and functionalities rapidly",
+      description: "Buckle up. We're full of innovative & futuristic ideas.",
+    },
+    {
+      title: "SEO is in safe hands",
+      description:
+        "We know how to rank on Google so your store can be easily seen.",
+    },
+    {
+      title: "Security is a top priority",
+      description: "We guarantee Fort Knox-level protection for your store.",
+    },
+    {
+      title: "Efficient and streamlined operations",
+      description: "We're the efficiency whisperers.",
     },
   ],
 };
