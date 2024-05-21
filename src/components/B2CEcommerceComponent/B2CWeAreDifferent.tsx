8352e9d3360cbdfd91c41f42a68832e1f4d0f02f
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-21 15:28:53 +1000

    [WEB-77] adjusted the stylings

--- src/components/B2CEcommerceComponent/B2CWeAreDifferent.tsx
+++ src/components/B2CEcommerceComponent/B2CWeAreDifferent.tsx
@@ -9,7 +9,7 @@ import React from "react";
 const B2CWeAreDifferent = () => {
   return (
     <section id="we_are_different">
-      <div className="wrapper_container mb-24">
+      <div className="wrapper_container mb-12 md:mb-24">
         <div>
           <h1 className="text-2xl md:text-3xl lg:text-4xl font-[700] text-gray-800 !leading-[1.25]  text-center md:mx-6 lg:mx-10 xl:mx-32 mb-12">
             <span className="border-b-[1px] border-gray-800">
--- src/components/B2CEcommerceComponent/RetailerListLayout.tsx
+++ src/components/B2CEcommerceComponent/RetailerListLayout.tsx
@@ -18,11 +18,11 @@ const RetailerListLayout = ({
 }: RetailerListLayoutProps) => {
   return (
     <div
-      className="border-l-2 border-gray-700 pl-8  py-3 tracking-wide mb-10"
+      className="border-l-2 border-gray-700 pl-8  py-1  md:py-3 tracking-wide mb-6 md:mb-10 ml-1"
       onClick={() => toogleMenu(index)}
     >
       <h3
-        className={`text-4xl font-[700] ${
+        className={`text-xl md:text-2xl lg:text-3xl 2xl:text-4xl font-[700] ${
           isMenuOpen(selectedMenu, index) ? "text-gray-800" : "text-gray-700"
         }`}
       >
@@ -32,8 +32,10 @@ const RetailerListLayout = ({
         <div className="mt-8">
           {list.map(({ title, description }, index) => (
             <div key={index} className="mb-3">
-              <p className="text-2xl font-[700] text-gray-800 mb-1">{title}</p>
-              <p>{description}</p>
+              <p className="text-base md:text-xl lg:text-2xl font-[700] text-gray-800 mb-1">
+                {title}
+              </p>
+              <p className="text-sm md:text-base">{description}</p>
             </div>
           ))}
         </div>
