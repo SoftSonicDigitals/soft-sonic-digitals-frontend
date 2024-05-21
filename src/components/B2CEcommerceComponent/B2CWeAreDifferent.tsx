686375f5ed4f827d31c7edaa3d5ff8a6228e2c35
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-21 11:46:28 +1000

    [WEB-76] made the headings responsive

--- src/components/B2CEcommerceComponent/B2CWeAreDifferent.tsx
+++ src/components/B2CEcommerceComponent/B2CWeAreDifferent.tsx
@@ -5,7 +5,7 @@ const B2CWeAreDifferent = () => {
     <section id="we_are_different">
       <div className="wrapper_container mb-24">
         <div>
-          <h1 className="text-4xl font-[700] text-gray-800 leading-tight text-center mx-32">
+          <h1 className="text-2xl md:text-3xl lg:text-4xl font-[700] text-gray-800 !leading-[1.25]  text-center md:mx-6 lg:mx-10 xl:mx-32">
             <span className="border-b-[1px] border-gray-800">
               {" "}
               For over a decade,
