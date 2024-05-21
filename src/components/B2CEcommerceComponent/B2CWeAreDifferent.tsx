8db4432e92dfc435e50cf4f50d24a042c52db344
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-21 11:46:01 +1000

    [WEB-76] added the headings for the section

--- src/components/B2CEcommerceComponent/B2CWeAreDifferent.tsx
+++ src/components/B2CEcommerceComponent/B2CWeAreDifferent.tsx
@@ -4,7 +4,17 @@ const B2CWeAreDifferent = () => {
   return (
     <section id="we_are_different">
       <div className="wrapper_container mb-24">
-        <div>Paragraph</div>
+        <div>
+          <h1 className="text-4xl font-[700] text-gray-800 leading-tight text-center mx-32">
+            <span className="border-b-[1px] border-gray-800">
+              {" "}
+              For over a decade,
+            </span>{" "}
+            we've been working with online retailers, learning their business
+            and challenges. We don't believe in cookie-cutter approaches or
+            empty promises
+          </h1>
+        </div>
         <div>
           <div>Image</div>
           <div>Key</div>
