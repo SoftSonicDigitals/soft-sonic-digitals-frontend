359635f495d1ab9eda8c48dfd5ed555fd41ea00d
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-21 13:39:00 +1000

    [WEB-77] added the stylings for list of concern

--- src/components/B2CEcommerceComponent/B2ConcernsExpertise.tsx
+++ src/components/B2CEcommerceComponent/B2ConcernsExpertise.tsx
@@ -6,11 +6,34 @@ const B2ConcernsExpertise = () => {
     <section id="concerns_and_expertise">
       <div className="wrapper_container mb-24">
         <div className="md:mx-6 lg:mx-10 xl:mx-32 mb-12 flex">
-          <div>
-            <div></div>
-            <div></div>
+          <div className="max-w-[650px]">
+            <div className="border-l-2 border-gray-700 pl-8  py-3 tracking-wide mb-10">
+              <h3 className="text-4xl font-[700] text-gray-900">
+                Retailer’s Immediate Concerns while going digital
+              </h3>
+              <div className="mt-8">
+                <div className="mb-3">
+                  <p className="text-2xl font-[700] text-gray-900 mb-1">
+                    Quick time-to-market
+                  </p>
+                  <p>We've got your back.</p>
+                </div>
+                <div className="mb-4">
+                  <p className="text-2xl font-[700] text-gray-900 mb-1">
+                    No more complex backend systems
+                  </p>
+                  <p>We simplify them for you.</p>
+                </div>
+              </div>
+            </div>
+            <div className="border-l-2 border-gray-700 pl-8  py-3 tracking-wide ">
+              <h3 className="text-4xl font-[700] text-gray-800">
+                Retailer’s Long-Term Goals, Our Expertise
+              </h3>
+              <div></div>
+            </div>
           </div>
-          <div>
+          <div className="">
             <Image
               src="/b2c/shape.png"
               alt="shape"
