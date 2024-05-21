f8369a47f0183e4081efd64123cce62975144bad
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-21 12:44:42 +1000

    [WEB-76] added constants for this section

--- src/components/B2CEcommerceComponent/B2CWeAreDifferent.tsx
+++ src/components/B2CEcommerceComponent/B2CWeAreDifferent.tsx
@@ -1,3 +1,7 @@
+import {
+  WE_ARE_DIFF_HEADING,
+  WE_ARE_DIFF_HEADING_TIMEFRAME,
+} from "@/constants/b2c_ecommerce";
 import Image from "next/image";
 import React from "react";
 
@@ -9,11 +13,9 @@ const B2CWeAreDifferent = () => {
           <h1 className="text-2xl md:text-3xl lg:text-4xl font-[700] text-gray-800 !leading-[1.25]  text-center md:mx-6 lg:mx-10 xl:mx-32 mb-12">
             <span className="border-b-[1px] border-gray-800">
               {" "}
-              For over a decade,
+              {WE_ARE_DIFF_HEADING_TIMEFRAME}
             </span>{" "}
-            we've been working with online retailers, learning their business
-            and challenges. We don't believe in cookie-cutter approaches or em
-            pty promises
+            {WE_ARE_DIFF_HEADING}
           </h1>
         </div>
         <div className="flex-center flex-col lg:flex-row gap-10  md:mx-6 lg:mx-10 xl:mx-32 mb-12 ">
