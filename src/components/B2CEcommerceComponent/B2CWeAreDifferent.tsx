bba2f5524cc11dfc9127c78fb87b00976949ee8c
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-21 12:03:36 +1000

    [WEB-76] added a responsive image in  the section

--- src/components/B2CEcommerceComponent/B2CWeAreDifferent.tsx
+++ src/components/B2CEcommerceComponent/B2CWeAreDifferent.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const B2CWeAreDifferent = () => {
@@ -5,7 +6,7 @@ const B2CWeAreDifferent = () => {
     <section id="we_are_different">
       <div className="wrapper_container mb-24">
         <div>
-          <h1 className="text-2xl md:text-3xl lg:text-4xl font-[700] text-gray-800 !leading-[1.25]  text-center md:mx-6 lg:mx-10 xl:mx-32">
+          <h1 className="text-2xl md:text-3xl lg:text-4xl font-[700] text-gray-800 !leading-[1.25]  text-center md:mx-6 lg:mx-10 xl:mx-32 mb-12">
             <span className="border-b-[1px] border-gray-800">
               {" "}
               For over a decade,
@@ -15,8 +16,17 @@ const B2CWeAreDifferent = () => {
             empty promises
           </h1>
         </div>
-        <div>
-          <div>Image</div>
+        <div className="flex-center flex-col">
+          <div className="hidden sm:block sm:mx-16 md:mx-8 lg:mx-10 xl:mx-32 ">
+            <Image
+              src="/b2c/we_are_different.png"
+              alt="we are different"
+              width={700}
+              height={707}
+              style={{ objectFit: "contain" }}
+            />
+          </div>
+
           <div>Key</div>
         </div>
       </div>
