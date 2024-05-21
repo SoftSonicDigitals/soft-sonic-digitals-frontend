3017bd54b5a712e0186b68a0d0983575529ae580
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-21 13:19:58 +1000

    [WEb-77] added sections

--- src/components/B2CEcommerceComponent/B2ConcernsExpertise.tsx
+++ src/components/B2CEcommerceComponent/B2ConcernsExpertise.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const B2ConcernsExpertise = () => {
-  return <div>B2ConcernsExpertise</div>;
+  return (
+    <section id="concerns_and_expertise">
+      <div className="wrapper_container mb-24">
+        <div className="md:mx-6 lg:mx-10 xl:mx-32 mb-12 flex">
+          <div>
+            <div></div>
+            <div></div>
+          </div>
+          <div>Image</div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default B2ConcernsExpertise;
