656ecfe0108d41bc02f1f8d01709cd5f43e97372
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-22 18:13:09 +1000

    [WEB-78] added styed heading

--- src/components/B2CEcommerceComponent/B2CHelp.tsx
+++ src/components/B2CEcommerceComponent/B2CHelp.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const B2CHelp = () => {
-  return <div>B2CHelp</div>;
+  return (
+    <section id="help_every_step">
+      <div className="wrapper_container mb-24">
+        <div>
+          <h1 className="text-2xl md:text-3xl lg:text-4xl font-[700] text-gray-800 !leading-[1.25]   md:mx-6 lg:mx-10 xl:mx-32 mb-12">
+            We help you on every step of your B2C eCommerce journey, so you can
+            always focus on what truly matters.
+          </h1>
+        </div>
+        <div></div>
+      </div>
+    </section>
+  );
 };
 
 export default B2CHelp;
