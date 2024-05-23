780c3fe15277cc3d4d1e6ecdaa9c5cdf3c64ad5c
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-23 20:22:22 +1000

    [WEB-79] added the b2c stats component

--- src/components/B2CEcommerceComponent/index.ts
+++ src/components/B2CEcommerceComponent/index.ts
@@ -2,6 +2,7 @@ import B2CHelp from "./B2CHelp";
 import B2CHelpTab from "./B2CHelpTab";
 import B2CHelpTabs from "./B2CHelpTabs";
 import B2CHero from "./B2CHero";
+import B2CStats from "./B2CStats";
 import B2CWeAreDifferent from "./B2CWeAreDifferent";
 import B2ConcernsExpertise from "./B2ConcernsExpertise";
 import ConcernExpertiseContainer from "./ConcernExpertiseContainer";
@@ -15,4 +16,5 @@ export {
   B2CHelp,
   B2CHelpTabs,
   B2CHelpTab,
+  B2CStats,
 };
