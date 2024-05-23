2bfae2f75bdab67c0b61130ab02f75f10c7d2e74
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-23 20:51:50 +1000

    [WEB-79] added the stats for the mobile view

--- src/components/B2CEcommerceComponent/B2CStats.tsx
+++ src/components/B2CEcommerceComponent/B2CStats.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import B2CStatsContainer from "./B2CStatsContainer";
 
 const B2CStats = () => {
   //   return <section id="ecommerce_stats" className="bg-white-100"></section>;
@@ -6,10 +7,12 @@ const B2CStats = () => {
   return (
     <section id="results">
       <div className="wrapper_container my-24 ">
-        <h1 className=" text-2xl sm:text-2xl  xl:text-4xl font-[700] text-gray-800 !leading-[1.32]   text-center mb-10">
+        <h1 className="md:mx-6 lg:mx-10 xl:mx-16 text-2xl sm:text-2xl  xl:text-4xl font-[700] text-gray-800 !leading-[1.32]   text-center mb-10">
           Uplift eCommerce experience for globally renowned brands through
           expertise
         </h1>
+
+        <B2CStatsContainer />
       </div>
     </section>
   );
