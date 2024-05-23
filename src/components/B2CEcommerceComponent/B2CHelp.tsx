af1a82ad1b2da6dc2cb7f7b40efee989325fc521
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-23 20:40:18 +1000

    [WEB-79] added the seciton heading

--- src/components/B2CEcommerceComponent/B2CHelp.tsx
+++ src/components/B2CEcommerceComponent/B2CHelp.tsx
@@ -8,7 +8,7 @@ const B2CHelp = () => {
       <div className="wrapper_container mb-12 mb:mb-24 ">
         <div className=" md:mx-6 lg:mx-10 xl:mx-16 flex flex-col lg:flex-row gap-8 lg:gap-16 xl:gap-16  2xl:gap-28 max-w-[725px] lg:max-w-fit mx-auto">
           <div className="lg:basis-[70%] xl:basis-[60%]">
-            <h1 className=" text-2xl sm:text-2xl  xl:text-4xl font-[700] text-gray-800 !leading-[1.25]   ">
+            <h1 className=" text-2xl sm:text-2xl  xl:text-4xl font-[700] text-gray-800 !leading-[1.32]   ">
               {B2C_HELP_HEADING}
             </h1>
           </div>
--- src/components/B2CEcommerceComponent/B2CStats.tsx
+++ src/components/B2CEcommerceComponent/B2CStats.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const B2CStats = () => {
-  return <section id="ecommerce_stats" className="bg-white-100"></section>;
+  //   return <section id="ecommerce_stats" className="bg-white-100"></section>;
+
+  return (
+    <section id="results">
+      <div className="wrapper_container my-24 ">
+        <h1 className=" text-2xl sm:text-2xl  xl:text-4xl font-[700] text-gray-800 !leading-[1.32]   text-center mb-10">
+          Uplift eCommerce experience for globally renowned brands through
+          expertise
+        </h1>
+      </div>
+    </section>
+  );
 };
 
 export default B2CStats;
