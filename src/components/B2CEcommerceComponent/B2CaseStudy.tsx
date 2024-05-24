83a32a28b96b2c4792f8c587cdc75c1e7f6e9303
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-24 20:52:14 +1000

    [WEB-81] made the section responsive

--- src/components/B2CEcommerceComponent/B2CaseStudy.tsx
+++ src/components/B2CEcommerceComponent/B2CaseStudy.tsx
@@ -3,9 +3,9 @@ import B2CaseStudies from "./B2CaseStudies";
 
 const B2CaseStudy = () => {
   return (
-    <section id="case_study">
+    <section id="case_study" className="bg-white-100 sm:bg-white">
       <div className="wrapper_container mb-24  ">
-        <div className="md:mx-6 lg:mx-10 xl:mx-16 py-12 bg-orange-100">
+        <div className="md:mx-6 lg:mx-10 xl:mx-16 py-12 sm:bg-orange-100">
           <h1 className="text-2xl md:text-3xl lg:text-4xl font-[700] text-gray-800 !leading-[1.25]  text-center md:mx-6 lg:mx-10 xl:mx-32 mb-4">
             Explore the brands who choose us as their partner
           </h1>
