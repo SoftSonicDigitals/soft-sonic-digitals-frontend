3f61e7cac7c48f94081f08727eb912fd1e24c1f2
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-24 12:41:20 +1000

    [WEB-80] added the clients component to the b2c page

--- src/app/(layout_pages)/services/b2c-retail-ecommerce/page.tsx
+++ src/app/(layout_pages)/services/b2c-retail-ecommerce/page.tsx
@@ -5,6 +5,7 @@ import {
   B2CHelp,
   B2CStats,
 } from "@/components/B2CEcommerceComponent";
+import { Clients } from "@/components/Clients";
 import React from "react";
 
 const page = () => {
@@ -15,6 +16,7 @@ const page = () => {
       <B2ConcernsExpertise />
       <B2CHelp />
       <B2CStats />
+      <Clients />
     </main>
   );
 };
