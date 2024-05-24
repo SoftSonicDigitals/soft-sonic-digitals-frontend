32382423d3d15c6f5537a4ef4b85b3263ddbc8e0
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-24 11:24:35 +1000

    [WEB-79] added the constants and adjusted the stylings

--- src/components/B2CEcommerceComponent/B2CStats.tsx
+++ src/components/B2CEcommerceComponent/B2CStats.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import B2CStatsContainer from "./B2CStatsContainer";
+import { B2C_STATS_HEADING } from "@/constants/b2c_ecommerce";
 
 const B2CStats = () => {
-  //   return <section id="ecommerce_stats" className="bg-white-100"></section>;
-
   return (
-    <section id="results" className="bg-white-100 py-10 mb-24">
+    <section id="results" className="bg-white-100 py-20 mb-24">
       <div className="wrapper_container  ">
         <h1 className="md:mx-6 lg:mx-10 xl:mx-16 text-2xl sm:text-2xl  xl:text-4xl font-[700] text-gray-800 !leading-[1.32]   text-center mb-10">
-          Uplift eCommerce experience for globally renowned brands through
-          expertise
+          {B2C_STATS_HEADING}
         </h1>
 
         <B2CStatsContainer />
