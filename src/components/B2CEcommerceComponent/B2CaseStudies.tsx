87763c81554a8e39cde8477b46e10c3df430d82a
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-24 20:52:14 +1000

    [WEB-81] made the section responsive

--- src/components/B2CEcommerceComponent/B2CaseStudies.tsx
+++ src/components/B2CEcommerceComponent/B2CaseStudies.tsx
@@ -6,13 +6,14 @@ import { PrimaryButton } from "../Reusable";
 
 const B2CaseStudies = () => {
   return (
-    <div className="flex-center mb-16 ">
-      <div className="grid grid-cols-1  gap-8 md:gap-4 items-center justify-center max-w-[1300px] md:px-[2%] lg:px-10 xl:px-6 2xl:p-0  2xl:gap-y-8">
+    <div className="flex-center mb-16  ">
+      <div className="grid grid-cols-1  gap-10 items-center justify-center max-w-[1300px] md:px-[2%] lg:mx-4  2xl:gap-y-12">
         {CASE_STUDIES.map(
           ({ title, link, imageName, caseStudyPhrase, description }, index) => (
             <div
               key={index}
-              className={`flex-center flex-col   px-4 2xl:px-0 gap-10 ${
+              className={`flex-center flex-col   px-0 sm:px-8 
+              lg:px-0 gap-6 md:gap-10 ${
                 index % 2 == 0 ? "lg:flex-row" : "lg:flex-row-reverse"
               }`}
             >
@@ -26,15 +27,15 @@ const B2CaseStudies = () => {
                   style={{ objectFit: "contain" }}
                 />
               </Link>
-              <div className="basis-full">
-                <h4 className="text-2xl font-[600] text-gray-900 mb-4">
+              <div className="basis-full px-2">
+                <h4 className="text-base sm:text-2xl font-[700] text-gray-800 mb-4 tracking-normal">
                   {caseStudyPhrase}
                 </h4>
                 <p className="mb-6">{description}</p>
                 <Link href={link}>
                   <PrimaryButton
                     title="VIEW CASE STUDY"
-                    className="px-6 py-3 "
+                    className="px-3 py-2 sm:px-6 sm:py-3 text-sm sm:text-base "
                   />
                 </Link>
               </div>
