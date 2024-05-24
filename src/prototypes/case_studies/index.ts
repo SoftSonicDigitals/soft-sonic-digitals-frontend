6f9c5ceffe305db9b1a0d7a9d2d3611dfce5b7f3
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-24 18:45:12 +1000

    [WEB-81] added the phrase and description in the array

--- src/prototypes/case_studies/index.ts
+++ src/prototypes/case_studies/index.ts
@@ -6,23 +6,39 @@ export const CASE_STUDIES: CaseStudiesType = [
     title: "Feather Down",
     imageName: "FD.webp",
     link: "/case-study/feather_down",
+    caseStudyPhrase:
+      "Feather Down revamped its business processes and optimized its online travel site using Magento",
+    description:
+      "Feather Down selected us to revamp their travel booking website, aiming to boost aesthetic allure, elevate booking rates, enhance user satisfaction, and attract more visitors.",
   },
   {
     id: "2",
     title: "Living It Up",
     imageName: "Living-it-up-1.webp",
     link: "/case-study/living_it_up",
+    caseStudyPhrase:
+      "Living it Up has recently transformed its online retail store with the help of us",
+    description:
+      "Living It Up helped build a user-friendly online retail store using Magento. This drove significant business growth and improved customer experience.",
   },
   {
     id: "3",
     title: "Power Look",
     imageName: "Powerlook.webp",
     link: "/case-study/powerlook",
+    caseStudyPhrase:
+      "Power Look simplifies its food Business with a Magento Website & Mobile App",
+    description:
+      "We helped Power Look streamline business operations by creating a online store. With our technical expertise, we facilitated a smooth transition from outdated technology to latest one and making it easier and more convenient for customers to shop online.",
   },
   {
     id: "4",
     title: "Petzone",
     imageName: "Petzone.webp",
     link: "/case-study/petzone",
+    caseStudyPhrase:
+      "Petzone revamped their website with us for better UI/UX design",
+    description:
+      "We improved Petzone's online presence by revamping its UI/UX theme, resulting in better customer relations.",
   },
 ];
