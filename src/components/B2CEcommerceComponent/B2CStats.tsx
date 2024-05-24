5854d61334371445ccf5db72532dc1568dca6a62
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-24 12:45:22 +1000

    [WEB-80] adjusted the margins

--- src/components/B2CEcommerceComponent/B2CStats.tsx
+++ src/components/B2CEcommerceComponent/B2CStats.tsx
@@ -4,7 +4,7 @@ import { B2C_STATS_HEADING } from "@/constants/b2c_ecommerce";
 
 const B2CStats = () => {
   return (
-    <section id="results" className="bg-white-100 py-20 mb-24">
+    <section id="results" className="bg-white-100 py-20 mb-12">
       <div className="wrapper_container  ">
         <h1 className="md:mx-6 lg:mx-10 xl:mx-16 text-2xl sm:text-2xl  xl:text-4xl font-[700] text-gray-800 !leading-[1.32]   text-center mb-10">
           {B2C_STATS_HEADING}
