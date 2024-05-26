d1934717b867b0b3ed9d14405c55b0b25100f73f
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-26 18:48:03 +1000

    [WEB-81] added the project banner component

--- src/app/(layout_pages)/services/b2c-retail-ecommerce/page.tsx
+++ src/app/(layout_pages)/services/b2c-retail-ecommerce/page.tsx
@@ -7,6 +7,7 @@ import {
 } from "@/components/B2CEcommerceComponent";
 import B2CaseStudy from "@/components/B2CEcommerceComponent/B2CaseStudy";
 import { Clients } from "@/components/Clients";
+import { EcommerceProjectBanner } from "@/components/Reusable";
 import React from "react";
 
 const page = () => {
@@ -19,6 +20,7 @@ const page = () => {
       <B2CStats />
       <Clients />
       <B2CaseStudy />
+      <EcommerceProjectBanner />
     </main>
   );
 };
