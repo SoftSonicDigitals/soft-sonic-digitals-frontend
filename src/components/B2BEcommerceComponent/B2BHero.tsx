e44d8c25f68914123ed92541a5375485934200d7
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-26 20:59:13 +1000

    [WEB-83] added the background image

--- src/components/B2BEcommerceComponent/B2BHero.tsx
+++ src/components/B2BEcommerceComponent/B2BHero.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
 import React from "react";
 
 const B2BHero = () => {
-  return <div>B2BHero</div>;
+  return (
+    <section>
+      <div className="pt-[48px] lg:pt-[88px] ">
+        <div className="relative w-[100vw] h-[50vh] xl:h-[100vh] 3xl:h-[50vh]">
+          <Image
+            src={`/b2b/hero.png`}
+            alt="hero"
+            fill
+            className="object-cover object-[85%] md:object-top"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default B2BHero;
