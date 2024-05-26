4ae1e1f5db51cf31ae7cc7b098be70d3c8c249f5
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-26 18:39:33 +1000

    [WEB-81] added the constants for the section

--- src/components/B2CEcommerceComponent/B2CaseStudy.tsx
+++ src/components/B2CEcommerceComponent/B2CaseStudy.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import B2CaseStudies from "./B2CaseStudies";
+import {
+  B2C_CASE_STUDIES_DESCRIPTION,
+  B2C_CASE_STUDIES_HEADING,
+} from "@/constants/b2c_ecommerce";
 
 const B2CaseStudy = () => {
   return (
@@ -7,11 +11,11 @@ const B2CaseStudy = () => {
       <div className="wrapper_container mb-24  ">
         <div className="md:mx-6 lg:mx-10 xl:mx-16 py-12 sm:bg-orange-100">
           <h1 className="text-2xl md:text-3xl lg:text-4xl font-[700] text-gray-800 !leading-[1.25]  text-center md:mx-6 lg:mx-10 xl:mx-32 mb-4">
-            Explore the brands who choose us as their partner
+            {B2C_CASE_STUDIES_HEADING}
           </h1>
 
           <p className="text-center md:text-lg  mb-10">
-            Improve your business outcomes with our expertise
+            {B2C_CASE_STUDIES_DESCRIPTION}
           </p>
 
           <B2CaseStudies />
