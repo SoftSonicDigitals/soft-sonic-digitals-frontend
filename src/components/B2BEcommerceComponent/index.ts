a715024f2422606160a724006123d0ee71ea6c0a
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-29 16:18:43 +1000

    [WEB-84] added the b2b digital ecoomerce details component

--- src/components/B2BEcommerceComponent/index.ts
+++ src/components/B2BEcommerceComponent/index.ts
@@ -1,5 +1,11 @@
 import B2BDigitalEcommerce from "./B2BDigitalEcommerce";
+import B2BDigitalEcommerceDetails from "./B2BDigitalEcommerceDetails";
 import B2BHero from "./B2BHero";
 import B2BHeroInfo from "./B2BHeroInfo";
 
-export { B2BHero, B2BHeroInfo, B2BDigitalEcommerce };
+export {
+  B2BHero,
+  B2BHeroInfo,
+  B2BDigitalEcommerce,
+  B2BDigitalEcommerceDetails,
+};
