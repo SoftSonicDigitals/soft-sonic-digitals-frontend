37c660661e64dbe10264cfef489fa3d4dec4a2d9
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-29 16:11:33 +1000

    [WEB-84] added the constants for the section

--- src/components/B2BEcommerceComponent/B2BDigitalEcommerce.tsx
+++ src/components/B2BEcommerceComponent/B2BDigitalEcommerce.tsx
@@ -1,16 +1,17 @@
+import {
+  B2B_DIGITAL_ECOMMERCE_DESCRIPTION,
+  B2B_DIGITAL_ECOMMERCE_HEADING,
+} from "@/constants/b2b_ecommerce";
 import React from "react";
-
 const B2BDigitalEcommerce = () => {
   return (
     <section id="digital_ecommerce" className="bg-white-100 ">
       <div className="wrapper_container mb-24  py-16 max-w-[750px] lg:max-w-fit">
         <h1 className="section_heading text-center mb-5">
-          B2B digital commerce transformation solutions all at one place
+          {B2B_DIGITAL_ECOMMERCE_HEADING}
         </h1>
         <p className="section_description px-3  lg:px-10">
-          Incredibly adaptable and fully composable B2B-first eCommerce
-          solutions are offered by Magneto IT Solutions for the digital
-          transformation of your B2B business.
+          {B2B_DIGITAL_ECOMMERCE_DESCRIPTION}
         </p>
       </div>
     </section>
--- src/constants/b2b_ecommerce/index.ts
+++ src/constants/b2b_ecommerce/index.ts
@@ -1,6 +1,18 @@
-export const B2B_HERO_HEADING =
+const B2B_HERO_HEADING =
   "World’s Best B2B eCommerce Solutions Provider For Manufacturing & Distribution Business";
-export const B2B_HERO_DESCRIPTION =
+const B2B_HERO_DESCRIPTION =
   "Deliver customised digital commerce services with composable & headless B2B ecommerce software solutions and transform your ways of interacting with buyers, suppliers, and channel partners.";
-export const B2B_HERO_BTN_TEXT =
-  "BOOK A 30 MINS NO-OBLIGATION CONSULTING SESSION";
+const B2B_HERO_BTN_TEXT = "BOOK A 30 MINS NO-OBLIGATION CONSULTING SESSION";
+
+const B2B_DIGITAL_ECOMMERCE_HEADING =
+  "B2B digital commerce transformation solutions all at one place";
+const B2B_DIGITAL_ECOMMERCE_DESCRIPTION =
+  "Incredibly adaptable and fully composable B2B-first eCommerce solutions are offered by Magneto IT Solutions for the digital transformation of your B2B business.";
+
+export {
+  B2B_HERO_HEADING,
+  B2B_HERO_DESCRIPTION,
+  B2B_HERO_BTN_TEXT,
+  B2B_DIGITAL_ECOMMERCE_HEADING,
+  B2B_DIGITAL_ECOMMERCE_DESCRIPTION,
+};
