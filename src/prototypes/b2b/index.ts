38fd8ddea8be788514801d39267c0949073a82ff
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-29 22:15:45 +1000

    [WEB-84] added the prototype and model for this section

---
+++ src/prototypes/b2b/index.ts
@@ -0,0 +1,18 @@
+import { B2BDigitalEcommerceInfo } from "@/models/b2b";
+
+export const B2B_DIGITAL_ECOMMERCE_INFO: B2BDigitalEcommerceInfo = {
+  videoTitle: "Best B2B Commerce Solution for Wholesaler and Manufacturer",
+  videoLink: "https://www.youtube.com/embed/hCCpvsfmiGI",
+  infoArray: [
+    {
+      title: "B2B Digital Commerce",
+      description:
+        "Experience the ultimate flexibility with our customized and integrated B2B eCommerce platform, tailored to your B2B, D2C, and Omnichannel commerce requirements. Trust Magneto IT Solutions for seamless decoupling or microservices integration.",
+    },
+    {
+      title: "B2B Marketplace Solutions",
+      description:
+        "Empower your wholesale business with our robust multi-vendor B2B marketplace solutions. Connect buyers and sellers, streamline operations, and maximize your dealer network's potential for unrivaled competitiveness.",
+    },
+  ],
+};
