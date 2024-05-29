aa4e42a1b50930ba71e42f07314d083fce35398d
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-29 15:43:01 +1000

    [WEB-84] added the b2b digital ecommerce component

---
+++ src/components/B2BEcommerceComponent/B2BDigitalEcommerce.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const B2BDigitalEcommerce = () => {
+  return <section>B2b Digital Ecommerce</section>;
+};
+
+export default B2BDigitalEcommerce;
--- src/components/B2BEcommerceComponent/index.ts
+++ src/components/B2BEcommerceComponent/index.ts
@@ -1,4 +1,5 @@
+import B2BDigitalEcommerce from "./B2BDigitalEcommerce";
 import B2BHero from "./B2BHero";
 import B2BHeroInfo from "./B2BHeroInfo";
 
-export { B2BHero, B2BHeroInfo };
+export { B2BHero, B2BHeroInfo, B2BDigitalEcommerce };
