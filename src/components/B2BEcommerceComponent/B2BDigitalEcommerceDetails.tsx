5efb0c2e31f5447fee25d5e948b11d3ec656d329
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-29 22:16:29 +1000

    [WEB-84] added mapped over the array

--- src/components/B2BEcommerceComponent/B2BDigitalEcommerceDetails.tsx
+++ src/components/B2BEcommerceComponent/B2BDigitalEcommerceDetails.tsx
@@ -1,3 +1,4 @@
+import { B2B_DIGITAL_ECOMMERCE_INFO } from "@/prototypes/b2b";
 import React from "react";
 
 const B2BDigitalEcommerceDetails = () => {
@@ -5,37 +6,22 @@ const B2BDigitalEcommerceDetails = () => {
     <div className="flex-center lg:flex-row flex-col gap-8 lg:gap-12">
       <div className="w-full max-w-[700px] lg:max-w-[550px] ">
         <iframe
-          src="https://www.youtube.com/embed/hCCpvsfmiGI"
-          title="Best B2B Commerce Solution for Wholesaler and Manufacturer"
+          src={B2B_DIGITAL_ECOMMERCE_INFO.videoLink}
+          title={B2B_DIGITAL_ECOMMERCE_INFO.videoTitle}
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           className=" w-full  h-[310px] mx-auto"
         />
       </div>
 
       <div className="max-w-[700px] lg:max-w-[550px] ">
-        <div className="mb-6">
-          <p className="font-[700] text-xl text-gray-800 mb-2">
-            B2B Digital Commerce
-          </p>
-          <p>
-            Experience the ultimate flexibility with our customized and
-            integrated B2B eCommerce platform, tailored to your B2B, D2C, and
-            Omnichannel commerce requirements. Trust Magneto IT Solutions for
-            seamless decoupling or microservices integration.
-          </p>
-        </div>
-
-        <div className="last:mb-0 mb-6">
-          <p className="font-[700] text-xl text-gray-800 mb-2">
-            B2B Digital Commerce
-          </p>
-          <p>
-            Experience the ultimate flexibility with our customized and
-            integrated B2B eCommerce platform, tailored to your B2B, D2C, and
-            Omnichannel commerce requirements. Trust Magneto IT Solutions for
-            seamless decoupling or microservices integration.
-          </p>
-        </div>
+        {B2B_DIGITAL_ECOMMERCE_INFO.infoArray.map(
+          ({ title, description }, index) => (
+            <div key={index} className=" last:mb-0 mb-6">
+              <p className="font-[700] text-xl text-gray-800 mb-2">{title} </p>
+              <p>{description}</p>
+            </div>
+          )
+        )}
       </div>
     </div>
   );
