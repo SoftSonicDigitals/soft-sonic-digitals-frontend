84f58459b6e76777bc664235f217f5a268b2cb5f
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-29 16:18:43 +1000

    [WEB-84] added the b2b digital ecoomerce details component

--- src/components/B2BEcommerceComponent/B2BDigitalEcommerce.tsx
+++ src/components/B2BEcommerceComponent/B2BDigitalEcommerce.tsx
@@ -3,6 +3,7 @@ import {
   B2B_DIGITAL_ECOMMERCE_HEADING,
 } from "@/constants/b2b_ecommerce";
 import React from "react";
+import B2BDigitalEcommerceDetails from "./B2BDigitalEcommerceDetails";
 const B2BDigitalEcommerce = () => {
   return (
     <section id="digital_ecommerce" className="bg-white-100 ">
@@ -10,9 +11,10 @@ const B2BDigitalEcommerce = () => {
         <h1 className="section_heading text-center mb-5">
           {B2B_DIGITAL_ECOMMERCE_HEADING}
         </h1>
-        <p className="section_description px-3  lg:px-10">
+        <p className="section_description px-3 lg:px-10 mb-10">
           {B2B_DIGITAL_ECOMMERCE_DESCRIPTION}
         </p>
+        <B2BDigitalEcommerceDetails />
       </div>
     </section>
   );
---
+++ src/components/B2BEcommerceComponent/B2BDigitalEcommerceDetails.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const B2BDigitalEcommerceDetails = () => {
+  return (
+    <div className="flex-center">
+      <div>Video</div>
+      <div>Infor</div>
+    </div>
+  );
+};
+
+export default B2BDigitalEcommerceDetails;
