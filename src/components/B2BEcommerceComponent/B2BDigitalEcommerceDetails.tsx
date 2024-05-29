ec7106b1a654807638d964bcf1c26837456af70c
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-29 21:46:21 +1000

    [WEB-84] added the details for this section

--- src/components/B2BEcommerceComponent/B2BDigitalEcommerceDetails.tsx
+++ src/components/B2BEcommerceComponent/B2BDigitalEcommerceDetails.tsx
@@ -2,9 +2,40 @@ import React from "react";
 
 const B2BDigitalEcommerceDetails = () => {
   return (
-    <div className="flex-center">
-      <div>Video</div>
-      <div>Infor</div>
+    <div className="flex-center gap-12">
+      <div>
+        <iframe
+          src="https://www.youtube.com/embed/hCCpvsfmiGI"
+          title="Best B2B Commerce Solution for Wholesaler and Manufacturer"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          className="w-[550px] h-[310px]"
+        />
+      </div>
+      <div className="max-w-[550px]">
+        <div className="mb-6">
+          <p className="font-[700] text-xl text-gray-800 mb-2">
+            B2B Digital Commerce
+          </p>
+          <p>
+            Experience the ultimate flexibility with our customized and
+            integrated B2B eCommerce platform, tailored to your B2B, D2C, and
+            Omnichannel commerce requirements. Trust Magneto IT Solutions for
+            seamless decoupling or microservices integration.
+          </p>
+        </div>
+
+        <div className="last:mb-0 mb-6">
+          <p className="font-[700] text-xl text-gray-800 mb-2">
+            B2B Digital Commerce
+          </p>
+          <p>
+            Experience the ultimate flexibility with our customized and
+            integrated B2B eCommerce platform, tailored to your B2B, D2C, and
+            Omnichannel commerce requirements. Trust Magneto IT Solutions for
+            seamless decoupling or microservices integration.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
