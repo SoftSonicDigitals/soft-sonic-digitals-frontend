7c80382257923cbe30435dc4451100f38f38b443
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-29 15:55:47 +1000

    [WEB-84] rendered the heading and description

--- src/components/B2BEcommerceComponent/B2BDigitalEcommerce.tsx
+++ src/components/B2BEcommerceComponent/B2BDigitalEcommerce.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const B2BDigitalEcommerce = () => {
-  return <section>B2b Digital Ecommerce</section>;
+  return (
+    <section id="digital_ecommerce" className="bg-white-100 ">
+      <div className="wrapper_container mb-24  py-16">
+        <h1 className="text-2xl md:text-3xl lg:text-4xl font-[700] text-gray-800 !leading-[1.25]  text-center mb-5">
+          B2B digital commerce transformation solutions all at one place
+        </h1>
+        <p className="text-xl text-center font-[500] px-10">
+          Incredibly adaptable and fully composable B2B-first eCommerce
+          solutions are offered by Magneto IT Solutions for the digital
+          transformation of your B2B business.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default B2BDigitalEcommerce;
