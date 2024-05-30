9ecc2ba488771a09ad443821ee1018411a4b0708
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-30 17:11:43 +1000

    [WEB-85] added the constants for the section

--- src/constants/b2b_ecommerce/index.ts
+++ src/constants/b2b_ecommerce/index.ts
@@ -9,10 +9,17 @@ const B2B_DIGITAL_ECOMMERCE_HEADING =
 const B2B_DIGITAL_ECOMMERCE_DESCRIPTION =
   "Incredibly adaptable and fully composable B2B-first eCommerce solutions are offered by Magneto IT Solutions for the digital transformation of your B2B business.";
 
+const B2B_DIGITIZATION_REASON_HEADING =
+  "Why do you need B2B commerce digitization? We’ll tell you, because we understand you!";
+const B2B_DIGITIZATION_REASON_DESCRIPTION =
+  "As someone who is into Manufacturing, Distribution or any business that involves B2B selling and management, you're under pressure to implement solutions that meet expectations of the modern times. For that, you will need a partner like us who understands your expectations and provides you with a reliable B2B platform that automates your business today and effortlessly scales it for the future.";
+
 export {
   B2B_HERO_HEADING,
   B2B_HERO_DESCRIPTION,
   B2B_HERO_BTN_TEXT,
   B2B_DIGITAL_ECOMMERCE_HEADING,
   B2B_DIGITAL_ECOMMERCE_DESCRIPTION,
+  B2B_DIGITIZATION_REASON_HEADING,
+  B2B_DIGITIZATION_REASON_DESCRIPTION,
 };
