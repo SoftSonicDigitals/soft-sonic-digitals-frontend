49adf499b10f7c79cb6d51dc9155d23895577f0e
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-30 18:01:22 +1000

    [WEB-85] mapped over the prototype array

--- src/components/B2BEcommerceComponent/B2BDigitizationReasons.tsx
+++ src/components/B2BEcommerceComponent/B2BDigitizationReasons.tsx
@@ -1,23 +1,51 @@
+import {
+  B2B_DIGITIZATION_REASON_DESCRIPTION,
+  B2B_DIGITIZATION_REASON_HEADING,
+} from "@/constants/b2b_ecommerce";
+import { B2B_DIGITIZATION_LIST } from "@/prototypes/b2b";
+import Image from "next/image";
 import React from "react";
 
 const B2BDigitizationReasons = () => {
   return (
     <section id="b2b_digitization_reason">
-      <div className="wrapper_container mb-24">
-        <div className="flex-center flex-col px-8 ">
+      <div className="wrapper_container mb-24 flex-center flex-col">
+        <div className="mb-12 text-center ">
           <h1 className="section_heading mb-6">
-            Why do you need B2B commerce digitization? We’ll tell you, because
-            we understand you!
+            {B2B_DIGITIZATION_REASON_HEADING}
           </h1>
           <p className="section_description ">
-            As someone who is into Manufacturing, Distribution or any business
-            that involves B2B selling and management, you're under pressure to
-            implement solutions that meet expectations of the modern times. For
-            that, you will need a partner like us who understands your
-            expectations and provides you with a reliable B2B platform that
-            automates your business today and effortlessly scales it for the
-            future.
+            {B2B_DIGITIZATION_REASON_DESCRIPTION}
           </p>
+        </div>
+
+        <div className="relative">
+          <Image
+            src="/b2b/Diagram.png"
+            alt="diagram"
+            width={1224}
+            height={652}
+            style={{ objectFit: "cover" }}
+            className="relative"
+          />
+
+          {B2B_DIGITIZATION_LIST.map(({ title, list }, index) => (
+            <div
+              key={index}
+              className={`absolute  ${
+                index % 2 == 0 ? "left-10" : "right-10 "
+              }  ${index < 2 ? "top-8 " : "bottom-16 "}   max-w-[400px] `}
+            >
+              <h3 className="text-3xl font-[700] text-orange mb-4">{title}</h3>
+              <ul className="list-disc text-sm">
+                {list.map((item, index) => (
+                  <li key={index} className="mb-1">
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
       </div>
     </section>
