73ef56717c4b8d0076f44cf735532ec14d9ac552
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-30 15:33:37 +1000

    [WEB-85] added the b2b digitization reasons component

--- src/app/(layout_pages)/services/b2b-ecommerce-solutions/page.tsx
+++ src/app/(layout_pages)/services/b2b-ecommerce-solutions/page.tsx
@@ -2,6 +2,7 @@ import {
   B2BDigitalEcommerce,
   B2BHero,
 } from "@/components/B2BEcommerceComponent";
+import B2BDigitizationReasons from "@/components/B2BEcommerceComponent/B2BDigitizationReasons";
 import React from "react";
 
 const page = () => {
@@ -9,6 +10,7 @@ const page = () => {
     <main className="overflow-hidden">
       <B2BHero />
       <B2BDigitalEcommerce />
+      <B2BDigitizationReasons />
     </main>
   );
 };
---
+++ src/components/B2BEcommerceComponent/B2BDigitizationReasons.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const B2BDigitizationReasons = () => {
+  return <div>B2BDigitizationReasons</div>;
+};
+
+export default B2BDigitizationReasons;
