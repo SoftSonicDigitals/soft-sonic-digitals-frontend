f67cd706ade98ec0c614ea30dbb6d506395f1e28
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-30 18:04:33 +1000

    [WEB-85] adjusted the stylings

--- src/components/B2BEcommerceComponent/B2BDigitizationReasons.tsx
+++ src/components/B2BEcommerceComponent/B2BDigitizationReasons.tsx
@@ -34,10 +34,14 @@ const B2BDigitizationReasons = () => {
               key={index}
               className={`absolute  ${
                 index % 2 == 0 ? "left-10" : "right-10 "
-              }  ${index < 2 ? "top-8 " : "bottom-16 "}   max-w-[400px] `}
+              }  ${
+                index < 2 ? "top-8 " : "bottom-8 xl:bottom-16 "
+              }   max-w-[400px] `}
             >
-              <h3 className="text-3xl font-[700] text-orange mb-4">{title}</h3>
-              <ul className="list-disc text-sm">
+              <h3 className="text-xl xl:text-2xl font-[700] text-orange mb-4">
+                {title}
+              </h3>
+              <ul className="list-disc text-xs xl:text-sm text-gray-700">
                 {list.map((item, index) => (
                   <li key={index} className="mb-1">
                     {item}
