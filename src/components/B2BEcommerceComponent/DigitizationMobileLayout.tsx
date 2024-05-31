2628717387901ce44bf86f38cba4597646441e8e
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2024-05-31 16:36:20 +1000

    [WEB-85] added the drop down list animation

--- src/components/B2BEcommerceComponent/DigitizationMobileLayout.tsx
+++ src/components/B2BEcommerceComponent/DigitizationMobileLayout.tsx
@@ -3,6 +3,7 @@ import useDropDown from "@/hooks/useDropDown";
 import { B2B_DIGITIZATION_LIST } from "@/prototypes/b2b";
 import Image from "next/image";
 import React from "react";
+import { Collapse } from "react-collapse";
 import { FaMinus, FaPlus } from "react-icons/fa6";
 const DigitizationMobileLayout = () => {
   const { selected, isOpen, toggle } = useDropDown();
@@ -13,30 +14,32 @@ const DigitizationMobileLayout = () => {
         alt="title"
         width={336}
         height={342}
-        className="mb-10"
+        className="mb-10 lg:hidden"
       />
-      <div className="max-w-[700px] w-full">
+      <div className="max-w-[700px] w-full lg:hidden">
         {B2B_DIGITIZATION_LIST.map(({ title, list }, index) => (
           <div
             onClick={() => toggle(index)}
             key={index}
-            className=" bg-white sm:bg-white-100 p-8 sm:p-12 rounded-[1rem] sm:shadow-xl mb-6"
+            className=" bg-white sm:bg-white-100 p-8 sm:p-10 rounded-[1rem] sm:shadow-xl sm:mb-6"
           >
-            <div className="mb-8 flex item-center justify-between ">
-              <p className="text-2xl font-[600] text-gray-800 uppercase">
+            <div className="flex item-center justify-between ">
+              <p className="text-xl sm:text-2xl font-[600] text-gray-800 uppercase">
                 {title}
               </p>
               {!isOpen(selected, index) ? <FaPlus /> : <FaMinus />}
             </div>
-            {isOpen(selected, index) && (
-              <ul className="list-disc text-sm xl:text-sm text-gray-700">
-                {B2B_DIGITIZATION_LIST[0].list.map((item, index) => (
-                  <li key={index} className="mb-1">
-                    {item}
-                  </li>
-                ))}
-              </ul>
-            )}
+            <Collapse isOpened={isOpen(selected, index)}>
+              {isOpen(selected, index) && (
+                <ul className=" mt-8 list-disc text-sm xl:text-sm text-gray-700">
+                  {list.map((item, index) => (
+                    <li key={index} className="mb-1">
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </Collapse>
           </div>
         ))}
       </div>
