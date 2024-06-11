e91e9b37c2653f970d2ac24fa73bf0292236bb57
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-11 22:31:55 +1000

    [WEB-86] added the b2b portal component

--- src/app/(layout_pages)/services/b2b-ecommerce-solutions/page.tsx
+++ src/app/(layout_pages)/services/b2b-ecommerce-solutions/page.tsx
@@ -1,6 +1,7 @@
 import {
   B2BDigitalEcommerce,
   B2BHero,
+  B2BPortal,
 } from "@/components/B2BEcommerceComponent";
 import B2BDigitizationReasons from "@/components/B2BEcommerceComponent/B2BDigitizationReasons";
 import React from "react";
@@ -11,6 +12,7 @@ const page = () => {
       <B2BHero />
       <B2BDigitalEcommerce />
       <B2BDigitizationReasons />
+      <B2BPortal />
     </main>
   );
 };
---
+++ src/components/B2BEcommerceComponent/B2BPortal.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const B2BPortal = () => {
+  return (
+    <section id="b2b_portal">
+      <div className="wrapper_container mb-24">
+        <div>list</div>
+        <div>image</div>
+      </div>
+    </section>
+  );
+};
+
+export default B2BPortal;
