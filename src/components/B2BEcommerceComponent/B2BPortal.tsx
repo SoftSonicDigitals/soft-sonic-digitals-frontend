54a33e022c4fa63149ffcb7cadbbfa283dc0581b
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-11 22:39:27 +1000

    [WEB-86] roughly rendered the section heading and description

--- src/components/B2BEcommerceComponent/B2BPortal.tsx
+++ src/components/B2BEcommerceComponent/B2BPortal.tsx
@@ -1,11 +1,31 @@
+import Image from "next/image";
 import React from "react";
 
 const B2BPortal = () => {
   return (
     <section id="b2b_portal">
       <div className="wrapper_container mb-24">
-        <div>list</div>
-        <div>image</div>
+        <div className="px-24">
+          <h1 className="section_heading text-center mb-4">
+            B2B portal development for a digital first world
+          </h1>
+          <p className="section_description text-center">
+            We develop bespoke B2B portals that promote business partnerships
+            between partners, assist in connecting vendors and clients, enable
+            efficient self-service, and give users full assistance.
+          </p>
+        </div>
+        <div className="flex-center">
+          <div>list</div>
+          <div>
+            <Image
+              src="/b2b/list.png"
+              alt="list image"
+              width={560}
+              height={793}
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
