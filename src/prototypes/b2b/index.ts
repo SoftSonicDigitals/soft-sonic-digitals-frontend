b976512a3fb9da5590e64ce231489f87f26ff108
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-12 19:41:38 +1000

    [WEB-86] added drop down funcitonality

--- src/prototypes/b2b/index.ts
+++ src/prototypes/b2b/index.ts
@@ -65,23 +65,37 @@ export const B2B_DIGITIZATION_LIST = [
 export const B2B_PORTAL_LIST = [
   {
     title: "B2B Customer Portal",
+    description:
+      "We can create a sophisticated B2B portal for you that includes pre-built capabilities like the ability for users to make purchase orders, access and subscribe to your services, submit service requests, exchange feedback, manage corporate accounts, and access catalogues and account-based pricing.",
   },
   {
     title: "B2B Vendor Portal",
+    description:
+      "Utilize our vendor portal development solutions to create B2B commerce experiences specifically for your company. Your B2B vendor portal can help you attract and onboard suppliers, send purchase orders, start price bidding, set up online invoicing and payment, and give sellers control over their items, catalogues, and categories.",
   },
   {
     title: "B2B Order Management Portal",
+    description:
+      "With our B2B order management portal solution, you can accelerate the order fulfilment procedure and enhance CX. Through your order management portal, you can keep track of client orders, manage all your customer and order information in one dashboard, fulfil orders according to your own timetables, create invoices and customizable estimates, and split orders into numerous shipments.",
   },
   {
     title: "B2B Digital Catalog Management",
+    description:
+      "For your business, Magneto IT Solutions can create a cloud-native B2B Digital Catalog to enhance customer satisfaction, boost sales effectiveness, and generate money. It enables you to manage product information on your website with ease, to comfortably filter products based on properties using modern search engines, and to enable distributors and retailers to access to your digital catalogue via API.",
   },
   {
     title: "B2B Product Information Management",
+    description:
+      "Using Product Information Management, we can develop cutting-edge B2B solutions for various companies, including distributor portals, vendors portals, and supplier portals. Using the B2B commerce platform developed by us, you can provide your suppliers, vendors, and distributors access to a single repository where they can keep track of all the information about their products and get real-time updates",
   },
   {
     title: "B2B Digital Asset Management",
+    description:
+      "Magneto is exceptional at building cutting-edge B2B portals that enable businesses to interact with external partners. We have successfully implemented vendor portals, supplier portals, distributor portals, digital agencies, etc. by integrating the digital asset management (DAM) technology. We offer digital asset management solutions that are utilised to interchange media files across a variety of output channels, including platforms aimed at consumers.",
   },
   {
     title: "B2B Master Data Management",
+    description:
+      "We offer master data management solutions that are scalable and flexible by utilising adaptable data models. Our clients in the B2B manufacturing and distribution sector can manage their B2B data accurately and conveniently in one place with other company data thanks to our expertise in master data management.",
   },
 ];
