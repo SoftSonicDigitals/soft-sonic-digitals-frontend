f67f4d00b1c672d4184a3c09e6aabea1a09531f5
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-12 18:58:34 +1000

    [WEB-86] roughly rendered the list item

--- src/components/B2BEcommerceComponent/B2BPortal.tsx
+++ src/components/B2BEcommerceComponent/B2BPortal.tsx
@@ -6,25 +6,27 @@ const B2BPortal = () => {
   return (
     <section id="b2b_portal">
       <div className="wrapper_container mb-24">
-        <div className="px-24 mb-16">
-          <h1 className="section_heading text-center mb-4">
-            B2B portal development for a digital first world
-          </h1>
-          <p className="section_description text-center">
-            We develop bespoke B2B portals that promote business partnerships
-            between partners, assist in connecting vendors and clients, enable
-            efficient self-service, and give users full assistance.
-          </p>
-        </div>
-        <div className="flex-center">
-          <DropdownMenu />
-          <div>
-            <Image
-              src="/b2b/list.png"
-              alt="list image"
-              width={560}
-              height={793}
-            />
+        <div>
+          <div className="px-24 mb-16">
+            <h1 className="section_heading text-center mb-4">
+              B2B portal development for a digital first world
+            </h1>
+            <p className="section_description text-center">
+              We develop bespoke B2B portals that promote business partnerships
+              between partners, assist in connecting vendors and clients, enable
+              efficient self-service, and give users full assistance.
+            </p>
+          </div>
+          <div className="flex items-start justify-center gap-16">
+            <DropdownMenu />
+            <div>
+              <Image
+                src="/b2b/list.png"
+                alt="list image"
+                width={560}
+                height={793}
+              />
+            </div>
           </div>
         </div>
       </div>
--- src/components/B2BEcommerceComponent/DropdownMenu.tsx
+++ src/components/B2BEcommerceComponent/DropdownMenu.tsx
@@ -1,7 +1,24 @@
+import { B2B_PORTAL_LIST } from "@/prototypes/b2b";
 import React from "react";
-
+import { FaPlus } from "react-icons/fa6";
+B2B_PORTAL_LIST;
 const DropdownMenu = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="flex item-center justify-start pl-12 gap-8">
+        <FaPlus />
+        <p className="text-xl font-[700]">{B2B_PORTAL_LIST[0].title}</p>
+      </div>
+      <div className="w-[587px] px-24 py-12 shadow-md border-l-4 border-gray-800">
+        <p className="text-xl font-[700]">{B2B_PORTAL_LIST[0].title}</p>
+        We can create a sophisticated B2B portal for you that includes pre-built
+        capabilities like the ability for users to make purchase orders, access
+        and subscribe to your services, submit service requests, exchange
+        feedback, manage corporate accounts, and access catalogues and
+        account-based pricing.
+      </div>
+    </div>
+  );
 };
 
 export default DropdownMenu;
