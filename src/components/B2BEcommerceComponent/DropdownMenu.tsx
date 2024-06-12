c7f7f1951666bc07b2e80cb8b05e9b80c49ddeb3
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-12 19:41:38 +1000

    [WEB-86] added drop down funcitonality

--- src/components/B2BEcommerceComponent/DropdownMenu.tsx
+++ src/components/B2BEcommerceComponent/DropdownMenu.tsx
@@ -1,22 +1,38 @@
+"use client";
+import useDropDown from "@/hooks/useDropDown";
 import { B2B_PORTAL_LIST } from "@/prototypes/b2b";
-import React from "react";
+import React, { Fragment } from "react";
 import { FaPlus } from "react-icons/fa6";
-B2B_PORTAL_LIST;
+
 const DropdownMenu = () => {
+  const { selected, isOpen, toggle } = useDropDown();
+
   return (
     <div>
-      <div className="flex item-center justify-start pl-12 gap-8">
-        <FaPlus />
-        <p className="text-xl font-[700]">{B2B_PORTAL_LIST[0].title}</p>
-      </div>
-      <div className="w-[587px] px-24 py-12 shadow-md border-l-4 border-gray-800">
-        <p className="text-xl font-[700]">{B2B_PORTAL_LIST[0].title}</p>
-        We can create a sophisticated B2B portal for you that includes pre-built
-        capabilities like the ability for users to make purchase orders, access
-        and subscribe to your services, submit service requests, exchange
-        feedback, manage corporate accounts, and access catalogues and
-        account-based pricing.
-      </div>
+      {B2B_PORTAL_LIST.map(({ title, description }, index) => (
+        <Fragment key={index}>
+          {!isOpen(selected, index) && (
+            <div
+              className={`flex item-center pl-12 gap-8 mb-20  w-[587px] `}
+              onClick={() => toggle(index)}
+            >
+              <div className="flex-center">
+                <FaPlus />
+              </div>
+              <p className="text-xl font-[700]">{title}</p>
+            </div>
+          )}
+          {isOpen(selected, index) && (
+            <div
+              className="w-[587px] px-24 py-12 shadow-lg border-l-4 border-gray-800 mb-8 "
+              onClick={() => toggle(index)}
+            >
+              <p className="text-xl font-[700] mb-3">{title}</p>
+              {description}
+            </div>
+          )}
+        </Fragment>
+      ))}
     </div>
   );
 };
