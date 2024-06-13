a804155fb964e84e37fd39ae67bf8d3ad0a927b9
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-13 20:37:08 +1000

    [WEB-86] made the section responsive

--- src/components/B2BEcommerceComponent/DropdownMenuItem.tsx
+++ src/components/B2BEcommerceComponent/DropdownMenuItem.tsx
@@ -21,7 +21,7 @@ const DropdownMenuItem = ({
     <Fragment>
       {!isItemOpen(selectedItem, index) && (
         <div
-          className={`flex item-center pl-12 gap-8 mb-20  w-[587px] `}
+          className={`flex item-center px-6 lg:pl-12 gap-8 mb-20 last:mb-0  xl:w-[587px] cursor-pointer `}
           onClick={() => toggleItem(index)}
         >
           <div className="flex-center">
@@ -33,11 +33,11 @@ const DropdownMenuItem = ({
 
       {isItemOpen(selectedItem, index) && (
         <div
-          className="w-[587px] px-24 py-12 shadow-lg border-l-4 border-gray-800 mb-8 "
+          className="max-w-[527px] xl:max-w-[587px]  mx-5 lg:mx-0 px-12 py-6 lg:px-24 lg:py-12 shadow-lg border-l-4 border-gray-800 mb-8 cursor-pointer "
           onClick={() => toggleItem(index)}
         >
           <p className="text-xl font-[700] mb-3">{title}</p>
-          {description}
+          <p className="text-sm lg:text-base">{description}</p>
         </div>
       )}
     </Fragment>
