cac5e06091989fbdd3eae4d7e14edc0dbd02033d
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-14 18:48:44 +1000

    [WEB-89] refactored to a component for case studies

--- src/components/CaseStudies/CaseStudies.tsx
+++ src/components/CaseStudies/CaseStudies.tsx
@@ -6,6 +6,7 @@ import {
   CASE_STUDIES_HEADING,
   CASE_STUDIES_SUBHEADING,
 } from "@/constants/case_studies";
+import CaseStudiesContainer from "./CaseStudiesContainer";
 
 const CaseStudies = () => {
   return (
@@ -17,21 +18,8 @@ const CaseStudies = () => {
           mainHeadingStyles="mx-4"
           containerStyles="mb-16 gap-4 px-5 md:px-8"
         />
-        <div className="flex-center mb-16 ">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-4 items-center justify-center max-w-[1300px] md:px-[2%] lg:px-10 xl:px-6 2xl:p-0 2xl:gap-x-8 2xl:gap-y-0">
-            {CASE_STUDIES.map(({ title, link, imageName, id }, index) => (
-              <CaseStudy
-                title={title}
-                link={link}
-                imageName={imageName}
-                index={index}
-                key={id}
-              />
-            ))}
-          </div>
-        </div>
 
-        <ViewAllBtn />
+        <CaseStudiesContainer />
       </div>
     </section>
   );
