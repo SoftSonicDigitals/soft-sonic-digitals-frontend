2e6efbbaf777de33acf27c973fa1a38f93e39c71
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-15 12:09:38 +1000

    [WEB-90] downloaded the icons

--- src/components/B2BEcommerceComponent/B2BIndustries.tsx
+++ src/components/B2BEcommerceComponent/B2BIndustries.tsx
@@ -4,8 +4,12 @@ const B2BIndustries = () => {
   return (
     <section id="b2b_industries">
       <div className="wrapper_container mb-24">
-        <div>heading</div>
-        <div>icons</div>
+        <div className="flex gap-8">
+          <p className="text-3xl">
+            Our B2B commerce solutions cover the following industries
+          </p>
+          <div className="grid grid-cols-4"></div>
+        </div>
       </div>
     </section>
   );
