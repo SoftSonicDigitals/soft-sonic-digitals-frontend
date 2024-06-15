3092feb0745f7f4eaa2d2ecd64f28cb2d6d352b5
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-15 21:46:43 +1000

    [WEB-92] added the drop down functionality for faq

--- src/components/B2BEcommerceComponent/FaqContainer.tsx
+++ src/components/B2BEcommerceComponent/FaqContainer.tsx
@@ -1,12 +1,23 @@
+"use client";
 import { B2B_FAQ_LIST } from "@/prototypes/b2b";
 import React from "react";
 import FaqItem from "./FaqItem";
+import useDropDown from "@/hooks/useDropDown";
 
 const FaqContainer = () => {
+  const { toggle, isOpen, selected } = useDropDown();
   return (
     <div>
       {B2B_FAQ_LIST.map(({ answer, question }, index) => (
-        <FaqItem answer={answer} question={question} key={index} />
+        <FaqItem
+          answer={answer}
+          question={question}
+          key={index}
+          index={index}
+          toggleItem={toggle}
+          isItemOpen={isOpen}
+          itemSelected={selected}
+        />
       ))}
     </div>
   );
--- src/components/B2BEcommerceComponent/FaqItem.tsx
+++ src/components/B2BEcommerceComponent/FaqItem.tsx
@@ -1,25 +1,45 @@
 import React from "react";
-import { FaPlus } from "react-icons/fa6";
+import { FaMinus, FaPlus } from "react-icons/fa6";
 
 type FaqItemProps = {
   question: string;
   answer: string | string[];
+  index: number;
+  toggleItem: (index: number) => void;
+  isItemOpen: (selected: number | null, index: number) => boolean;
+  itemSelected: number | null;
 };
-const FaqItem = ({ question, answer }: FaqItemProps) => {
+const FaqItem = ({
+  question,
+  answer,
+  toggleItem,
+  isItemOpen,
+  itemSelected,
+  index,
+}: FaqItemProps) => {
+  const renderAnswer = Array.isArray(answer) ? (
+    <ul className="text-sm md:text-base ml-8 list-disc">
+      {answer.map((item, index) => (
+        <li key={index}>{item} </li>
+      ))}
+    </ul>
+  ) : (
+    <p className="text-sm md:text-base ml-8">{answer}</p>
+  );
+
   return (
     <div
       className="flex items-center justify-start
- gap-8 pb-4 border-b-[1px] border-gray-200 text-gray-800 mx-4 md:mx-8 lg:mx-12  mb-4"
+ gap-8 pb-4 border-b-[1px] border-gray-200 text-gray-800 mx-4 md:mx-8 lg:mx-12  mb-6"
+      onClick={() => toggleItem(index)}
     >
       <div className="flex flex-col ">
         <div className="flex items-center gap-4 mb-2 md:mb-4">
-          <FaPlus />
+          {!isItemOpen(itemSelected, index) ? <FaPlus /> : <FaMinus />}
 
-          <p className="text-base md:text-xl lg:text-[1.4rem] font-[600]">
-            {question}
-          </p>
+          <p className="text-base md:text-xl  font-[600]">{question}</p>
         </div>
-        <p className="text-sm md:text-base ml-8">{answer}</p>
+        {isItemOpen(itemSelected, index) && renderAnswer}
       </div>
     </div>
   );
