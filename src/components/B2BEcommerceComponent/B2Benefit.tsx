21effc5d2a40b5c06c5ade458bf697d4bf72fc74
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-15 15:59:40 +1000

    [WEB-91] added b2b benefit section

---
+++ src/components/B2BEcommerceComponent/B2Benefit.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const B2Benefit = () => {
+  return (
+    <section id="b2b_benefit">
+      <div className=""></div>
+    </section>
+  );
+};
+
+export default B2Benefit;
