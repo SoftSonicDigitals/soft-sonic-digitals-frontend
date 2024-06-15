45fd3882cba48697fc0b40eddc6b94a0dcab47b1
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-15 15:07:59 +1000

    [WEB-90] refactored the icon to its component

---
+++ src/components/B2BEcommerceComponent/IndustryItem.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import React from "react";
+
+type IndustryItemProps = {
+  title: string;
+  iconPath: string;
+};
+
+const IndustryItem = ({ title, iconPath }: IndustryItemProps) => {
+  return (
+    <div className="flex-col  flex-center w-[175px] lg:w-[155px] xl:w-[175px] h-[200px] border-[1.5px] border-gray-100">
+      <Image
+        src={iconPath}
+        alt={title}
+        width={72}
+        height={72}
+        className="mb-6"
+      />
+      <p className="text-center text-lg mb-6 font-[500] text-gray-800">
+        {title}
+      </p>
+    </div>
+  );
+};
+
+export default IndustryItem;
