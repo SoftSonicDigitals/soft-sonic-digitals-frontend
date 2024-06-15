d5ee868ed6865617b737f2dee5070c07b4653a83
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-15 21:31:40 +1000

    [WEB-92]  mapped over the faq list

--- src/components/B2BEcommerceComponent/FaqContainer.tsx
+++ src/components/B2BEcommerceComponent/FaqContainer.tsx
@@ -1,7 +1,15 @@
+import { B2B_FAQ_LIST } from "@/prototypes/b2b";
 import React from "react";
+import FaqItem from "./FaqItem";
 
 const FaqContainer = () => {
-  return <div></div>;
+  return (
+    <div>
+      {B2B_FAQ_LIST.map(({ answer, question }, index) => (
+        <FaqItem answer={answer} question={question} key={index} />
+      ))}
+    </div>
+  );
 };
 
 export default FaqContainer;
