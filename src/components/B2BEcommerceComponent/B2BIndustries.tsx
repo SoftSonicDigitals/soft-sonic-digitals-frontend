81437c419d654c17f9847ab01a61a5db501200d8
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-15 12:28:42 +1000

    [WEB-90] added the icons

--- src/components/B2BEcommerceComponent/B2BIndustries.tsx
+++ src/components/B2BEcommerceComponent/B2BIndustries.tsx
@@ -1,14 +1,32 @@
+import { B2B_INDUSTRIES } from "@/prototypes/b2b";
+import Image from "next/image";
 import React from "react";
 
 const B2BIndustries = () => {
   return (
     <section id="b2b_industries">
       <div className="wrapper_container mb-24">
-        <div className="flex gap-8">
-          <p className="text-3xl">
+        <div className="flex-center  gap-6 px-10">
+          <p className="text-3xl basis-[30%]">
             Our B2B commerce solutions cover the following industries
           </p>
-          <div className="grid grid-cols-4"></div>
+          <div className="grid grid-cols-4  border-2 p-4 ">
+            {B2B_INDUSTRIES.map(({ title, iconPath }, index) => (
+              <div
+                key={index}
+                className="flex-col flex-center w-[175px] h-[200px]"
+              >
+                <Image
+                  src={iconPath}
+                  alt={title}
+                  width={72}
+                  height={72}
+                  className="mb-6"
+                />
+                <p className="text-center mb-6">{title}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
