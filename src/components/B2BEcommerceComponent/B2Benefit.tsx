0454a6ead7602e7a8fc6f618000e9add725d0c21
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-15 17:41:06 +1000

    [WEB-91] refactored the dropdown component to receive props

--- src/components/B2BEcommerceComponent/B2Benefit.tsx
+++ src/components/B2BEcommerceComponent/B2Benefit.tsx
@@ -5,6 +5,7 @@ import {
 import React from "react";
 import DropdownMenu from "./DropdownMenu";
 import Image from "next/image";
+import { B2B_BENEFIT_LIST } from "@/prototypes/b2b";
 
 const B2Benefit = () => {
   return (
@@ -20,7 +21,7 @@ const B2Benefit = () => {
         </div>
 
         <div className="flex-col md:flex-row flex items-start justify-center gap-10 lg:gap-16">
-          <DropdownMenu />
+          <DropdownMenu dropDownList={B2B_BENEFIT_LIST} />
           <div className="basis-full mx-auto">
             <Image
               src="/b2b/list2.png"
