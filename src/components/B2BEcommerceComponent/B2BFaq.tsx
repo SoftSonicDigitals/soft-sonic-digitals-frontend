a2f9963e8893e7a15533b40625596620c5f7e294
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-15 19:15:13 +1000

    [WEB-92] added the faq heading

--- src/components/B2BEcommerceComponent/B2BFaq.tsx
+++ src/components/B2BEcommerceComponent/B2BFaq.tsx
@@ -3,7 +3,11 @@ import React from "react";
 const B2BFaq = () => {
   return (
     <section id="b2b_faq">
-      <div className="wrapper_container mb-24"></div>
+      <div className="wrapper_container mb-24">
+        <h1 className="section_heading text-center mb-4">
+          {"Frequently asked questions"}
+        </h1>
+      </div>
     </section>
   );
 };
