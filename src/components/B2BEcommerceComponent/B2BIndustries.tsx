ebdafc259af21edaf2ad3603c6d488a1febbc5c3
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-15 15:05:05 +1000

    [WEB-90] made the section responsive

--- src/components/B2BEcommerceComponent/B2BIndustries.tsx
+++ src/components/B2BEcommerceComponent/B2BIndustries.tsx
@@ -6,15 +6,15 @@ const B2BIndustries = () => {
   return (
     <section id="b2b_industries">
       <div className="wrapper_container mb-24">
-        <div className="flex-center  gap-6 ">
-          <p className="text-4xl font-[500] basis-[30%] text-gray-800 tracking-wide">
+        <div className="flex flex-col lg:flex-row  items-center justify-around  gap-6 ">
+          <p className="text-2xl lg:text-4xl font-[500] basis-[30%] text-gray-800 tracking-wide !leading-snug text-center lg:text-left">
             Our B2B commerce solutions cover the following industries
           </p>
-          <div className="grid grid-cols-4   p-4 ">
+          <div className="grid grid-cols-2 md:grid-cols-3  lg:grid-cols-4   p-4 ">
             {B2B_INDUSTRIES.map(({ title, iconPath }, index) => (
               <div
                 key={index}
-                className="flex-col flex-center w-[175px] h-[200px] border-[1.5px]"
+                className="flex-col  flex-center w-[175px] lg:w-[155px] xl:w-[175px] h-[200px] border-[1.5px] border-gray-100"
               >
                 <Image
                   src={iconPath}
