347fea94d2e0da61fc9b522bce0add4acad30545
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-17 21:30:44 +1000

    [WEB-93] adjusted the stylings

--- src/components/B2BEcommerceComponent/MACHInfo.tsx
+++ src/components/B2BEcommerceComponent/MACHInfo.tsx
@@ -3,13 +3,15 @@ import React from "react";
 
 const MACHInfo = () => {
   return (
-    <div className="ml-24">
+    <div className="pl-64">
       {B2B_MACH_FEATURES.map(({ title, description }, index) => (
-        <div key={index} className="flex-center gap-12">
-          <p className="text-6xl font-[600] ">{title.slice(0, 1)}</p>
+        <div key={index} className="flex-center gap-12 mb-16 text-gray-900">
+          <div className="text-[4rem] font-[700] w-[52.5px] text-orange-600 ">
+            {title.slice(0, 1)}
+          </div>
           <div>
-            <h4>{title}</h4>
-            <p>{description}</p>
+            <h4 className="text-xl font-[700] mb-2">{title}</h4>
+            <p className="tracking-wide">{description}</p>
           </div>
         </div>
       ))}
