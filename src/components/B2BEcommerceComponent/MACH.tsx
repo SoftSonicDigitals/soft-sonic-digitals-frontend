e53f1855bcc1915b4171cf7b7f4c2692e50f37f9
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-17 19:55:39 +1000

    [WEB-93] added section constants and rendered it

--- src/components/B2BEcommerceComponent/MACH.tsx
+++ src/components/B2BEcommerceComponent/MACH.tsx
@@ -1,7 +1,19 @@
+import { B2B_MACH, B2B_MACH_DESCRIPTION } from "@/constants/b2b_ecommerce";
 import React from "react";
 
 const MACH = () => {
-  return <section id="mach_b2b">MACH</section>;
+  return (
+    <section id="mach_b2b" className="bg-white-100 py-16">
+      <div className="wrapper_container ">
+        <div className="px-3 lg:px-12 mb-16">
+          <h1 className="section_heading  mb-4">{B2B_MACH}</h1>
+          <p className="section_description whitespace-pre-wrap">
+            {B2B_MACH_DESCRIPTION}
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default MACH;
