20af41a569cfbd81b4917abf9c6b78e625e85aa1
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-17 20:03:35 +1000

    [WEB-93] added the prototype for the mach and ajusted the stylings

--- src/components/B2BEcommerceComponent/MACH.tsx
+++ src/components/B2BEcommerceComponent/MACH.tsx
@@ -5,7 +5,7 @@ const MACH = () => {
   return (
     <section id="mach_b2b" className="bg-white-100 py-16">
       <div className="wrapper_container ">
-        <div className="px-3 lg:px-12 mb-16">
+        <div className="px-3 lg:px-8 xl:px-12  mb-16">
           <h1 className="section_heading  mb-4">{B2B_MACH}</h1>
           <p className="section_description whitespace-pre-wrap">
             {B2B_MACH_DESCRIPTION}
