8f106c7aae0181c06fccb0e853b8b182c13868e4
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-18 15:04:07 +1000

    [WEB-96] mapped over the features

--- src/components/B2BEcommerceComponent/ExcellingFeaturesContainer.tsx
+++ src/components/B2BEcommerceComponent/ExcellingFeaturesContainer.tsx
@@ -1,10 +1,15 @@
+import { B2B_EXCELLING_FEATURES } from "@/prototypes/b2b";
 import React from "react";
 
 const ExcellingFeaturesContainer = () => {
   return (
-    <div className="basis-3/5 bg-white">
-      <p>Title</p>
-      <p>description</p>
+    <div className="basis-3/5 bg-white p-14">
+      {B2B_EXCELLING_FEATURES.map(({ title, description }, index) => (
+        <div key={index} className="mb-3 last:mb-0 ">
+          <p className="font-[700] mb-1 tracking-wide">{title}</p>
+          <p className="leading-2 trackig-wide">{description}</p>
+        </div>
+      ))}
     </div>
   );
 };
