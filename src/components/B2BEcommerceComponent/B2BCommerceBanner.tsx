baa8f747a15046756152d20762acbb1627b38e0b
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-18 12:31:45 +1000

    [WEB-94] rendered the image for the section

--- src/components/B2BEcommerceComponent/B2BCommerceBanner.tsx
+++ src/components/B2BEcommerceComponent/B2BCommerceBanner.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
 import React from "react";
 
 const B2BCommerceBanner = () => {
-  return <div>B2BCommerceBanner</div>;
+  return (
+    <section id="b2b_commerce_banner">
+      <Image
+        src="/b2b/b2b_commerce.png"
+        alt="commerce service banner"
+        width={364}
+        height={302}
+        style={{ objectFit: "contain" }}
+        className="-mt-14"
+      />
+    </section>
+  );
 };
 
 export default B2BCommerceBanner;
