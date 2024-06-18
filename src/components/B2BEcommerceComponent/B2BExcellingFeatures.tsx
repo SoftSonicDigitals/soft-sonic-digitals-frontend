e71293338b7a66ddd3868947eda9341f1af81533
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-18 14:37:30 +1000

    [WEB-96] roughly rendered the headings

--- src/components/B2BEcommerceComponent/B2BExcellingFeatures.tsx
+++ src/components/B2BEcommerceComponent/B2BExcellingFeatures.tsx
@@ -2,8 +2,21 @@ import React from "react";
 
 const B2BExcellingFeatures = () => {
   return (
-    <section id="b2b_excelling_features" className="bg-white-100">
-      <div className="wrapper_container">B2BExcellingFeatures</div>
+    <section id="b2b_excelling_features" className="bg-white-100 py-20 mb-24">
+      <div className="wrapper_container ">
+        <div className="flex-center gap-20 mb-16">
+          <h1 className="section_heading mb-4 w-[520px] basis-2/5">
+            Grow how you want with excelling features
+          </h1>
+          <p className="section_description basis-3/5 ">
+            When you avail our B2B website development services, you get robust
+            B2B marketplaces, websites etc. with a comprehensive set of features
+            which are ready to take your business to new heights. We have
+            designed our solution so that it can be used with any B2B eCommerce
+            company model at any time.
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
