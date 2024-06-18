30e5816bc904cd5ca24f6aa5a7895c42d185cf4c
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-18 14:47:49 +1000

    [WEB-96] added the image for the section

--- src/components/B2BEcommerceComponent/B2BExcellingFeatures.tsx
+++ src/components/B2BEcommerceComponent/B2BExcellingFeatures.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const B2BExcellingFeatures = () => {
@@ -5,16 +6,28 @@ const B2BExcellingFeatures = () => {
     <section id="b2b_excelling_features" className="bg-white-100 py-20 mb-24">
       <div className="wrapper_container ">
         <div className="flex-center gap-20 mb-16">
-          <h1 className="section_heading mb-4 w-[520px] basis-2/5">
+          <h1 className="section_heading mb-4   !w-[488px] flex-shrink-0">
             Grow how you want with excelling features
           </h1>
-          <p className="section_description basis-3/5 ">
+          <p className="section_description  ">
             When you avail our B2B website development services, you get robust
             B2B marketplaces, websites etc. with a comprehensive set of features
             which are ready to take your business to new heights. We have
             designed our solution so that it can be used with any B2B eCommerce
             company model at any time.
           </p>
+        </div>
+
+        <div className="flex-center gap-20 mb-16">
+          <Image
+            src="/b2b/excelling_features.png"
+            alt="excelling features"
+            width={488}
+            height={396}
+            className="basis-2/5 "
+          />
+
+          <div className="basis-3/5 bg-white"></div>
         </div>
       </div>
     </section>
