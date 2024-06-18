2b238259085cee65f35fc195947815f30875519f
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-18 15:10:59 +1000

    [WEB-96] adjustedt the stylings and added the grid layout

--- src/components/B2BEcommerceComponent/ExcellingFeaturesContainer.tsx
+++ src/components/B2BEcommerceComponent/ExcellingFeaturesContainer.tsx
@@ -4,12 +4,14 @@ import React from "react";
 const ExcellingFeaturesContainer = () => {
   return (
     <div className="basis-3/5 bg-white p-14">
-      {B2B_EXCELLING_FEATURES.map(({ title, description }, index) => (
-        <div key={index} className="mb-3 last:mb-0 ">
-          <p className="font-[700] mb-1 tracking-wide">{title}</p>
-          <p className="leading-2 trackig-wide">{description}</p>
-        </div>
-      ))}
+      <div className="grid grid-cols-2 gap-8 ">
+        {B2B_EXCELLING_FEATURES.map(({ title, description }, index) => (
+          <div key={index} className="mb-3 last:mb-0 ">
+            <p className="text-lg font-[700] mb-1 tracking-wide">{title}</p>
+            <p className="leading-2 trackig-wide">{description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
