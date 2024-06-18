5bf7cd472c8dc56e168c100c478f6cc6857b0b50
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-18 12:39:02 +1000

    [WEB-94] renderd the banner message

--- src/components/B2BEcommerceComponent/B2BCommerceBanner.tsx
+++ src/components/B2BEcommerceComponent/B2BCommerceBanner.tsx
@@ -3,15 +3,25 @@ import React from "react";
 
 const B2BCommerceBanner = () => {
   return (
-    <section id="b2b_commerce_banner">
-      <Image
-        src="/b2b/b2b_commerce.png"
-        alt="commerce service banner"
-        width={364}
-        height={302}
-        style={{ objectFit: "contain" }}
-        className="-mt-14"
-      />
+    <section id="b2b_commerce_banner" className="bg-orange-100">
+      <div className="wrapper_container">
+        <div className="flex-center gap-10">
+          <Image
+            src="/b2b/b2b_commerce.png"
+            alt="commerce service banner"
+            width={364}
+            height={302}
+            style={{ objectFit: "contain" }}
+            className="relative -top-12 left-8 "
+          />
+          <div>
+            <p className="text-3xl font-[600] text-gray-800">
+              Are you ready to take your business to next level? Avail our B2B
+              commerce services now!
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
