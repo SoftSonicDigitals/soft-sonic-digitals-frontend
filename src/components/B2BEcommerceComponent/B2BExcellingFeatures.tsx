14cba65ade097a7a868ac2467e56d7cfa870b660
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-18 14:36:38 +1000

    [WEB-96] added the eccelling features component

---
+++ src/components/B2BEcommerceComponent/B2BExcellingFeatures.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const B2BExcellingFeatures = () => {
+  return (
+    <section id="b2b_excelling_features" className="bg-white-100">
+      <div className="wrapper_container">B2BExcellingFeatures</div>
+    </section>
+  );
+};
+
+export default B2BExcellingFeatures;
