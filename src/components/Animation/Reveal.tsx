cd3ef6b55763f4d82c8012724062725499702b31
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-07-05 17:04:09 +1000

    [WEB-100] added the sliding animation

---
+++ src/components/Animation/Reveal.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { motion, useAnimation, useInView } from "framer-motion";
+import React, { ReactNode, useEffect, useRef } from "react";
+
+type RevealProps = {
+  children: ReactNode;
+  width?: "fit-content" | "100%";
+};
+
+const Reveal = ({ children, width = "fit-content" }: RevealProps) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    console.log("view", isInView);
+    if (isInView) {
+      mainControls.start("visible");
+    }
+  }, [isInView]);
+
+  return (
+    <div
+      ref={ref}
+      style={{
+        position: "relative",
+        width,
+        overflow: "hidden",
+      }}
+    >
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, x: 75 },
+          visible: { opacity: 1, x: 0 },
+        }}
+        initial="hidden"
+        animate={mainControls}
+        transition={{ duration: 0.5, delay: 0.25 }}
+      >
+        {children}
+      </motion.div>
+    </div>
+  );
+};
+
+export default Reveal;
