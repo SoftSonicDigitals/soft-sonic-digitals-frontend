c5eaebb8f913bf43c262fc762aa5565743f1952a
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-07-07 19:01:32 +1000

    [WEB-101] added the animations to b2c page

--- src/components/B2CEcommerceComponent/B2CHelp.tsx
+++ src/components/B2CEcommerceComponent/B2CHelp.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import B2CHelpTabs from "./B2CHelpTabs";
 import { B2C_HELP_HEADING } from "@/constants/b2c_ecommerce";
+import Reveal from "../Animation/Reveal";
 
 const B2CHelp = () => {
   return (
     <section id="help_every_step">
-      <div className="wrapper_container mb-12 mb:mb-24 ">
-        <div className=" md:mx-6 lg:mx-10 xl:mx-16 flex flex-col lg:flex-row gap-8 lg:gap-16 xl:gap-16  2xl:gap-28 max-w-[725px] lg:max-w-fit mx-auto">
-          <div className="lg:basis-[70%] xl:basis-[60%]">
-            <h1 className=" text-2xl sm:text-2xl  xl:text-4xl font-[700] text-gray-800 !leading-[1.32]   ">
-              {B2C_HELP_HEADING}
-            </h1>
-          </div>
+      <Reveal>
+        <div className="wrapper_container mb-12 mb:mb-24 ">
+          <div className=" md:mx-6 lg:mx-10 xl:mx-16 flex flex-col lg:flex-row gap-8 lg:gap-16 xl:gap-16  2xl:gap-28 max-w-[725px] lg:max-w-fit mx-auto">
+            <div className="lg:basis-[70%] xl:basis-[60%]">
+              <h1 className=" text-2xl sm:text-2xl  xl:text-4xl font-[700] text-gray-800 !leading-[1.32]   ">
+                {B2C_HELP_HEADING}
+              </h1>
+            </div>
 
-          <B2CHelpTabs />
+            <B2CHelpTabs />
+          </div>
         </div>
-      </div>
+      </Reveal>
     </section>
   );
 };
--- src/components/B2CEcommerceComponent/ConcernExpertiseContainer.tsx
+++ src/components/B2CEcommerceComponent/ConcernExpertiseContainer.tsx
@@ -3,6 +3,7 @@ import { CONCERNS_AND_GOALS } from "@/prototypes/b2c";
 import React from "react";
 import RetailerListLayout from "./RetailerListLayout";
 import useDropDown from "@/hooks/useDropDown";
+import Reveal from "../Animation/Reveal";
 
 const ConcernExpertiseContainer = () => {
   const { selected, toggle, isOpen } = useDropDown();
@@ -10,15 +11,17 @@ const ConcernExpertiseContainer = () => {
   return (
     <>
       {CONCERNS_AND_GOALS.map((listItem, index) => (
-        <RetailerListLayout
-          toogle={toggle}
-          selected={selected}
-          key={index}
-          title={listItem.title}
-          list={listItem.list}
-          isOpen={isOpen}
-          index={index}
-        />
+        <Reveal key={index} index={index}>
+          <RetailerListLayout
+            toogle={toggle}
+            selected={selected}
+            key={index}
+            title={listItem.title}
+            list={listItem.list}
+            isOpen={isOpen}
+            index={index}
+          />
+        </Reveal>
       ))}
     </>
   );
