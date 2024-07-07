17bb59410b3336a689a3d59c007d0c5d6ef3a835
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-07-07 19:28:21 +1000

    [WEB-101] added the animation to case studies page

--- src/components/OurWork/CaseStudyTiles.tsx
+++ src/components/OurWork/CaseStudyTiles.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CaseStudyTile from "./CaseStudyTile";
 import { CASE_STUDIES_ARRAY } from "@/prototypes/case_study_pages";
+import Reveal from "../Animation/Reveal";
 
 const CaseStudyTiles = () => {
   return (
@@ -8,12 +9,14 @@ const CaseStudyTiles = () => {
       <div className="wrapper_container grid md:grid-cols-2 lg:grid-cols-3 sm:mt-4 mb-12 lg:mb-24 max-w-[1190px]">
         {CASE_STUDIES_ARRAY.map(
           ({ caseName, displayImg, displayDescription }, index) => (
-            <CaseStudyTile
-              key={index}
-              title={caseName}
-              description={displayDescription}
-              displayImg={displayImg}
-            />
+            <Reveal key={index} index={index}>
+              <CaseStudyTile
+                key={index}
+                title={caseName}
+                description={displayDescription}
+                displayImg={displayImg}
+              />
+            </Reveal>
           )
         )}
       </div>
--- src/components/OurWork/OurWork.tsx
+++ src/components/OurWork/OurWork.tsx
@@ -1,18 +1,21 @@
 import { OUR_WORK_DESCRIPTION, OUR_WORK_HEADING } from "@/constants/our_work";
 import React from "react";
+import Reveal from "../Animation/Reveal";
 
 const OurWork = () => {
   return (
     <section id="our_work">
       <div className=" h-[calc(80vh-48px)]  xl:h-[100vh] flex-center bg-gray-950">
-        <div className="flex flex-col max-w-[600px] lg:max-w-[720px]  text-white items-center justify-center text-center mx-6 md:mx-10 pt-[48px] lg:pt-[88px]">
-          <h1 className="text-3xl md:text-6xl  mb-3 tracking-wide">
-            {OUR_WORK_HEADING}
-          </h1>
-          <p className="font-[500] tracking-wider text-base">
-            {OUR_WORK_DESCRIPTION}
-          </p>
-        </div>
+        <Reveal>
+          <div className="flex flex-col max-w-[600px] lg:max-w-[720px]  text-white items-center justify-center text-center mx-6 md:mx-10 pt-[48px] lg:pt-[88px]">
+            <h1 className="text-3xl md:text-6xl  mb-3 tracking-wide">
+              {OUR_WORK_HEADING}
+            </h1>
+            <p className="font-[500] tracking-wider text-base">
+              {OUR_WORK_DESCRIPTION}
+            </p>
+          </div>
+        </Reveal>
       </div>
     </section>
   );
