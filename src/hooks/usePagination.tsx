f07dc669cfc56e559cda1a45179002a0c37fb9f0
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-07-12 19:28:44 +1000

    [WEB-104] added pagination

--- src/hooks/usePagination.tsx
+++ src/hooks/usePagination.tsx
@@ -1,4 +1,5 @@
 import { getClientCount, getClients } from "@/actions";
+import { CLIENT_TABLE_LIMIT } from "@/constants/dashboard";
 import { useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import useSWR from "swr";
@@ -6,11 +7,13 @@ import useSWR from "swr";
 const usePagination = () => {
   const searchParmas = useSearchParams();
   // get params
-  const page = searchParmas.get("page") ?? "1";
-  const perPage = searchParmas.get("per_page") ?? "5";
+  let page = Number(searchParmas.get("page"));
+  if (!page || page < 1) {
+    page = 1;
+  }
 
   //   getTotalClients
-  const [totalClientCount, setTotalClientCount] = useState<number | null>();
+  const [totalClientCount, setTotalClientCount] = useState<number>(0);
 
   const fetchTotalClient = async () => {
     const totalClient = await getClientCount();
@@ -22,12 +25,12 @@ const usePagination = () => {
   }, []);
 
   const clientsPerPage = async () => {
-    return await getClients(+page, +perPage);
+    return await getClients(page, CLIENT_TABLE_LIMIT);
   };
 
   const { data: clients, error, isLoading } = useSWR("clients", clientsPerPage);
 
-  return { totalClientCount, clients, isLoading };
+  return { totalClientCount, clients, isLoading, page };
 };
 
 export default usePagination;
