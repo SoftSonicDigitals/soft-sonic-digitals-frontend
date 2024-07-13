435be75db9def6b6131001692013da1a500ab34c
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-07-13 10:28:55 +1000

    [WEB-104] added client details

--- src/app/admin/client/[clientId]/page.tsx
+++ src/app/admin/client/[clientId]/page.tsx
@@ -1,13 +1,26 @@
+import { getClientInfo } from "@/actions";
+import { ClientDetailsTable, DashboardNav } from "@/components/Dashboard";
+
 import React from "react";
 
-const page = ({
+const page = async ({
   params: { clientId },
 }: {
   params: {
     clientId: string;
   };
 }) => {
-  return <div>{clientId}</div>;
+  const clientInfo = await getClientInfo(clientId);
+
+  return (
+    <>
+      <DashboardNav />
+      <div className="wrapper_container pt-32">
+        <div className="text-4xl mb-10">Project Details</div>
+        <ClientDetailsTable clientInfo={clientInfo} />
+      </div>
+    </>
+  );
 };
 
 export default page;
--- src/components/Dashboard/index.ts
+++ src/components/Dashboard/index.ts
@@ -1,6 +1,12 @@
+import ClientDetailsTable from "./ClientDetailsTable";
 import ClientRow from "./ClientRow";
 import ClientTables from "./ClientTable";
 import DashboardNav from "./DashboardNav";
 import PaginationSection from "./PaginationSection";
-
-export { DashboardNav, ClientTables, ClientRow, PaginationSection };
+export {
+  DashboardNav,
+  ClientTables,
+  ClientRow,
+  PaginationSection,
+  ClientDetailsTable,
+};
