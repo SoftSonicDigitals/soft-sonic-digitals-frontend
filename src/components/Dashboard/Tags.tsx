e7ea55a7ce8ca9833760c742f32701baf536100d
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-07-18 21:25:25 +1000

    [WEB-104] added edit tags

--- src/components/Dashboard/Tags.tsx
+++ src/components/Dashboard/Tags.tsx
@@ -1,17 +1,23 @@
 import React from "react";
+import { BsTags } from "react-icons/bs";
 
 const Tags = () => {
   return (
-    <div className="mb-8">
-      <div
-        className="inline-block px-3 py-2 rounded-xl  
+    <div className="mb-8 flex items-center gap-4">
+      <div>
+        <div
+          className="inline-block px-3 py-2 rounded-xl  
       text-sm font-[700] tracking-wider text-white bg-[#F57C00] mr-3"
-      >
-        Critical
-      </div>
+        >
+          Critical
+        </div>
 
-      <div className="inline-block px-3 py-2 rounded-xl text-sm  font-[700] tracking-wider text-white bg-[#D32F2F]">
-        Urgent
+        <div className="inline-block px-3 py-2 rounded-xl text-sm  font-[700] tracking-wider text-white bg-[#D32F2F]">
+          Urgent
+        </div>
+      </div>
+      <div className="hover:bg-gray-300 p-1.5 rounded-lg cursor-pointer border-[1px] transition duration-200">
+        <BsTags className="h-[24px] w-[24px]" />
       </div>
     </div>
   );
