2fb9ba1a19a35d2fc23864e392acbcf70c211ddd
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-07-27 12:48:44 +1000

    [WEB-106] added the side nav for larger screen

---
+++ src/app/admin/(auth)/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Raleway } from "next/font/google";
+import "../../globals.css";
+
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "react-hot-toast";
+
+const raleway = Raleway({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "SoftSonic",
+  description: "Consulting and Development Services",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={raleway.className}>
+          {children}
+          <Toaster position="top-right" reverseOrder={false} />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
