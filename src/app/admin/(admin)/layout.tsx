6384db5cd81ba01a1226539ad822d938b8ed9d5c
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-07-29 17:43:39 +1000

    [WEB-106] adjusted the imports and stylings

--- src/app/admin/(admin)/layout.tsx
+++ src/app/admin/(admin)/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${raleway.className} `}>
+        <body className={`${raleway.className} overflow-hidden`}>
           <SideNavBar />
           {children}
           <Toaster position="top-right" reverseOrder={false} />
