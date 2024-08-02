10366e140e6e0f4ea7d9e8da0ff80413955b5a4b
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-08-02 16:43:23 +1000

    [WEB-107] added the UI for Client Statistics

--- src/app/admin/(admin)/clients/page.tsx
+++ src/app/admin/(admin)/clients/page.tsx
@@ -10,11 +10,12 @@ const page = () => {
   return (
     <main className="md:pl-[5rem] pt-16 md:pt-0">
       <ClientLayoutAnimator>
-        <div className=" wrapper_container">
-          <div className="flex justify-between items-center ">
-            <h1 className="text-3xl font-[600] my-10">{CLIENTS}</h1>
+        <div>
+          <div className="flex justify-between lg:items-center flex-col lg:flex-row my-10 gap-6">
+            <h1 className="text-3xl font-[600] ">{CLIENTS}</h1>
+
             <Link
-              className="bg-black-500 text-white text-sm flex flex-center gap-2 py-2 px-4 hover:bg-black "
+              className="bg-black-500 text-white text-sm flex flex-center gap-2 py-2.5 px-5 hover:bg-black w-36"
               href="/contact"
             >
               <IoAddSharp className="text-lg" />
--- src/components/Dashboard/Clients/ClientStatistics.tsx
+++ src/components/Dashboard/Clients/ClientStatistics.tsx
@@ -1,8 +1,15 @@
 import React from "react";
-import { IoAddSharp } from "react-icons/io5";
-
+import StatsCard from "./StatsCard";
 const ClientStatistics = () => {
-  return <></>;
+  return (
+    <section id="client_statistics">
+      <div className="grid grid-cols-1  lg:grid-cols-3 lg:gap-4 border-y-2 lg:border-y-0 mb-16">
+        <StatsCard title={"Total Clients"} data={2140} change={20} />
+        <StatsCard title={"Active Now"} data={300} change={20} />
+        <StatsCard title={"Completed"} data={700} change={20} />
+      </div>
+    </section>
+  );
 };
 
 export default ClientStatistics;
