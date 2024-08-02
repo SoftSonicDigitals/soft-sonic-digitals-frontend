c6d0974eb218ace9351009b61180019dd0bf7897
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-08-02 13:45:03 +1000

    [WEB-107] added the heading and add customer btn

--- src/app/admin/(admin)/clients/page.tsx
+++ src/app/admin/(admin)/clients/page.tsx
@@ -2,11 +2,26 @@ import {
   ClientLayoutAnimator,
   ClientStatistics,
 } from "@/components/Dashboard/Clients";
+import { ADD_CLIENT_BTN_TEXT, CLIENTS } from "@/constants/dashboard";
+import Link from "next/link";
+import { IoAddSharp } from "react-icons/io5";
 
 const page = () => {
   return (
-    <main className="pl-[5rem]">
+    <main className="md:pl-[5rem] pt-16 md:pt-0">
       <ClientLayoutAnimator>
+        <div className=" wrapper_container">
+          <div className="flex justify-between items-center ">
+            <h1 className="text-3xl font-[600] my-10">{CLIENTS}</h1>
+            <Link
+              className="bg-black-500 text-white text-sm flex flex-center gap-2 py-2 px-4 hover:bg-black "
+              href="/contact"
+            >
+              <IoAddSharp className="text-lg" />
+              {ADD_CLIENT_BTN_TEXT}
+            </Link>
+          </div>
+        </div>
         <ClientStatistics />
       </ClientLayoutAnimator>
     </main>
--- src/constants/dashboard/index.ts
+++ src/constants/dashboard/index.ts
@@ -1,3 +1,6 @@
 export const DASHBOARD_TABLE_HEADING = "Clients";
 
 export const CLIENT_TABLE_LIMIT = 5;
+
+export const CLIENTS = "Clients";
+export const ADD_CLIENT_BTN_TEXT = "Add client";
