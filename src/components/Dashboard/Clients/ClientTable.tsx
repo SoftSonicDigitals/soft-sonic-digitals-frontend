04c7c568f9aec5b5a2ad901f8af5aa2026f634c1
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-08-02 19:58:52 +1000

    [WEB-107] added search bar

--- src/components/Dashboard/Clients/ClientTable.tsx
+++ src/components/Dashboard/Clients/ClientTable.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Filters from "./Filters";
+import Searchbar from "./Searchbar";
 
 const ClientTable = () => {
   return (
     <section id="client_tables">
-      <Filters />
+      <div className="flex flex-col-reverse gap-4 sm:flex-row lg:items-center justify-between mb-4">
+        <Filters />
+        <Searchbar />
+      </div>
     </section>
   );
 };
---
+++ src/components/Dashboard/Clients/Searchbar.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React, { RefObject, useRef } from "react";
+import { IoSearch } from "react-icons/io5";
+const Searchbar = () => {
+  const searchRef = useRef<HTMLInputElement | null>(null);
+
+  const onSearchSubmit = () => {
+    console.log(searchRef.current?.value);
+  };
+
+  return (
+    <div className="py-2.5 px-4 border-[1.5px] border-gray-500 flex sm:items-center sm:justify-center gap-3 max-w-full sm:max-w-68">
+      <IoSearch
+        className="text-lg cursor-pointer
+      "
+        onClick={() => onSearchSubmit()}
+      />
+      <input
+        ref={searchRef}
+        placeholder="Search"
+        className="w-64  outline-none border-gray-300 text-sm font-[600] text-gray-700"
+        onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+          e.key == "Enter" ? onSearchSubmit() : null;
+        }}
+      />
+    </div>
+  );
+};
+
+export default Searchbar;
