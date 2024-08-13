be4c21238366cbf2f3b33c251af7d40e254e48e2
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-08-13 14:13:06 +1000

    [WEB-107] chnaged the code for dynamic route and pushed for testing

--- src/app/api/clients/route.ts
+++ src/app/api/clients/route.ts
@@ -2,7 +2,7 @@ import prismadb from "../../../lib/prismadb";
 import { NextRequest, NextResponse } from "next/server";
 
 // make the route dynamic
-export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export async function GET(request: NextRequest) {
   try {
