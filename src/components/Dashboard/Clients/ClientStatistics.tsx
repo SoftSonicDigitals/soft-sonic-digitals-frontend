c0030a94a8f3285086e90756402377d249b70dee
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-08-17 14:28:02 +1000

    [WEB-107] removed statistics from smaller screens

--- src/components/Dashboard/Clients/ClientStatistics.tsx
+++ src/components/Dashboard/Clients/ClientStatistics.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import StatsCard from "./StatsCard";
 const ClientStatistics = () => {
   return (
-    <section id="client_statistics">
+    <section id="client_statistics" className="hidden md:block">
       <div className="grid grid-cols-1  lg:grid-cols-3 lg:gap-4 border-y-2 lg:border-y-0 mb-16">
         <StatsCard title={"Total Customers"} data={2140} change={20} />
         <StatsCard title={"Active Now"} data={300} change={20} />
