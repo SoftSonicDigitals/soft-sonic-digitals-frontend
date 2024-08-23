3147cb46a77a23743715e94e15e2578781c4c1cb
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-08-23 23:07:28 +1000

    added skeleton for filter sub menu

---
+++ src/components/Dashboard/Clients/FilterTags.tsx
@@ -0,0 +1,56 @@
+"use client";
+import React, { useState } from "react";
+
+type FilterTagsProps = {
+  tags: {
+    id: string;
+    label: string;
+  }[];
+};
+
+const FilterTags = ({ tags }: FilterTagsProps) => {
+  const [selectedTags, setSelectedTags] = useState<
+    {
+      id: string;
+      label: string;
+    }[]
+  >([]);
+
+  const handleTagSelect = (tag: any) => {
+    setSelectedTags((prevSelectedTags: any) => {
+      if (prevSelectedTags.includes(tag.id)) {
+        return prevSelectedTags.filter(
+          (selectedTagId: any) => selectedTagId !== tag.id
+        );
+      } else {
+        return [...prevSelectedTags, tag.id];
+      }
+    });
+  };
+
+  return (
+    <>
+      <div className="flex-wrap gap-2 flex">
+        {tags.map((tag) => (
+          <button
+            className={`px-3.5 py-1 border-2   text-sm font-[500] rounded-full ${
+              selectedTags.includes(tag.id)
+                ? "bg-orange text-white border-orange"
+                : "bg-white text-black border-gray-700"
+            }
+            )}`}
+            key={tag.id}
+            onClick={() => handleTagSelect(tag)}
+          >
+            {tag.label}
+          </button>
+        ))}
+      </div>
+      <button className="mt-6  py-2 px-4 bg-black-200 text-white rounded-md  w-full">
+        Filter Now
+      </button>
+    </>
+  );
+};
+
+export default FilterTags;
--- src/prototypes/admin/filter.ts
+++ src/prototypes/admin/filter.ts
@@ -5,19 +5,72 @@ import { LuUserCog2 } from "react-icons/lu";
 
 export const filterMenuOptions = [
   {
-    name: "Date",
+    name: "date",
     icon: HiOutlineCalendar,
   },
   {
-    name: "Tags",
+    name: "tags",
     icon: GoTag,
   },
   {
-    name: "State",
+    name: "state",
     icon: HiOutlineGlobeAsiaAustralia,
   },
   {
-    name: "Service",
+    name: "service",
     icon: LuUserCog2,
   },
+];
+
+export const filterDate = [
+  { id: "1", label: "January" },
+  { id: "2", label: "February" },
+  { id: "3", label: "March" },
+  { id: "4", label: "April" },
+  { id: "5", label: "May" },
+  { id: "6", label: "June" },
+  { id: "7", label: "July" },
+  { id: "8", label: "August" },
+  { id: "9", label: "September" },
+  { id: "10", label: "October" },
+  { id: "11", label: "November" },
+  { id: "12", label: "December" },
+];
+
+export const filterTags = [
+  { id: "blocker", label: "Blocker" },
+  { id: "critical", label: "Critical" },
+  { id: "duplicate", label: "Duplicate" },
+  { id: "in-progress", label: "In-Progress" },
+  { id: "normal", label: "Normal" },
+  { id: "pending", label: "Pending" },
+  { id: "reopened", label: "Reopened" },
+  { id: "resolved", label: "Resolved" },
+  { id: "review", label: "Review" },
+  { id: "urgent", label: "Urgent" },
+  { id: "verified", label: "Verified" },
+];
+
+export const filterState = [
+  { id: "NSW", label: "New South Wales" },
+  { id: "VIC", label: "Victoria" },
+  { id: "QLD", label: "Queensland" },
+  { id: "SA", label: "South Australia" },
+  { id: "WA", label: "Western Australia" },
+  { id: "TAS", label: "Tasmania" },
+  { id: "ACT", label: "Australian Capital Territory" },
+  { id: "NT", label: "Northern Territory" },
+];
+
+export const filterService = [
+  { id: "Ecommerce Consultation", label: "Ecommerce Consultation" },
+  { id: "Ecommerce Development", label: "Ecommerce Development" },
+  { id: "Ecommerce Replatforming", label: "Ecommerce Replatforming" },
+  {
+    id: "Mobile Application Development",
+    label: "Mobile Application Development",
+  },
+  { id: "Web Application Development", label: "Web Application Development" },
+  { id: "Digital Marketing", label: "Digital Marketing" },
+  { id: "UI & UX design", label: "UI & UX design" },
 ];
