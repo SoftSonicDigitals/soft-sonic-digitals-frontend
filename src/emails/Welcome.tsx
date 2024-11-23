f67a160bfe01a3e7487005fe5f31c5b8b0ee0c7e
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-11-23 19:23:16 +1100

    [WEB-105] added the react email and integrated resend for testing

---
+++ src/emails/Welcome.tsx
@@ -0,0 +1,101 @@
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
+import * as React from "react";
+
+interface GithubAccessTokenEmailProps {
+  username?: string;
+}
+
+const baseUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "";
+
+export const Welcome = () => (
+  <Html>
+    <Head />
+    <Preview>Thank you for reaching out soft sonic</Preview>
+    <Body style={main}>
+      <Container style={container}>
+        <Img
+          src={`${baseUrl}/static/github.png`}
+          width="32"
+          height="32"
+          alt="Github"
+        />
+
+        <Text style={title}>Thank you for reaching out soft sonic</Text>
+
+        <Section style={section}>
+          <Text style={text}>
+            Dear <strong>User</strong>,
+          </Text>
+          <Text style={text}>
+            Thank you for contacting <strong>Softsonic Digitals</strong>. We
+            have received your inquiry and will get back to you shortly.
+          </Text>
+
+          <Text style={text}>Best regards,</Text>
+          <Text style={text}>The Softsonic Team</Text>
+        </Section>
+
+        <Text style={footer}>
+          Softsonic Digitals・88 Colin P Kelly Jr Street ・San Francisco, CA
+          94107
+        </Text>
+      </Container>
+    </Body>
+  </Html>
+);
+
+// GithubAccessTokenEmail.PreviewProps = {
+//   username: "alanturing",
+// } as GithubAccessTokenEmailProps;
+
+export default Welcome;
+
+const main = {
+  backgroundColor: "#ffffff",
+  color: "#24292e",
+  fontFamily:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji"',
+};
+
+const container = {
+  maxWidth: "480px",
+  margin: "0 auto",
+  padding: "20px 0 48px",
+};
+
+const title = {
+  fontSize: "24px",
+  lineHeight: 1.25,
+};
+
+const section = {
+  padding: "24px",
+  border: "solid 1px #dedede",
+  borderRadius: "5px",
+  textAlign: "center" as const,
+};
+
+const text = {
+  margin: "0 0 10px 0",
+  textAlign: "left" as const,
+};
+
+const footer = {
+  color: "#6a737d",
+  fontSize: "12px",
+  textAlign: "center" as const,
+  marginTop: "60px",
+};
